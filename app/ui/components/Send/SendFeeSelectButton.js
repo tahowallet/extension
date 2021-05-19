155f1d290b7c0c99dd21fb29cd67ed66354a4be2
Henry Boldizsar <henrymb67@gmail.com>
2021-05-18 19:36:27 -0500

    Init send fee select button
    Signed-off-by: Henry Boldizsar <henrymb67@gmail.com>

---
+++ app/ui/components/Send/SendFeeSelectButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function SendFeeSelectButton() {
+  return (
+    <>
+      <div className="wrap" />
+      <style jsx>
+        {`
+          .wrap {
+            width: 106px;
+            height: 72px;
+            border-radius: 4px;
+            background-color: #193330;
+          }
+        `}
+      </style>
+    </>
+  );
+}
