61cd2887a0a143027d39ae58b23b0d5bb91a0776
Henry Boldizsar <henrymb67@gmail.com>
2021-05-18 19:36:58 -0500

    Add tab bar and tab bar icon components
    Signed-off-by: Henry Boldizsar <henrymb67@gmail.com>

---
+++ ui/components/TabBar/TabBarIcon.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-chrome-extension-router';
+
+export default function TabBarIcon(props) {
+  const { name, isActive, component } = props;
+
+  return (
+    <>
+      <Link component={component}>
+        <div className={`wrap${isActive ? ' active' : ''}`}>
+          <div className="icon" />
+        </div>
+      </Link>
+      <style jsx>
+        {`
+          .icon {
+            background: url('/assets/img/${name}.svg');
+            background-size: 24px 24px;
+            width: 24px;
+            height: 24px;
+            cursor: pointer;
+          }
+          .active {
+            filter: brightness(0) saturate(100%) invert(64%) sepia(9%)
+              saturate(2395%) hue-rotate(354deg) brightness(89%) contrast(112%);
+          }
+        `}
+      </style>
+    </>
+  );
+}
+
+TabBarIcon.propTypes = {
+  name: PropTypes.string.isRequired,
+  isActive: PropTypes.bool,
+  component: PropTypes.node.isRequired,
+};
+
+TabBarIcon.defaultProps = {
+  isActive: false,
+};
