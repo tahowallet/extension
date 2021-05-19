1ca875f89b95467b0873d4f07f29b3b9e527ac39
Henry Boldizsar <henrymb67@gmail.com>
2021-05-18 19:28:27 -0500

    Add placeholder pages
    Accounts, Earn, Menu, Swap
    
    Signed-off-by: Henry Boldizsar <henrymb67@gmail.com>

---
+++ ui/pages/Swap.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { registerRoute } from '../config/routes';
+import CorePageWithTabs from '../components/Core/CorePageWithTabs';
+
+function Swap() {
+  return (
+    <>
+      <CorePageWithTabs>
+        <span className="title">Swap</span>
+      </CorePageWithTabs>
+      <style jsx>
+        {`
+          .title {
+            width: 375px;
+            height: 46px;
+            color: #fefefc;
+            font-family: 'Quincy CF';
+            font-size: 38px;
+            font-weight: 400;
+            text-align: center;
+          }
+        `}
+      </style>
+    </>
+  );
+}
+
+registerRoute('swap', Swap);
