22ae07ba76cf55a61119e5ba39d35f1689d52dc4
Henry Boldizsar <henrymb67@gmail.com>
2021-05-18 19:36:38 -0500

    Init shared asset input
    Signed-off-by: Henry Boldizsar <henrymb67@gmail.com>

---
+++ ui/components/Shared/SharedAssetInput.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function SharedAssetInput() {
+  return (
+    <>
+      <div className="wrap" />
+      <style jsx>
+        {`
+          .wrap {
+            width: 352px;
+            height: 72px;
+            border-radius: 4px;
+            background-color: #193330;
+          }
+        `}
+      </style>
+    </>
+  );
+}
