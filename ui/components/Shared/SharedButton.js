5db539b490070b772fb7715f046c01b6d6ea4ca5
Henry Boldizsar <henrymb67@gmail.com>
2021-05-28 23:13:38 -0500

    Handle disabled, tertiary, secondary, large, and multiple icon sizes

--- ui/components/Shared/SharedButton.js
+++ ui/components/Shared/SharedButton.js
@@ -2,13 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function SharedButton(props) {
-  const { label } = props;
+  const { label, type, size, onClick, isDisabled, icon, iconSize } = props;
 
   return (
     <>
-      <button type="button" className="wrap">
+      <button
+        type="button"
+        className={`wrap${size === 'large' ? ' large' : ''}${
+          type === 'secondary' ? ' secondary' : ''
+        }${isDisabled ? ' disabled' : ''}${
+          type === 'tertiary' ? ' tertiary' : ''
+        }`}
+        onClick={onClick}
+      >
         {label}
-        <div className="icon_button" />
+        {icon ? (
+          <div
+            className={`icon_button${
+              iconSize === 'large' ? ' icon_large' : ''
+            }`}
+          />
+        ) : null}
       </button>
       <style jsx>
         {`
@@ -29,11 +43,48 @@ export default function SharedButton(props) {
             padding: 0 17px;
           }
           .icon_button {
-            background: url('./images/${label}@2x.png');
-            background-size: 12px 12px;
+            mask-image: url('./images/${icon}@2x.png');
+            mask-size: 12px 12px;
             width: 12px;
             height: 12px;
             margin-left: 9px;
+            background-color: #ffffff;
+          }
+          .large {
+            height: 48px;
+            border-radius: 8px;
+            padding: 0 24px;
+          }
+          .icon_large {
+            mask-size: 24px 24px;
+            width: 24px;
+            height: 24px;
+            margin-left: 10px;
+          }
+          .secondary {
+            background: unset;
+            border: 2px solid var(--trophy-gold);
+            color: var(--trophy-gold);
+          }
+          .secondary .icon_button {
+            background-color: var(--trophy-gold);
+          }
+          .disabled {
+            background-color: var(--green-60);
+            color: var(--green-80);
+          }
+          .disabled .icon_button {
+            background-color: var(--green-80);
+          }
+          .tertiary {
+            color: var(--trophy-gold);
+            background: unset;
+            border: unset;
+            padding: unset;
+            font-size: 18px;
+          }
+          .tertiary .icon_button {
+            background-color: var(--trophy-gold);
           }
         `}
       </style>
@@ -43,4 +94,17 @@ export default function SharedButton(props) {
 
 SharedButton.propTypes = {
   label: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['primary', 'secondary', 'tertiary']).isRequired,
+  size: PropTypes.oneOf(['medium', 'large']).isRequired,
+  icon: PropTypes.string,
+  iconSize: PropTypes.oneOf(['medium', 'large']),
+  onClick: PropTypes.func,
+  isDisabled: PropTypes.bool,
+};
+
+SharedButton.defaultProps = {
+  icon: null,
+  isDisabled: false,
+  iconSize: 'medium',
+  onClick: () => {},
 };
