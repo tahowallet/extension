5d4f95bf8f8d93f38cd11b16830c7b2306e2a957
Henry Boldizsar <henrymb67@gmail.com>
2021-06-07 01:35:41 -0500

    Add swap quote component

---
+++ ui/components/Swap/SwapQuote.js
@@ -0,0 +1,170 @@
+import React, { useState } from 'react';
+import _ from 'lodash';
+import SharedButton from '../Shared/SharedButton';
+import SwapAssetsHeader from './SwapAssetsHeader';
+import SwapQuoteAssetCard from './SwapQuoteAssetCard';
+import SwapTransactionSettings from './SwapTransactionSettings';
+import SwapApprovalStep from './SwapApprovalStep';
+
+export default function SwapQoute() {
+  const [stepComplete, setStepComplete] = useState(-1);
+
+  function handleApproveClick() {
+    setStepComplete(0);
+    _.delay(() => {
+      setStepComplete(1);
+    }, 1500);
+    _.delay(() => {
+      setStepComplete(2);
+    }, 3000);
+    _.delay(() => {
+      setStepComplete(3);
+    }, 4500);
+  }
+
+  return (
+    <>
+      <div className="wrap center_horizontal">
+        <SwapAssetsHeader />
+        <div className="qoute_cards">
+          <SwapQuoteAssetCard />
+          <div className="icon_switch" />
+          <SwapQuoteAssetCard />
+        </div>
+        <div className="label label_right">1 ETH = 9,843 KEEP</div>
+        <div className="settings_wrap">
+          <SwapTransactionSettings />
+        </div>
+        {stepComplete > -1 ? (
+          <>
+            <div className="approval_steps">
+              <SwapApprovalStep
+                isDone={stepComplete >= 1}
+                label="Approve to spend ETH"
+              />
+              <SwapApprovalStep
+                isDone={stepComplete >= 2}
+                label="Approve to spend KEEP"
+              />
+              <SwapApprovalStep
+                isDone={stepComplete === 3}
+                label="Swap Approved"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="exchange_section_wrap">
+              <div className="label">Exchange route</div>
+              <div className="exchange_content">
+                <div className="left">
+                  <div className="icon_uniswap" />
+                  Uniswap v3
+                </div>
+                <div>100%</div>
+              </div>
+            </div>
+            <div className="approve_button center_horizontal">
+              <SharedButton
+                type="primary"
+                size="large"
+                label="Aprove Assets & Swap"
+                disableIcon
+                onClick={handleApproveClick}
+              />
+            </div>
+          </>
+        )}
+      </div>
+      <style jsx>
+        {`
+          .wrap {
+            width: 352px;
+            margin-top: -24px;
+          }
+          .icon_uniswap {
+            background: url('./images/uniswap@2x.png');
+            background-size: 24px 24px;
+            width: 24px;
+            height: 24px;
+            margin-right: 8px;
+          }
+          .approval_steps {
+            height: 96px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            margin-top: 24px;
+          }
+          .icon_switch {
+            background: url('./images/switch@2x.png') center no-repeat;
+            background-size: 20px 20px;
+            width: 40px;
+            height: 32px;
+            border-radius: 4px;
+            border: 3px solid var(--hunter-green);
+            background-color: var(--green-95);
+            margin-left: -11px;
+            margin-right: -11px;
+            z-index: 5;
+            flex-grow: 1;
+            flex-shrink: 0;
+          }
+          .qoute_cards {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          }
+          .label {
+            height: 17px;
+            color: var(--green-60);
+            font-family: Segment;
+            font-size: 14px;
+            font-weight: 400;
+            letter-spacing: 0.42px;
+            line-height: 16px;
+            margin-bottom: 5px;
+            margin-left: 7px;
+            display: flex;
+            justify-content: space-between;
+          }
+          .label_right {
+            float: right;
+            margin-top: 16px;
+          }
+          .settings_wrap {
+            margin-top: 44px;
+          }
+          .exchange_content {
+            width: 352px;
+            height: 40px;
+            border-radius: 4px;
+            background-color: var(--green-95);
+            color: var(--green-20);
+            font-family: Segment;
+            font-size: 14px;
+            font-weight: 400;
+            letter-spacing: 0.42px;
+            line-height: 16px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0px 16px;
+            box-sizing: border-box;
+          }
+          .approve_button {
+            width: fit-content;
+            margin-top: 36px;
+          }
+          .exchange_section_wrap {
+            margin-top: 16px;
+          }
+          .left {
+            display: flex;
+            align-items: center;
+          }
+        `}
+      </style>
+    </>
+  );
+}
