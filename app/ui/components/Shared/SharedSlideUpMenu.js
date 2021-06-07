aa123a48ac86214173f553c23d75b385a48604ed
Henry Boldizsar <henrymb67@gmail.com>
2021-06-07 00:44:23 -0500

    Update slide up menu to also have small and large size options

--- app/ui/components/Shared/SharedSlideUpMenu.js
+++ app/ui/components/Shared/SharedSlideUpMenu.js
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function SharedSlideUpMenu(props) {
-  const { isOpen, isRunAnimation, close, children } = props;
+  const { isOpen, isRunAnimation, close, size, children } = props;
+
+  let menuHeight = '536px';
+  if (size === 'large') {
+    menuHeight = '600px';
+  } else if (size === 'small') {
+    menuHeight = '268px';
+  }
 
   return (
     <>
       <div
-        className={`slide_up_menu ${
-          isRunAnimation && !isOpen ? ' closed_animate' : ''
-        }${isRunAnimation && isOpen ? ' open_animate' : ''}
+        className={`slide_up_menu
+        ${size === 'large' ? ' large' : ''}
+        ${isRunAnimation && !isOpen ? ' closed_animate' : ''}
+        ${isRunAnimation && isOpen ? ' open_animate' : ''}
         ${!isRunAnimation && !isOpen ? ' closed' : ''}`}
       >
         <button
@@ -24,7 +32,7 @@ export default function SharedSlideUpMenu(props) {
         {`
           .slide_up_menu {
             width: 100vw;
-            height: 536px;
+            height: ${menuHeight};
             border-radius: 16px;
             background-color: var(--green-95);
             position: fixed;
@@ -32,7 +40,7 @@ export default function SharedSlideUpMenu(props) {
             right: 0px;
             bottom: 0px;
             z-index: 999;
-            transform: translateY(536px);
+            transform: translateY(${menuHeight});
             padding-top: 24px;
             box-sizing: border-box;
           }
@@ -44,22 +52,25 @@ export default function SharedSlideUpMenu(props) {
             position: absolute;
             right: 24px;
           }
+          .large {
+            background-color: #002522;
+          }
           .open_animate {
             transform: translateY(0px);
             animation: slideUp cubic-bezier(0.19, 1, 0.22, 1) 0.445s;
             animation-direction: forward;
           }
           .closed {
-            transform: translateY(536px);
+            transform: translateY(${menuHeight});
           }
           .closed_animate {
-            transform: translateY(536px);
+            transform: translateY(${menuHeight});
             animation: slideDown linear 0.16s;
             animation-direction: backward;
           }
           @keyframes slideUp {
             0% {
-              transform: translateY(536px);
+              transform: translateY(${menuHeight});
             }
             100% {
               transform: translateY(0px);
@@ -70,7 +81,7 @@ export default function SharedSlideUpMenu(props) {
               transform: translateY(0px);
             }
             100% {
-              transform: translateY(536px);
+              transform: translateY(${menuHeight});
             }
           }
         `}
@@ -84,4 +95,9 @@ SharedSlideUpMenu.propTypes = {
   isRunAnimation: PropTypes.bool.isRequired,
   close: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+};
+
+SharedSlideUpMenu.defaultProps = {
+  size: 'medium',
 };
