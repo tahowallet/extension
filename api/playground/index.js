8e63be4468df0c321028d773a29761f2727ca7b3
frankiebee <frankie.diamond@gmail.com>
2021-06-08 18:50:07 -1000

    remove playground

--- api/playground/index.js
+++
@@ -1,6 +0,0 @@
-export { EnsController } from './ens'
-export { NetworkController } from './network'
-export { TransactionController } from './transactions'
-export { PermissionsController } from './permissions'
-
-
