02ff6c665dfa3e017c4951a8b3383508a6855b7e
Henry Boldizsar <henrymb67@gmail.com>
2021-06-28 22:41:08 -0500

    Add onboarding create password component

---
+++ ui/components/Onboarding/OnboardingCreatePassword.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SharedInput from '../Shared/SharedInput';
+import SharedButton from '../Shared/SharedButton';
+import { titleStyle } from './titleStyle';
+
+export default function OnboardingCreatePassword(props) {
+  const { triggerNextStep } = props;
+
+  return (
+    <section>
+      <div className="full_logo" />
+      <h1 className="serif_header">Good hunting.</h1>
+      <div className="subtitle">The decentralized web awaits.</div>
+      <SharedInput placeholder="Password" />
+      <div className="repeat_password_wrap">
+        <SharedInput placeholder="Repeat Password" />
+      </div>
+      <SharedButton
+        type="primary"
+        size="large"
+        label="Begin the hunt"
+        onClick={triggerNextStep}
+      />
+      <div className="restore">
+        <SharedButton
+          type="tertiary"
+          size="medium"
+          label="Restoring account?"
+        />
+      </div>
+      <style jsx>
+        {`
+          ${titleStyle}
+          .full_logo {
+            background: url('./images/full_logo@2x.png');
+            background-size: cover;
+            width: 118px;
+            height: 120px;
+            margin-bottom: 17px;
+          }
+          .repeat_password_wrap {
+            margin-top: 19px;
+            margin-bottom: 24px;
+          }
+          .restore {
+            position: fixed;
+            bottom: 26px;
+          }
+        `}
+      </style>
+    </section>
+  );
+}
+
+OnboardingCreatePassword.propTypes = {
+  triggerNextStep: PropTypes.func.isRequired,
+};
