8f35d364758805c9396fad834606561bffcebbf7
frankiebee <frankie.diamond@gmail.com>
2021-07-13 11:02:29 -1000

    comment out polling till needed

--- api/networks/ethereum/polling.js
+++ api/networks/ethereum/polling.js
@@ -1,35 +1,35 @@
-import { alarms } from '../../../lib/alarms'
+// import { alarms } from '../../../lib/alarms'
 
 
-export default class Poller extends EventEmitter {
-  constructor ({ alarmName, periodInMinutes = 1, method }) {
-    this.time = time //|| figure out the time based on seconds?
-    this.method = method
-    this.ready = new Promise((resolve, reject) => {
-      this.isReady = resolve
-    })
-    alarms.get(alarmName)
-    .then((alarm) => {
-      if (alarm) {
-        return this.isReady()
-      } else {
-        alarms.create(alarmName, {
-          periodInMinutes
-        })
-      }
+// export default class Poller extends EventEmitter {
+//   constructor ({ alarmName, periodInMinutes, method }) {
+//     this.time = time //|| figure out the time based on seconds?
+//     this.method = method
+//     this.ready = new Promise((resolve, reject) => {
+//       this.isReady = resolve
+//     })
+//     alarms.get(alarmName)
+//     .then((alarm) => {
+//       if (alarm) {
+//         return this.isReady()
+//       } else {
+//         alarms.create(alarmName, {
+//           periodInMinutes
+//         })
+//       }
 
-    })
-  }
+//     })
+//   }
 
-  async start () {
-    await this.ready
-  }
+//   async start () {
+//     await this.ready
+//   }
 
-  stop () {
+//   stop () {
 
-  }
+//   }
 
-  _handler () {
+//   _handler () {
 
-  }
-}
+//   }
+// }
