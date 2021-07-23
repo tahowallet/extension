bcd60247e23c8142578010c0e14bf9a97c08795c
Antonio Salazar Cardozo <antonio@thesis.co>
2021-07-23 15:58:43 -0400

    Setup ESLint for TypeScript and JS both
    This configuration now Actually Runs™, unlike the previous one.
    
    Errors are everywhere, however....

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    "airbnb",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    browser: "readonly",
+  },
+  rules: {
+    "react/jsx-one-expression-per-line": [0],
+    "react/jsx-filename-extension": [0],
+    "import/no-extraneous-dependencies": [
+      "error",
+      // Don't slap build files for importing devDependencies.
+      { devDependencies: ["!+(src/api|ui)/**/*.+(ts|js)"] },
+    ],
+  },
+  ignorePatterns: "dist/",
+  parser: "@typescript-eslint/parser",
+}
