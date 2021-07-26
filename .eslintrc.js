67c56e17a648a8c4a7019d258760018a94958d55
Antonio Salazar Cardozo <antonio@thesis.co>
2021-07-26 10:57:31 -0400

    Adapt ESLint rules for TypeScript more closely
    Remove errors for missing .ts extensions on imports and replace two
    base ESLint rules enabled by AirBnB with their TypeScript-aware
    equivalents.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,18 @@ module.exports = {
       // Don't slap build files for importing devDependencies.
       { devDependencies: ["!+(src/api|ui)/**/*.+(ts|js)"] },
     ],
+    "import/extensions": [
+      "error",
+      {
+        ts: "never",
+      },
+    ],
+    // Replace a couple of base ESLint rules defined by airbnb with TypeScript
+    // extensions that understand certain TypeScript-specific features.
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    "no-useless-constructor": "off",
+    "@typescript-eslint/no-useless-constructor": ["error"],
   },
   ignorePatterns: "dist/",
   parser: "@typescript-eslint/parser",
