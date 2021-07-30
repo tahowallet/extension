47f3fee9d724ea4726d0b1cbc0ad056716690940
Henry Boldizsar <henrymb67@gmail.com>
2021-07-30 14:49:16 -0500

    Use persisted ETH address if existing

--- ui/slices/account.js
+++ ui/slices/account.js
@@ -36,38 +36,62 @@ export const { loadAccount, loadAccountSuccess, loadAccountFailure } =
 export const accountSelector = (state) => state.account
 export default accountSlice.reducer
 
+// Temporarily fill in hard coded USD conversion
+function enrichWithUSDAmounts(account) {
+  const updatedAccount = { ...account }
+  if (updatedAccount?.total_balance?.amount) {
+    const usdAmount = (
+      updatedAccount?.total_balance?.amount * 2411.44
+    ).toLocaleString("en-US", {
+      maximumFractionDigits: 2,
+    })
+    updatedAccount.total_balance.usd_amount = usdAmount
+    updatedAccount.tokens[0].usd_balance = usdAmount
+  }
+
+  return updatedAccount
+}
+
+async function getAccountAddressIfExists() {
+  const account = await send({
+    route: "/accounts/",
+    method: "GET",
+  })
+
+  return account.address
+}
+
+async function createOrGetAddress() {
+  const existingAddress = await getAccountAddressIfExists()
+  let address
+  if (!existingAddress) {
+    address = await send({
+      route: "/accounts/",
+      method: "POST",
+      params: {
+        data: SEED_PHRASE_MM,
+      },
+    })
+  } else {
+    address = existingAddress
+  }
+
+  return address
+}
+
 export function subscribeToAccount() {
   return async (dispatch) => {
     dispatch(loadAccount())
 
     try {
-      const address = await send({
-        route: "/accounts/",
-        method: "POST",
-        params: {
-          data: SEED_PHRASE_MM,
-        },
-      })
-
+      const address = await createOrGetAddress()
       subscribe(
         {
           route: `/accounts/${address}`,
           method: "GET",
         },
         (account) => {
-          const updatedAccount = { ...account }
-
-          // Temporarily fill in hard coded USD conversion
-          if (updatedAccount?.total_balance?.amount) {
-            const usdAmount = (
-              updatedAccount?.total_balance?.amount * 2411.44
-            ).toLocaleString("en-US", {
-              maximumFractionDigits: 2,
-            })
-            updatedAccount.total_balance.usd_amount = usdAmount
-            updatedAccount.tokens[0].usd_balance = usdAmount
-          }
-
+          const updatedAccount = enrichWithUSDAmounts(account)
           dispatch(loadAccountSuccess(updatedAccount))
         }
       )
