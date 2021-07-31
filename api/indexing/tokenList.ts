cde148be812ee85a377bebfbe792b3faf1696026
Matt Luongo <mhluongo@gmail.com>
2021-07-30 21:40:04 -0400

    Introduce a few more Token List utils
    ... as well as relevant types.

--- api/indexing/tokenList.ts
+++ api/indexing/tokenList.ts
@@ -1,47 +1,120 @@
-import { Network, NetworkFungibleAsset } from '../types'
+import Ajv from "ajv"
 
-import Ajv from 'ajv'
+import { TokenList, schema } from "@uniswap/token-lists"
 
-import { TokenList, schema } from '@uniswap/token-lists'
+import { Network, NetworkFungibleAsset } from "../types"
 
 // TODO integrate this with /api/networks
-const ETHEREUM_NETWORK : Network = {
-  name: 'Ethereum Main Net',
+const ETHEREUM_NETWORK: Network = {
+  name: "Ethereum Main Net",
   baseAsset: {
-    name: 'Ether',
-    symbol: 'ETH',
-    decimals: BigInt(18),
+    name: "Ether",
+    symbol: "ETH",
+    decimals: 18,
   },
-  chainId: '1',
-  family: 'EVM'
+  chainId: "1",
+  family: "EVM",
 }
 
-async function fetchAndValidateTokenList(url : string) {
-  let response = await fetch(url)
-  if (response.ok) {
-    let json = await response.json()
-    let valid = (new Ajv()).validate(schema, json)
-    if (valid) {
-      return json as TokenList
-    }
+export interface TokenListAndReference {
+  url: string
+  tokenList: TokenList
+}
+
+async function fetchAndValidateTokenList(
+  url: string
+): Promise<TokenListAndReference> {
+  const response = await fetch(url)
+  if (!response.ok) {
+    throw new Error(`Error resolving token list at ${url}`)
+  }
+  const json = await response.json()
+  const valid = new Ajv().validate(schema, json)
+  if (!valid) {
+    throw new Error(`Invalid token list at ${url}`)
+  }
+  return {
+    tokenList: json as TokenList,
+    url,
   }
 }
 
-function tokenListToFungibleAssets(tokenList : TokenList) : NetworkFungibleAsset[] {
+async function fetchAndValidateTokenLists(urls: string[]) {
+  return (await Promise.allSettled(urls.map(fetchAndValidateTokenList)))
+    .filter((l) => l.status === "fulfilled")
+    .map((l) => (l as PromiseFulfilledResult<TokenListAndReference>).value)
+}
+
+function tokenListToFungibleAssets(
+  url: string,
+  tokenList: TokenList
+): NetworkFungibleAsset[] {
   return tokenList.tokens.map((t) => {
     return {
       metadata: {
         logoURL: t.logoURI,
+        tokenLists: [
+          {
+            url,
+            name: tokenList.name,
+            logoURL: tokenList.logoURI,
+          },
+        ],
       },
       name: t.name,
       symbol: t.symbol,
-      decimals: BigInt(t.decimals),
+      decimals: t.decimals,
       homeNetwork: ETHEREUM_NETWORK,
-      contractAddress: t.address
+      contractAddress: t.address,
     }
   })
 }
 
+/*
+ * Return all tokens in the provided lists, de-duplicated and structured in our
+ * types for easy manipulation, and sorted by the number of lists each appears
+ * in.
+ */
+export function networkAssetFromLists(tokenLists: TokenListAndReference[]) {
+  const fungibleAssets = tokenLists
+    .map((listAndRef) =>
+      tokenListToFungibleAssets(listAndRef.url, listAndRef.tokenList)
+    )
+    .reduce((a, b) => a.concat(b), [])
+
+  function tokenReducer(
+    acc: { [contractAddress: string]: NetworkFungibleAsset },
+    asset: NetworkFungibleAsset
+  ) {
+    if (asset.contractAddress in acc) {
+      const original = acc[asset.contractAddress]
+      acc[asset.contractAddress] = {
+        ...original,
+        metadata: {
+          ...original.metadata,
+          ...asset.metadata,
+          tokenLists: original.metadata.tokenLists.concat(
+            asset.metadata.tokenLists
+          ),
+        },
+      }
+    }
+    return acc
+  }
+
+  const merged = fungibleAssets.reduce(tokenReducer, {})
+
+  return Object.entries(merged)
+    .map(([k, v]) => v)
+    .slice()
+    .sort(
+      (a, b) =>
+        (a.metadata?.tokenLists?.length || 0) -
+        (b.metadata?.tokenLists?.length || 0)
+    )
+}
+
+// TODO track "tokens of interest" in extension storage
 // TODO fetch, validate, and cache a token list
 // TODO maintain a cached NetworkFungibleAsset list from prioritized token lists
 // TODO get the latest list of assets from the cache
--- api/types/index.ts
+++ api/types/index.ts
@@ -1,9 +1,16 @@
 export { Transaction, UnsignedTransaction } from "@ethersproject/transactions"
 
+export interface TokenListReference {
+  name: string
+  url: string
+  logoURL?: string
+}
+
 export interface AssetMetadata {
-  coinGeckoURL?: string
+  coinGeckoId?: string
   logoURL?: string
   websiteURL?: string
+  tokenLists: TokenListReference[]
 }
 
 export interface Asset {
@@ -12,11 +19,16 @@ export interface Asset {
   metadata?: AssetMetadata
 }
 
+/*
+ * Fungible assets include coins, currencies, and many tokens.
+ */
 export type FungibleAsset = Asset & {
-  decimals: BigInt
+  decimals: number
 }
 
-export type NetworkFamily = 'EVM' | 'BTC'
+export type FiatCurrency = FungibleAsset
+
+export type NetworkFamily = "EVM" | "BTC"
 
 export interface Network {
   name: string
@@ -33,3 +45,21 @@ export type NetworkSpecificSmartContract = {
 export type NetworkAsset = Asset & NetworkSpecificSmartContract
 
 export type NetworkFungibleAsset = FungibleAsset & NetworkSpecificSmartContract
+
+/*
+ * The primary type representing amounts in fungible asset transactions.
+ */
+export interface AssetAmount {
+  asset: FungibleAsset
+  amount: BigInt
+}
+
+/*
+ * Used to represent the price (per single unit) of an asset, fungible or
+ * non-fungible, against a fungible asset. Note the fungible asset can be
+ * something like an ERC-20 token or fiat currency.
+ */
+export interface UnitPriceAndTime {
+  unitPrice: AssetAmount
+  lastUpdated: number
+}
