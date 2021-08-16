a78c7b4ac3ca86dd2f4367d9f7a5bdebe684bee8
Henry Boldizsar <henrymb67@gmail.com>
2021-08-15 21:26:50 -0500

    Update accounts panel with footer & wallet name component

--- ui/components/AccountsNotificationPanel/AccountsNotificationPanelAccounts.js
+++ ui/components/AccountsNotificationPanel/AccountsNotificationPanelAccounts.js
@@ -1,5 +1,66 @@
 import React, { useState } from "react"
 import AccountsNotificationPanelAccountItem from "./AccountsNotificationPanelAccountItem"
+import SharedButton from "../Shared/SharedButton"
+
+function WalletName() {
+  return (
+    <>
+      <div className="wallet_title">
+        <div className="left">
+          <div className="icon_wallet" />
+          Trezor
+          <div className="icon_edit" />
+        </div>
+        <div className="right">
+          <SharedButton
+            type="tertiary"
+            size="small"
+            label="Add address"
+            icon="plus"
+            iconSize="medium"
+            isDisabled
+          />
+        </div>
+      </div>
+      <style jsx>{`
+        .wallet_title {
+          color: #fff;
+          font-size: 18px;
+          font-weight: 600;
+          line-height: 24px;
+          padding: 0px 12px 0px 16px;
+          margin-bottom: 16px;
+          margin-top: 8px;
+          align-items: center;
+          display: flex;
+          justify-content: space-between;
+        }
+        .icon_wallet {
+          background: url("./images/wallet_kind_icon@2x.png") center no-repeat;
+          background-size: cover;
+          width: 24px;
+          height: 24px;
+          margin-right: 8px;
+        }
+        .icon_edit {
+          background: url("./images/edit@2x.png") center no-repeat;
+          background-size: cover;
+          width: 13px;
+          height: 13px;
+          margin-left: 8px;
+        }
+        .left {
+          align-items: center;
+          display: flex;
+        }
+        .right {
+          align-items: center;
+          display: flex;
+        }
+      `}</style>
+    </>
+  )
+}
 
 export default function AccountsNotificationPanelAccounts() {
   const [selectedAccount, setSelectedAccount] = useState(1)
@@ -7,11 +68,7 @@ export default function AccountsNotificationPanelAccounts() {
 
   return (
     <div>
-      <div className="wallet_title">
-        <div className="icon_wallet" />
-        Trezor
-        <div className="icon_edit" />
-      </div>
+      <WalletName />
       <ul>
         {Array(1)
           .fill("")
@@ -31,11 +88,7 @@ export default function AccountsNotificationPanelAccounts() {
             )
           })}
       </ul>
-      <div className="wallet_title">
-        <div className="icon_wallet" />
-        Trezor
-        <div className="icon_edit" />
-      </div>
+      <WalletName />
       <ul>
         {Array(3)
           .fill("")
@@ -45,7 +98,6 @@ export default function AccountsNotificationPanelAccounts() {
                 type="button"
                 onClick={() => {
                   setSelectedWallet(1)
-
                   setSelectedAccount(index)
                 }}
               >
@@ -56,40 +108,45 @@ export default function AccountsNotificationPanelAccounts() {
             )
           })}
       </ul>
+      <footer>
+        <SharedButton
+          type="tertiary"
+          size="medium"
+          label="Add Wallet"
+          icon="plus"
+          iconSize="medium"
+          iconPosition="left"
+        />
+      </footer>
+
       <style jsx>
         {`
           ul {
+            display: flex;
             flex-direction: column;
-          }
-          .wallet_title {
-            color: #fff;
-            font-size: 18px;
-            font-weight: 600;
-            line-height: 24px;
-            padding-left: 16px;
-            margin-bottom: 16px;
-            margin-top: 8px;
+            justify-content: center;
             align-items: center;
+            align-content: center;
           }
-          .wallet_title:first-of-type {
-            margin-top: 24px;
-          }
-          .icon_wallet {
-            background: url("./images/wallet_kind_icon@2x.png") center no-repeat;
-            background-size: cover;
-            width: 24px;
-            height: 24px;
-            margin-right: 8px;
-          }
-          .icon_edit {
-            background: url("./images/edit@2x.png") center no-repeat;
-            background-size: cover;
-            width: 13px;
-            height: 13px;
-            margin-left: 8px;
+          footer {
+            width: 100%;
+            height: 48px;
+            background-color: var(--hunter-green);
+            position: fixed;
+            bottom: 0px;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            padding: 0px 12px;
+            box-sizing: border-box;
           }
         `}
       </style>
+      <style jsx global>{`
+        .wallet_title:first-of-type {
+          margin-top: 24px;
+        }
+      `}</style>
     </div>
   )
 }
