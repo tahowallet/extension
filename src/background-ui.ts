f73f345e67d5babac3a0a7545c1fd3b482100aa1
Antonio Salazar Cardozo <antonio@thesis.co>
2021-08-18 17:53:56 -0400

    Move the background-ui script to the proxy store
    Rather than subscribing via the API, the background-ui script now
    creates its own proxy store and subscribes to its updates to observe
    when browser notifications need to go out.
    
    Right now the subscription mechanism is quite raw. Whether it is useful
    to make it more sophisticated will be seen later.

--- src/background-ui.ts
+++ src/background-ui.ts
@@ -1,29 +1,45 @@
-import { browser, startApi } from "@tallyho/tally-api"
+import { browser, newProxyStore } from "@tallyho/tally-api"
 
-startApi().then(async ({ main }) => {
-  const accountsApi = main.getApi()["/accounts/"]
-  let latestActivityHashes = (
-    await accountsApi.GET({ address: null })
-  ).activity.map(({ hash }) => hash)
-  main
-    .getApi()
-    ["/accounts/"].subscribe(
-      ({
+newProxyStore().then((backgroundStore) => {
+  // undefined if no account has been resolved, string array with the latest
+  // activity hashes if it has.
+  let latestActivityHashes: Set<string> | undefined
+
+  backgroundStore.subscribe(() => {
+    const state = backgroundStore.getState()
+    const { account } = state.account
+
+    if (account) {
+      const {
         total_balance: { amount: totalBalance },
         activity: updatedActivity,
-      }) => {
+      } = account
+
+      // Undefined activity hashes means we're initializing. Otherwise, notify
+      // for any new activity.
+      if (typeof latestActivityHashes === "undefined") {
+        latestActivityHashes = new Set()
+      } else {
         const newActivity = updatedActivity.filter(({ hash }) =>
-          latestActivityHashes.includes(hash)
+          latestActivityHashes.has(hash)
         )
 
-        browser.notifications.create("balance-udpate", {
+        browser.notifications.create("balance-update", {
           type: "basic",
           title: "Balance Update",
           message: `<address> has balance ${totalBalance}`,
           contextMessage: `${newActivity.length} transactions have updated the balance for <address> to ${totalBalance}`,
         })
-
-        latestActivityHashes = updatedActivity.map(({ hash }) => hash)
       }
-    )
+
+      latestActivityHashes = updatedActivity.reduce(
+        (acc, { hash }) => acc.add(hash),
+        latestActivityHashes
+      )
+    } else {
+      // Account has been cleared, reset activity hashes so they can be
+      // reinitialized next time the account is set.
+      latestActivityHashes = undefined
+    }
+  })
 })
