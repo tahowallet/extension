5b264f1672c90ed75ccf8c33e69443bea55e2718
Henry Boldizsar <henrymb67@gmail.com>
2021-08-19 17:37:42 -0500

    Add earn card component

--- ui/pages/Earn.tsx
+++ ui/pages/Earn.tsx
@@ -1,5 +1,78 @@
 import React, { ReactElement } from "react"
 import CorePage from "../components/Core/CorePage"
+import SharedAssetIcon from "../components/Shared/SharedAssetIcon"
+import SharedButton from "../components/Shared/SharedButton"
+
+function EarnCard() {
+  return (
+    <div className="card">
+      <div className="asset_icon_wrap">
+        <SharedAssetIcon />
+      </div>
+      <span className="token_name">ETH</span>
+      <span className="apy_info_label">Current APY</span>
+      <span className="apy_percent">250%</span>
+      <div className="icon_rewards_locked" />
+      <div className="divider" />
+      <SharedButton type="secondary" size="medium" label="Deposit" />
+      <style jsx>{`
+        .card {
+          width: 160px;
+          height: 266px;
+          border-radius: 8px;
+          background-color: var(--green-95);
+          flex-shrink: 0;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 18px;
+        }
+        .asset_icon_wrap {
+          border: solid var(--green-95) 3px;
+          border-radius: 500px;
+          margin-top: -18px;
+        }
+        .token_name {
+          color: var(--green-20);
+          font-size: 16px;
+          font-weight: 500;
+          line-height: 24px;
+          text-transform: uppercase;
+          margin-top: 3px;
+          margin-bottom: 4px;
+        }
+        .apy_info_label {
+          color: var(--green-40);
+          font-size: 14px;
+          line-height: 17px;
+          margin-bottom: -4px;
+        }
+        .apy_percent {
+          color: #11bea9;
+          font-family: "Quincy CF";
+          font-size: 36px;
+          font-weight: 500;
+          line-height: 42px;
+          text-align: center;
+          margin-bottom: 4px;
+        }
+        .icon_rewards_locked {
+          background: url("./images/reward_locked@2x.png") center no-repeat;
+          background-size: cover;
+          width: 51px;
+          height: 58px;
+          margin-bottom: 13px;
+          margin-top: 2px;
+        }
+        .divider {
+          width: 128px;
+          border-bottom: 1px solid var(--green-120);
+          margin-bottom: 15px;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export default function Earn(): ReactElement {
   return (
@@ -17,11 +90,11 @@ export default function Earn(): ReactElement {
         </header>
         <section className="standard_width">
           <h3>Your deposits</h3>
-          <div className="cards_wrap">{Array(2).fill("").map(FarmCard)}</div>
+          <div className="cards_wrap">{Array(2).fill("").map(EarnCard)}</div>
         </section>
         <section className="standard_width">
           <h3>Earn 1</h3>
-          <div className="cards_wrap">{Array(2).fill("").map(FarmCard)}</div>
+          <div className="cards_wrap">{Array(2).fill("").map(EarnCard)}</div>
         </section>
       </CorePage>
       <style jsx>
