a90ee4e2463ec98c71f5bb3090176df34e6272f4
frankiebee <frankie.diamond@gmail.com>
2021-08-29 22:24:13 -1000

    wip: exposing all signing methods

--- api/keys/constants.ts
+++ api/keys/constants.ts
@@ -1,11 +1,11 @@
 export const PERSITIANCE_KEY: string = "tally-vault"
 
-export enum HD_PATHS {
+export const HD_PATHS = {
   Ethereum: "m’/44’/60’/0’/0",
   Ethereum_Classic: "m’/44’/61’/0’/0",
   Ethereum_Testnet_Ropsten: "m’/44’/1’/0’/0",
 }
 
 export const LOCKED_ERROR: string = "Can not preform key operations while locked"
-export const NO_VAULT_ERROR: string =
-  "Must first create a vault or import keys to do any key operations"
+export const NO_VAULT_ERROR: string = "Must first create a vault or import keys to do any key operations"
+export const ADDRESS_NOT_FOUND_ERROR: string = "Can not find keyring for address:"
--- api/keys/index.ts
+++ api/keys/index.ts
@@ -1,11 +1,11 @@
-import KeyringController from "@tallyho/eth-keyring-controller"
-
+import KeyringController, { normalizeAddress } from "@tallyho/eth-keyring-controller"
 import { getPersistedState, persistState } from "../lib/db"
-
+import { Transaction } from "ethereumjs-tx"
 import {
   PERSITIANCE_KEY,
   LOCKED_ERROR,
   NO_VAULT_ERROR,
+  ADDRESS_NOT_FOUND_ERROR,
 } from "./constants"
 
 export enum KEY_TYPE {
@@ -15,20 +15,28 @@ export enum KEY_TYPE {
   singleSECP = "single#secp256k1",
 }
 
-export type keyTypeStrings = keyof typeof KEY_TYPE
+export type KeyTypeStrings = keyof typeof KEY_TYPE
 
+export type MsgParams = {
+  data: string
+  from: string
+}
 
 // TODO: type declarations in eth-hd-tree
 export interface Seed {
-  data: any //string // seed material
+  data: string // seed material
   type: KEY_TYPE
-  index: any //number // the current account index
+  index: number // the current account index
   reference: string // unique reference
-  path: any //string // default path to derive new keys
+  path: string // default path to derive new keys
 }
 
-export interface ImportData {
-  type: keyTypeStrings
+interface Opts {
+  [key: string]: string | number
+}
+
+export type ImportData = {
+  type: KeyTypeStrings
   data: string
   password ?: string
 }
@@ -165,21 +173,33 @@ export default class Keys {
     }
     return this.#keyrings[reference].mnemonic
   }
-  // returns address list for specific keyring if no reference is
-  // supplied returns all address for all keyrings
-  // TODO: figure out why ts dosent like the reduce method
+  /**
+   * getAddresses
+   *
+   * Returns the public addresses of all current accounts
+   * managed by all currently unlocked keyrings.
+   *
+   * @returns {Promise<Array<string>>} The array of accounts.
+   */
   async getAddresses (reference?: string): Promise<string[]> {
     await this.#isready
     if (reference === undefined) {
-      const addresses = Object.values(this.#keyrings).reduce((agg: string[], keyring: any): Promise<string[]>[] => {
-        return agg.push(keyring.getAccounts())
-      }, [])
+      const addresses: Promise<string[]>[] = Object.values(this.#keyrings).map((keyring: any): Promise<string[]> => {
+        return keyring.getAccounts()
+      })
       const finAddresses = await Promise.all(addresses)
       return finAddresses.reduce((agg: string[], addressList: string[]) => agg.concat(addressList), [])
     }
     return this.#keyrings[reference].getAccounts()
   }
 
+  /**
+   * getWalletReferences
+   *
+   * Returns the id's of all seeds to be used as a reference for each keyring
+   *
+   * @returns {Promise<string[]>} The array of accounts.
+   */
   async getWalletReferences (): Promise<string[]> {
     await this.#isready
     return Object.keys(this.#keyrings)
@@ -194,6 +214,190 @@ export default class Keys {
     await this.#updateIndex(reference, allAccounts.length)
     return accounts
   }
+  /**
+   * Export Account
+   *
+   * Requests the private key from the keyring controlling
+   * the specified address.
+   *
+   * Returns a Promise that may resolve with the private key string.
+   *
+   * @param {string} address - The address of the account to export.
+   * @returns {Promise<string>} The private key of the account.
+   */
+  async exportAccount (address) {
+    const keyring = await this.#getKeyringFromAddress(address)
+    return keyring.exportAccount(normalizeAddress(address))
+  }
+
+  /**
+   *
+   * Remove Account
+   *
+   * Removes a specific account from a keyring
+   * If the account is the last/only one then it also removes the keyring.
+   *
+   * @param {string} address - The address of the account to remove.
+   * @returns {Promise<void>} A Promise that resolves if the operation was successful.
+   */
+  async removeAccount (address: string) {
+    const keyring = await this.#getKeyringFromAddress(address)
+    // Not all the keyrings support this, so we have to check
+    if (typeof keyring.removeAccount === 'function') {
+      keyring.removeAccount(address)
+      const accounts = await keyring.getAccounts()
+      // Check if this was the last/only account
+      if (accounts.length === 0) {
+        this.#masterKeyring.removeEmptyKeyrings()
+      }
+      await this.#persistState()
+    } else {
+      throw new Error(
+        `Keyring ${keyring.type} doesn't support account removal operations`,
+      )
+    }
+  }
+
+  //
+  // SIGNING METHODS
+  //
+
+  /**
+   * Sign Ethereum Transaction
+   *
+   * Signs an Ethereum transaction object.
+   *
+   * @param {Object} ethTx - The transaction to sign.
+   * @param {string} _fromAddress - The transaction 'from' address.
+   * @param {Object} opts - Signing options.
+   * @returns {Promise<Object>} The signed transactio object.
+   */
+  async signTransaction (ethTx: Transaction, _fromAddress: string, opts: Opts = {}) {
+    const fromAddress = normalizeAddress(_fromAddress)
+    const keyring = await this.#getKeyringFromAddress(fromAddress)
+    return keyring.signTransaction(fromAddress, ethTx, opts)
+  }
+
+  /**
+   * Sign Message
+   *
+   * Attempts to sign the provided message parameters.
+   *
+   * @param {Object} msgParams - The message parameters to sign.
+   * @returns {Promise<Buffer>} The raw signature.
+   */
+  async signMessage (msgParams: MsgParams, opts: Opts = {}) {
+    const address = normalizeAddress(msgParams.from)
+    const keyring = await this.#getKeyringFromAddress(address)
+    return keyring.signMessage(address, msgParams.data, opts)
+  }
+
+  /**
+   * Sign Personal Message
+   *
+   * Attempts to sign the provided message paramaters.
+   * Prefixes the hash before signing per the personal sign expectation.
+   *
+   * @param {Object} msgParams - The message parameters to sign.
+   * @returns {Promise<Buffer>} The raw signature.
+   */
+  async signPersonalMessage (msgParams: MsgParams, opts: Opts = {}) {
+    const address = normalizeAddress(msgParams.from)
+    const keyring = await this.#getKeyringFromAddress(address)
+    return keyring.signPersonalMessage(address, msgParams.data, opts)
+  }
+
+  /**
+   * Get encryption public key
+   *
+   * Get encryption public key for using in encrypt/decrypt process.
+   *
+   * @param {Object} address - The address to get the encryption public key for.
+   * @returns {Promise<Buffer>} The public key.
+   */
+  async getEncryptionPublicKey (_address: string, opts: Opts = {}) {
+    const address = normalizeAddress(_address)
+    const keyring = await this.#getKeyringFromAddress(address)
+    return keyring.getEncryptionPublicKey(address, opts)
+  }
+
+  /**
+   * Decrypt Message
+   *
+   * Attempts to decrypt the provided message parameters.
+   *
+   * @param {Object} msgParams - The decryption message parameters.
+   * @returns {Promise<Buffer>} The raw decryption result.
+   */
+  async decryptMessage (msgParams, opts: Opts = {}) {
+    const address = normalizeAddress(msgParams.from)
+    const keyring = await this.#getKeyringFromAddress(address)
+    return keyring.decryptMessage(address, msgParams.data, opts)
+  }
+
+  /**
+   * Sign Typed Data
+   * (EIP712 https://github.com/ethereum/EIPs/pull/712#issuecomment-329988454)
+   *
+   * @param {Object} msgParams - The message parameters to sign.
+   * @returns {Promise<Buffer>} The raw signature.
+   */
+  async signTypedMessage (msgParams, opts: Opts = { version: 'V1' }) {
+    const address = normalizeAddress(msgParams.from)
+    const keyring = await this.#getKeyringFromAddress(address)
+    return keyring.signTypedData(address, msgParams.data, opts)
+  }
+
+  /**
+   * Gets the app key address for the given Ethereum address and origin.
+   *
+   * @param {string} _address - The Ethereum address for the app key.
+   * @param {string} origin - The origin for the app key.
+   * @returns {string} The app key address.
+   */
+  async getAppKeyAddress (_address: string, origin: string) {
+    const address = normalizeAddress(_address)
+    const keyring = await this.#getKeyringFromAddress(address)
+    return keyring.getAppKeyAddress(address, origin)
+  }
+
+  /**
+   * Exports an app key private key for the given Ethereum address and origin.
+   *
+   * @param {string} _address - The Ethereum address for the app key.
+   * @param {string} origin - The origin for the app key.
+   * @returns {string} The app key private key.
+   */
+  async exportAppKeyForAddress (_address: string, origin: string) {
+    const address = normalizeAddress(_address)
+    const keyring = await this.#getKeyringFromAddress(address)
+    if (!('exportAccount' in keyring)) {
+      throw new Error(`The keyring for address ${_address} does not support exporting.`)
+    }
+    return keyring.exportAccount(address, { withAppKeyOrigin: origin })
+  }
+
+
+  //
+  // PRIVATE METHODS
+  //
+
+  async #getKeyringFromAddress (address: string): Promise<any> {
+    const keyrings = Object.values(this.#keyrings)
+    const addresses = await Promise.all(keyrings.map(async (keyring: any) => {
+      return keyring.getAccounts()
+    }))
+    let index
+    addresses.forEach((addressList: string[], i: number) => {
+      if (addressList.includes(address)) {
+        index = i
+      }
+    })
+    if (index === undefined) {
+      throw new Error(ADDRESS_NOT_FOUND_ERROR + address)
+    }
+    return keyrings[index]
+  }
 
   async #persistState (): Promise<void> {
     const encryptedState = await this.#masterKeyring.encryptor.encrypt(
@@ -206,9 +410,14 @@ export default class Keys {
   }
 
 
-  // todo look into freezing or use-strict not sure if possible or applicable here?
-  // one way hash method for creating references to mnemonics
-  // also makes it easy to know if it already exists
+  /**
+   * #createReference
+   *
+   * one way hash method for creating references to mnemonics
+   *
+   * @returns {Promise<string>} has string
+   */
+
   async #createReference(data: string): Promise<string> {
     const dataUint8 = new TextEncoder().encode(data)
     const hashBufer = await crypto.subtle.digest("SHA-256", dataUint8)
