d384603a646595c4bcef2f921298be6396f754b3
Henry Boldizsar <henrymb67@gmail.com>
2021-09-03 10:07:50 -0500

    Antonio-proof `env.d.ts` w/ notice of its importance

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,6 @@
 /// <reference types="styled-jsx" />
 
+// Although you would expect this file to be unnecessary, removing it will
+// result in a handful of type errors. See PR #196.
+
 declare module "styled-jsx/style"
