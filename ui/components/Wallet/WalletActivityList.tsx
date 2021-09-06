dfcd9db4c652e144837f9afbbe1915a7d294eb5b
Henry Boldizsar <henrymb67@gmail.com>
2021-09-06 17:19:45 -0500

    Use `AnyEVMTransaction` type from bg in activity list*
    * // Leaving the old activity type as an option for now to use the
    // existing single asset page placeholder data.

--- ui/components/Wallet/WalletActivityList.tsx
+++ ui/components/Wallet/WalletActivityList.tsx
@@ -1,18 +1,19 @@
 // @ts-check
 
 import React, { ReactElement } from "react"
+import { AnyEVMTransaction } from "@tallyho/tally-api/types"
 import { setShowingActivityDetail } from "@tallyho/tally-api/redux-slices/ui"
 import { useBackgroundDispatch, useBackgroundSelector } from "../../hooks"
 import SharedSlideUpMenu from "../Shared/SharedSlideUpMenu"
 import WalletActivityDetails from "./WalletActivityDetails"
 import WalletActivityListItem from "./WalletActivityListItem"
 
+// Leaving the old activity type as an option for now to use the
+// existing single asset page placeholder data.
 interface Props {
-  activity: {
-    hash?: string
-    timeStamp?: string
-    from?: string
-  }[]
+  activity:
+    | AnyEVMTransaction[]
+    | { timeStamp?: string; value?: string; from?: string }[]
 }
 
 export default function WalletActivityList(props: Props): ReactElement {
