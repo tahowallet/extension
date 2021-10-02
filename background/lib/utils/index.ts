6eb795725f527c8b1e222c6e147e73a0fe84fdf6
Matt Luongo <mhluongo@gmail.com>
2021-10-02 13:49:48 -0400

    Nix unused background util functions
    We'll replace them with proper currency utils in #252

--- background/lib/utils/index.ts
+++ background/lib/utils/index.ts
@@ -8,29 +8,6 @@ export function normalizeEVMAddress(address: string | Buffer): HexString {
   return normalizeHexAddress(address)
 }
 
-export function idGenerator(start?: number) {
-  let index = start || 1
-  return () => {
-    index += 1
-    return index
-  }
-}
-
-export function createEthProviderWrapper(provider: any) {
-  return new Proxy(provider, {
-    get: (_, method) => {
-      if (method === "provider") return provider
-      return async (...params) => {
-        return provider.request({ method, params })
-      }
-    },
-  })
-}
-
-export function weiToEth(value: string | number): number {
-  return (typeof value === "number" ? value : parseInt(value, 10)) / 10e17
-}
-
 export function convertToEth(value: string | number): string {
   if (value && value >= 1) {
     return utils.formatUnits(BigInt(value))
@@ -38,17 +15,7 @@ export function convertToEth(value: string | number): string {
   return ""
 }
 
-export function transactionFee(
-  gas: string | number,
-  gasPrice: string | number
-): number {
-  return (
-    (typeof gas === "number" ? gas : parseInt(gas, 10)) *
-    (typeof gasPrice === "number" ? gasPrice : parseInt(gasPrice, 10))
-  )
-}
-
-// BigInts are CUTTING EDGE and can't be saved natively in Redux / Browser storage
+// BigInts are CUTTING EDGE and can't be saved natively in Redux / browser storage
 export function jsonEncodeBigInt(input: unknown): string {
   return JSONBig({ useNativeBigInt: true }).stringify(input)
 }
