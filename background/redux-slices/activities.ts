633d035d2cc4690b9af747e3bdca04afa6a286a4
Henry Boldizsar <henrymb67@gmail.com>
2021-10-21 01:46:08 -0500

    Init Activities slice

---
+++ background/redux-slices/activities.ts
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit"
+import Emittery from "emittery"
+import { AnyEVMTransaction } from "../types"
+
+export type ActivityItem = AnyEVMTransaction & {
+  timestamp?: string
+  value: bigint
+  from?: string
+  isSent?: boolean
+}
+
+export type ActivitiesState = {
+  activities: {
+    [address: string]: {
+      [hash: string]: ActivityItem
+    }
+  }
+}
+
+export const initialState: ActivitiesState = {
+  activities: {},
+}
+
+const activitiesSlice = createSlice({
+  name: "activities",
+  initialState,
+  reducers: {
+    activityEncountered: (immerState, { payload: activityItem }) => {
+      const from = activityItem.from.toLowerCase()
+
+      // TODO handle more than "from", and confirm it's an
+      // added account first
+      if (!immerState.activities[from]) {
+        immerState.activities[from] = {}
+      }
+
+      // TODO Move KeyRenameAndPickMap here
+      immerState.activities[from][activityItem.hash] = activityItem
+    },
+  },
+})
+
+export type Events = {
+  activityEncountered: AnyEVMTransaction
+}
+
+export const emitter = new Emittery<Events>()
+
+export const { activityEncountered } = activitiesSlice.actions
+export default activitiesSlice.reducer
