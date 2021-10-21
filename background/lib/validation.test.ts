83505c93e6d212a50ac4507cb393de3c13c896f0
Gergo Nagy <gergo.nagy@tally.cash>
2021-10-21 13:37:20 +0200

    simple validation test for jsonschema

--- background/lib/validation.test.ts
+++ background/lib/validation.test.ts
@@ -1,4 +1,4 @@
-import { jtdValidatorFor } from "./validation"
+import { jsonSchemaValidatorFor, jtdValidatorFor } from "./validation"
 
 describe("jtdValidatorFor", () => {
   it("the validation fn should pass for correct data", () => {
@@ -86,6 +86,103 @@ describe("jtdValidatorFor", () => {
 
     schemaDataError.forEach((data) => {
       const validatorFn = jtdValidatorFor(data.schema)
+      expect(validatorFn(data.data)).toBeFalsy()
+
+      if (!Array.isArray(validatorFn.errors)) return
+      expect(validatorFn.errors[0]).toEqual(data.error)
+    })
+  })
+})
+
+describe("jsonSchemaValidatorFor", () => {
+  it("the validation fn should pass for correct data", () => {
+    const schemaDataArr = [
+      {
+        schema: {
+          type: "object",
+          required: [],
+          additionalProperties: {
+            type: "object",
+            properties: {
+              last_updated_at: { type: "number" } as {
+                type: "number"
+                nullable: true
+              },
+            },
+            required: ["last_updated_at"],
+            additionalProperties: { type: "number", nullable: true },
+            nullable: true,
+          },
+        },
+        data: {
+          ethereum: {
+            usd: 3836.53,
+            eur: 3297.36,
+            cny: 24487,
+            last_updated_at: 1634672101,
+          },
+          bitcoin: {
+            usd: 63909,
+            eur: 54928,
+            cny: 407908,
+            last_updated_at: 1634672139,
+          },
+        },
+      },
+    ]
+
+    schemaDataArr.forEach((data) => {
+      // @ts-expect-error find out what's the problem with the argument typing
+      const validatorFn = jsonSchemaValidatorFor(data.schema)
+      expect(validatorFn(data.data)).toBeTruthy()
+    })
+  })
+  it("the validation fn should fail for incorrect data", () => {
+    const schemaDataError = [
+      {
+        schema: {
+          type: "object",
+          required: [],
+          additionalProperties: {
+            type: "object",
+            properties: {
+              last_updated_at: { type: "number" } as {
+                type: "number"
+                nullable: true
+              },
+            },
+            required: ["last_updated_at"] as never[],
+            additionalProperties: { type: "number", nullable: true },
+            nullable: true,
+          },
+        },
+        data: {
+          ethereum: {
+            usd: "3836.53", // wrong type
+            eur: 3297.36,
+            cny: 24487,
+            // last_updated_at: 1634672101, // missing required prop
+          },
+          bitcoin: {
+            usd: 63909,
+            eur: 54928,
+            cny: 407908,
+            last_updated_at: 1634672139,
+          },
+        },
+        error: {
+          instancePath: "/ethereum",
+          schemaPath: "#/additionalProperties/required",
+          keyword: "required",
+          params: { missingProperty: "last_updated_at" },
+          message: "must have required property 'last_updated_at'",
+        },
+      },
+    ]
+
+    schemaDataError.forEach((data) => {
+      // @ts-expect-error find out what's the problem with the argument typing
+      const validatorFn = jsonSchemaValidatorFor(data.schema)
       expect(validatorFn(data.data)).toBeFalsy()
 
       if (!Array.isArray(validatorFn.errors)) return
