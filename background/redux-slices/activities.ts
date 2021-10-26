c34897ef7c0326e2d5123520b427a61e4a8e5af2
Henry Boldizsar <henrymb67@gmail.com>
2021-10-25 22:14:53 -0500

    Handle more than "from", KeyRenameAndPickMap in slice
    And remove extra nesting in ActivitiesState

--- background/redux-slices/activities.ts
+++ background/redux-slices/activities.ts
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 import Emittery from "emittery"
+import dayjs from "dayjs"
+import { convertToEth } from "../lib/utils"
 import { AnyEVMTransaction } from "../types"
 
 export type ActivityItem = AnyEVMTransaction & {
@@ -7,35 +9,114 @@ export type ActivityItem = AnyEVMTransaction & {
   value: bigint
   from?: string
   isSent?: boolean
-}
-
-export type ActivitiesState = {
-  activities: {
-    [address: string]: {
-      [hash: string]: ActivityItem
+  detailRows: {
+    [name: string]: {
+      label: string
+      value: any
+      valueDetail: string
     }
   }
 }
 
-export const initialState: ActivitiesState = {
-  activities: {},
+export type ActivitiesState = {
+  [address: string]: {
+    [hash: string]: ActivityItem
+  }
+}
+
+export const initialState: ActivitiesState = {}
+
+type KeyRenameAndPickMap<T> = {
+  [P in keyof T]?: {
+    readableName: string
+    transformer: (value: T[P]) => string
+    detailTransformer: (value: T[P]) => string
+  }
+}
+
+const renameAndPickKeys = <T extends unknown>(
+  keysMap: KeyRenameAndPickMap<T>,
+  item: T
+) =>
+  // The as below is dicey but reasonable in our usage.
+  Object.keys(item).reduce((previousValue, key) => {
+    if (key in keysMap) {
+      const knownKey = key as keyof KeyRenameAndPickMap<T> // guaranteed to be true by the `in` test
+      return {
+        ...previousValue,
+        ...{
+          [keysMap[knownKey].readableName]: keysMap[knownKey].transformer(
+            item[knownKey]
+          ),
+        },
+      }
+    }
+    return previousValue
+  }, {})
+
+function ethTransformer(value: string | number | bigint) {
+  return `${convertToEth(value)} ETH`
+}
+
+const keysMap: KeyRenameAndPickMap<ActivityItem> = {
+  blockHeight: {
+    readableName: "Block Height",
+    transformer: (item) => item.toString(),
+    detailTransformer: () => {
+      return ""
+    },
+  },
+  value: {
+    readableName: "Amount",
+    transformer: ethTransformer,
+    detailTransformer: ethTransformer,
+  },
+  gas: {
+    readableName: "Gas",
+    transformer: ethTransformer,
+    detailTransformer: ethTransformer,
+  },
+  maxFeePerGas: {
+    readableName: "Max Fee/Gas",
+    transformer: ethTransformer,
+    detailTransformer: ethTransformer,
+  },
+  gasPrice: {
+    readableName: "Gas Price",
+    transformer: ethTransformer,
+    detailTransformer: ethTransformer,
+  },
+  timestamp: {
+    readableName: "Timestamp",
+    transformer: (item) => {
+      return dayjs.unix(item).format("MM/DD/YYYY hh:mm a")
+    },
+    detailTransformer: () => {
+      return ""
+    },
+  },
 }
 
 const activitiesSlice = createSlice({
   name: "activities",
   initialState,
   reducers: {
-    activityEncountered: (immerState, { payload: activityItem }) => {
-      const from = activityItem.from.toLowerCase()
+    activityEncountered: (immerState, { payload }) => {
+      const activityItem = payload.tx
+      const detailRows = renameAndPickKeys(keysMap, activityItem)
 
-      // TODO handle more than "from", and confirm it's an
-      // added account first
-      if (!immerState.activities[from]) {
-        immerState.activities[from] = {}
-      }
+      payload.forAccounts.forEach((account: string) => {
+        const address = account.toLowerCase()
 
-      // TODO Move KeyRenameAndPickMap here
-      immerState.activities[from][activityItem.hash] = activityItem
+        if (!immerState[address]) {
+          immerState[address] = {}
+        }
+
+        immerState[address][activityItem.hash] = {
+          ...activityItem,
+          detailRows,
+        }
+      })
     },
   },
 })
