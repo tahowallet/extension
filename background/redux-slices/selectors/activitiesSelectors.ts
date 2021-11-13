775b474420022515f7e41b8bc3dd3dad0de61abc
Henry Boldizsar <henrymb67@gmail.com>
2021-11-12 18:48:47 -0600

    Create activities selector in new selector directory
    Helps to avoid a dependency cycle when including state types from multiple slices

---
+++ background/redux-slices/selectors/activitiesSelectors.ts
@@ -0,0 +1,42 @@
+import { createSelector, EntityId } from "@reduxjs/toolkit"
+import { AccountState } from "../accounts"
+import { UIState } from "../ui"
+import { ActivitiesState, ActivityItem } from "../activities"
+
+export const selectAcrossActivityRelevantStates = createSelector(
+  (state: {
+    ui: UIState
+    activities: ActivitiesState
+    account: AccountState
+  }) => {
+    return {
+      activities: state.activities,
+      ui: state.ui,
+      account: state.account,
+    }
+  },
+  (relevantState) => relevantState
+)
+
+export const selectSelectedAccountActivitiesWithTimestamps = createSelector(
+  selectAcrossActivityRelevantStates,
+  ({ activities, ui, account }) => {
+    const activitiesNew = activities[ui.selectedAccount?.address]
+    return {
+      activities: activitiesNew?.ids?.map(
+        (id: EntityId): ActivityItem | undefined => {
+          const activityItem = activitiesNew.entities[id]
+          if (activityItem) {
+            return {
+              ...activityItem,
+              timestamp:
+                activityItem?.blockHeight &&
+                account.blocks[activityItem?.blockHeight]?.timestamp,
+            }
+          }
+          return undefined
+        }
+      ),
+    }
+  }
+)
