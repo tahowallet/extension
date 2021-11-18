7dbcd969e97547f19199865f5ca7ec76b07d9561
Wojciech Turek <wojciech.turek@tally.cash>
2021-11-18 14:41:19 +0100

    Add types for the abi

--- background/redux-slices/utils/contract-utils.ts
+++ background/redux-slices/utils/contract-utils.ts
@@ -2,6 +2,24 @@ import { Provider, AlchemyProvider, getNetwork } from "@ethersproject/providers"
 import { Contract, ethers } from "ethers"
 import { getEthereumNetwork } from "../../lib/utils"
 
+type ContractInterface = ContractElement[]
+
+interface ContractElement {
+  inputs: InputOutputData[]
+  type: string
+  stateMutability?: string
+  name?: string
+  anonymous?: boolean
+  outputs?: InputOutputData[]
+}
+
+interface InputOutputData {
+  internalType: string
+  name: string
+  type: string
+  indexed?: boolean
+}
+
 export function getProvider(): Provider {
   const provider = new AlchemyProvider(
     getNetwork(Number(getEthereumNetwork().chainID)),
@@ -12,7 +30,7 @@ export function getProvider(): Provider {
 
 export const getContract = async (
   address: string,
-  abi: any[]
+  abi: ContractInterface
 ): Promise<Contract> => {
   const provider = getProvider()
   return new ethers.Contract(address, abi, provider)
