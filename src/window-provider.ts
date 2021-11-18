3ce00b8e07bc04bc3e152e0034e99a26963b8d1a
Gergo Nagy <gergo.nagy@tally.cash>
2021-11-18 15:53:49 +0100

    add window-provider updates

--- src/window-provider.ts
+++ src/window-provider.ts
@@ -1,4 +1,5 @@
-// For design considerations see https://github.com/tallycash/tally-extension/blob/main/docs/inpage.md
+// @ts-nocheck
+import EventEmitter from "events"
 
 // it's better to have our own copy of these functions so nobody
 // can temper w / them in any way we would not want to
@@ -7,42 +8,99 @@
 // so it's not 100% safe but safe enough (for 100% iframe trick is necessary)
 const unsafePostMessage = window.postMessage
 const unsafeAddEventListener = window.addEventListener
-const unsafeOrigin = window.location.origin
+const unsafeRemoveEventListener = window.removeEventListener
 
-// to demonstrate how it works it was necessary. Will remove later
-// eslint-disable-next-line no-console
-console.log("inpage.js in da house", Date.now())
+console.log("inpage.js in da house")
+
+export class InpageEip1193Bridge extends EventEmitter {
+  chainId: number | undefined = 1 // TODO: get this from bg
+  selectedAddress: string | undefined
+  isConnected = false
+  isTally = true
+  isMetaMask = true
+  listeners = new Map()
+
+  async request(request: {
+    method: string
+    params?: Array<any>
+  }): Promise<any> {
+    return this.send(request.method, request.params || [])
+  }
+
+  async send(method: string, params?: Array<any>): Promise<any> {
+    const sendData = {
+      id: Date.now().toString(),
+      target: "tally-provider-bridge",
+      payload: {
+        method,
+        params,
+      },
+    }
+
+    console.log("---")
+    console.log("inpage: ", JSON.stringify(sendData))
+
+    // ‼️ Always include target origin to avoid unwanted attention
+    unsafePostMessage(sendData, window.location.origin)
+
+    return new Promise((resolve) => {
+      function listener(event: {
+        origin: string
+        source: unknown
+        data: { target: string; payload: { result: unknown } }
+      }) {
+        if (
+          event.origin !== window.location.origin || // we want to recieve msgs only from the provider-bridge script
+          event.source !== window || // we want to recieve msgs only from the provider-bridge script
+          event.data.target !== "tally-window-provider"
+        )
+          return
+
+        if (sendData.id !== event.data.id) return
+
+        unsafeRemoveEventListener(
+          "message",
+          this.listeners.get(sendData.id),
+          false
+        )
+        this.listeners.delete(sendData.id)
+
+        const { method, params } = sendData.payload
+        const { result } = event.data.payload
+        console.log("inpage: ", JSON.stringify(event.data))
+        if (method === "eth_chainId") {
+          if (!this.isConnected) {
+            this.isConnected = true
+            this.emit("connect", { chainId: result })
+          }
+
+          if (this.chainId !== result) {
+            this.chainId = result
+            this.emit("chainChanged", result)
+            this.emit("networkChanged", result)
+          }
+        }
+
+        if (method === "eth_accounts") {
+          if (this.selectedAddress !== result[0]) {
+            this.selectedAddress = result[0]
+            this.emit("accountsChanged", [this.selectedAddress])
+          }
+        }
+
+        resolve(result)
+        console.log(this.listeners.size)
+        return
+      }
+
+      this.listeners.set(sendData.id, listener.bind(this))
+      unsafeAddEventListener("message", this.listeners.get(sendData.id))
+    })
+  }
+}
 
 const WINDOW_PROVIDER_FLAG = "isTallyWindowProviderEnabled"
 
-const enabled = window.localStorage.getItem(WINDOW_PROVIDER_FLAG)
-
 if (enabled === "true") {
-  setInterval(() => {
-    // ‼️ Always include target origin to avoid unwanted attention
-    unsafePostMessage(
-      {
-        target: "tally-provider-bridge",
-        message: "SYN",
-      },
-      unsafeOrigin
-    )
-
-    // to demonstrate how it works it was necessary. Will remove later
-    // eslint-disable-next-line no-console
-    console.log("-------------")
-  }, 1000)
-
-  unsafeAddEventListener("message", (event) => {
-    if (
-      event.origin !== unsafeOrigin || // we want to recieve msgs only from the provider bridge
-      event.source !== window || // we want to recieve msgs only from the provider bridge
-      event.data.target !== "tally-window-provider"
-    )
-      return
-
-    // to demonstrate how it works it was necessary. Will remove later
-    // eslint-disable-next-line no-console
-    console.log("inpage: ", event.data)
-  })
+  ;(window as any).ethereum = new InpageEip1193Bridge()
 }
