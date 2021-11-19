bcd88aec10b1d3c250766afec7be04cf23916fb3
Henry Boldizsar <henrymb67@gmail.com>
2021-11-19 17:59:26 -0600

    Add transaction data interpretation helper
    In a new utils folder in UI

---
+++ ui/utils/transactionInputInterpretionHelper.ts
@@ -0,0 +1,20 @@
+import { ActivityItem } from "@tallyho/tally-background/redux-slices/activities"
+
+export function determineActivityValue(activityItem: ActivityItem) {
+  const { asset } = activityItem
+  let { value } = activityItem
+  // Derive value from transaction transfer input if not ETH
+  if (asset && asset.symbol !== "ETH" && activityItem?.input) {
+    const isSwap =
+      activityItem.input.includes("0x38") || activityItem.input.includes("0x18")
+
+    if (!isSwap) {
+      value = BigInt(parseInt(activityItem.input.slice(-64), 16))
+    } else {
+      value = BigInt(parseInt(activityItem.input.slice(10).slice(0, 64), 16))
+    }
+  }
+  return value
+}
+
+export default determineActivityValue
