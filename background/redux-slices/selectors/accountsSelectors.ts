11959954484108f5a7dfaa196fe7b1a29bf1452f
Antonio Salazar Cardozo <antonio@thesis.co>
2021-12-02 21:23:10 -0500

    Introduce selectCurrentAccountBalances selector
    This selector allows for the selection of just the currently selected
    account's balances, both with a total in the main currency value and per
    asset owned by that particular account.

--- background/redux-slices/selectors/accountsSelectors.ts
+++ background/redux-slices/selectors/accountsSelectors.ts
@@ -22,6 +22,9 @@ const mainCurrencySymbol = "USD"
 const userValueDustThreshold = 2
 
 const getAccountState = (state: RootState) => state.account
+const getCurrentAccountState = (state: RootState) => {
+  return state.account.accountsData[state.ui.selectedAccount.address]
+}
 const getAssetsState = (state: RootState) => state.assets
 
 export const selectAccountAndTimestampedActivities = createSelector(
@@ -103,6 +106,73 @@ export const selectAccountAndTimestampedActivities = createSelector(
       },
       accountData: account.accountsData,
       activity,
+    }
+  }
+)
+
+export const selectCurrentAccountBalances = createSelector(
+  getCurrentAccountState,
+  getAssetsState,
+  selectHideDust,
+  (currentAccount, assets, hideDust) => {
+    if (currentAccount === "loading") {
+      return undefined
+    }
+
+    // Keep a tally of the total user value; undefined if no main currency data
+    // is available.
+    let totalMainCurrencyAmount: number | undefined
+
+    // Derive account "assets"/assetAmount which include USD values using
+    // data from the assets slice
+    const accountAssetAmounts = Object.values(currentAccount.balances)
+      .map<CompleteAssetAmount>(({ assetAmount }) => {
+        const assetPricePoint = selectAssetPricePoint(
+          assets,
+          assetAmount.asset.symbol,
+          mainCurrencySymbol
+        )
+
+        if (assetPricePoint) {
+          const enrichedAssetAmount = enrichAssetAmountWithDecimalValues(
+            enrichAssetAmountWithMainCurrencyValues(
+              assetAmount,
+              assetPricePoint,
+              desiredDecimals
+            ),
+            desiredDecimals
+          )
+
+          if (typeof enrichedAssetAmount.mainCurrencyAmount !== "undefined") {
+            totalMainCurrencyAmount ??= 0 // initialize if needed
+            totalMainCurrencyAmount += enrichedAssetAmount.mainCurrencyAmount
+          }
+
+          return enrichedAssetAmount
+        }
+
+        return enrichAssetAmountWithDecimalValues(assetAmount, desiredDecimals)
+      })
+      .filter((assetAmount) => {
+        const isNotDust =
+          typeof assetAmount.mainCurrencyAmount === "undefined"
+            ? true
+            : assetAmount.mainCurrencyAmount > userValueDustThreshold
+        const isPresent = assetAmount.decimalAmount > 0
+
+        // Hide dust and missing amounts.
+        return hideDust ? isNotDust && isPresent : isPresent
+      })
+
+    return {
+      assetAmounts: accountAssetAmounts,
+      totalMainCurrencyValue: totalMainCurrencyAmount
+        ? formatCurrencyAmount(
+            mainCurrencySymbol,
+            totalMainCurrencyAmount,
+            desiredDecimals
+          )
+        : undefined,
     }
   }
 )
