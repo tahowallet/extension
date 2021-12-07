4f3b48cc2676e3851e04c339c2eee502a0e8e2c3
Antonio Salazar Cardozo <antonio@thesis.co>
2021-12-06 23:11:14 -0500

    Add and use selectIsCurrentAccountSigner and use
    The current use is to only show the Send/Receive/Swap buttons if the
    current account can sign transactions. It is additionally guarded by the
    HIDE_SEND_BUTTON env flag where applicable.

---
+++ background/redux-slices/selectors/keyringsSelectors.ts
@@ -0,0 +1,9 @@
+import { createSelector } from "@reduxjs/toolkit"
+import { RootState } from ".."
+
+export const selectIsCurrentAccountSigner = createSelector(
+  (state: RootState) =>
+    state.keyrings.keyrings.flatMap((keyring) => keyring.addresses),
+  (state: RootState) => state.ui.selectedAccount,
+  (addresses, selectedAccount) => addresses.includes(selectedAccount.address)
+)
