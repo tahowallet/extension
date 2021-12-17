fc939066cd470dbed22bcf1b2705c97d4e1a57d2
Henry Boldizsar <henrymb67@gmail.com>
2021-12-17 08:46:35 -0600

    Revert "Fix sign transaction reject button not dismissing window"
    This reverts commit ba2a080a08d6be01b14622dbd3ac8c72b83e2509.

--- ui/pages/SignTransaction.tsx
+++ ui/pages/SignTransaction.tsx
@@ -219,7 +219,7 @@ export default function SignTransaction(): ReactElement {
           iconSize="large"
           size="large"
           type="secondary"
-          onClick={() => window.close()}
+          onClick={() => history.goBack()}
         >
           Reject
         </SharedButton>
