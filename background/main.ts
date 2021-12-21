ed7d315f09261096ecf6c1adb6434257b09a21cf
Matt Luongo <mhluongo@gmail.com>
2021-12-20 22:35:30 -0500

    Refactor blocks in Redux to a networks slice
    Storing them in the accounts slice never made sense. The status of a
    block on a particular network isn't account specific.
    
    The push for multi-network support means more state will need to be
    keyed by network. While we only support EVM-based chains, that means
    chain ID. More refactors will inevitably follow; for now, store the
    EVM network state in an EVM-specific area with chain ids under it.

--- background/main.ts
+++ background/main.ts
@@ -29,7 +29,6 @@ import { AddressOnNetwork, NameOnNetwork } from "./accounts"
 import rootReducer from "./redux-slices"
 import {
   loadAccount,
-  blockSeen,
   updateAccountBalance,
   updateENSName,
   updateENSAvatar,
@@ -43,6 +42,7 @@ import {
   updateKeyrings,
   setKeyringToVerify,
 } from "./redux-slices/keyrings"
+import { blockSeen } from "./redux-slices/networks"
 import {
   initializationLoadingTimeHitLimit,
   emitter as uiSliceEmitter,
@@ -101,7 +101,7 @@ const devToolsSanitizer = (input: unknown) => {
 
 // The version of persisted Redux state the extension is expecting. Any previous
 // state without this version, or with a lower version, ought to be migrated.
-const REDUX_STATE_VERSION = 3
+const REDUX_STATE_VERSION = 4
 
 type Migration = (prevState: Record<string, unknown>) => Record<string, unknown>
 
@@ -140,6 +140,54 @@ const REDUX_MIGRATIONS: { [version: number]: Migration } = {
     newState.assets = []
 
     return newState
+  },
+  4: (prevState: Record<string, unknown>) => {
+    // Migrate the ETH-only block data in store.accounts.blocks[blockHeight] to
+    // a new networks slice. Block data is now network-specific, keyed by EVM
+    // chainID in store.networks.networkData[chainId].blocks
+    type OldState = {
+      account?: {
+        blocks?: { [blockHeight: number]: unknown }
+      }
+    }
+    type NetworkState = {
+      evm: {
+        [chainID: string]: {
+          blockHeight: number | null
+          blocks: {
+            [blockHeight: number]: unknown
+          }
+        }
+      }
+    }
+
+    const oldState = prevState as OldState
+
+    const networks: NetworkState = {
+      evm: {
+        "1": {
+          blocks: { ...oldState.account?.blocks },
+          blockHeight:
+            Math.max(
+              ...Object.keys(oldState.account?.blocks ?? {}).map((s) =>
+                parseInt(s, 10)
+              )
+            ) || null,
+        },
+      },
+    }
+
+    const { blocks, ...oldStateAccountWithoutBlocks } = oldState.account ?? {
+      blocks: undefined,
+    }
+
+    return {
+      ...prevState,
+      // Drop blocks from account slice.
+      account: oldStateAccountWithoutBlocks,
+      // Add new networks slice data.
+      networks,
+    }
   },
 }
 
