36ec7416ebb06ee62c85f4eaf862b064ea8e975d
Henry Boldizsar <henrymb67@gmail.com>
2021-12-20 21:22:45 -0600

    Update unlock keyring page for new design

--- ui/components/Keyring/KeyringUnlock.tsx
+++ ui/components/Keyring/KeyringUnlock.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useEffect, useState } from "react"
 import { useHistory } from "react-router-dom"
 import { unlockKeyrings } from "@tallyho/tally-background/redux-slices/keyrings"
+import { rejectTransactionSignature } from "@tallyho/tally-background/redux-slices/transaction-construction"
 import { useBackgroundDispatch, useAreKeyringsUnlocked } from "../../hooks"
 import SharedButton from "../Shared/SharedButton"
 import SharedInput from "../Shared/SharedInput"
@@ -9,7 +10,10 @@ import titleStyle from "../Onboarding/titleStyle"
 export default function KeyringUnlock(): ReactElement {
   const [password, setPassword] = useState("")
   const [errorMessage, setErrorMessage] = useState("")
-  const history = useHistory()
+  const history: {
+    entries?: { pathname: string }[]
+    goBack: () => void
+  } = useHistory()
 
   const areKeyringsUnlocked = useAreKeyringsUnlocked(false)
 
@@ -30,12 +34,34 @@ export default function KeyringUnlock(): ReactElement {
     setErrorMessage("Incorrect password")
   }
 
+  const handleReject = async () => {
+    await dispatch(rejectTransactionSignature())
+  }
+
+  // TODO: replace this
+  const isPopup =
+    history?.entries &&
+    history.entries[0]?.pathname === "/" &&
+    history.entries[1]?.pathname === "/keyring/unlock"
+
   return (
     <section>
-      <div className="full_logo" />
+      {isPopup && (
+        <div className="back_button_wrap">
+          <SharedButton
+            type="tertiaryWhite"
+            size="small"
+            onClick={handleReject}
+          >
+            Cancel tx
+          </SharedButton>
+        </div>
+      )}
+      <div className="illustration_unlock" />
       <h1 className="serif_header">Unlock Your Wallet</h1>
       <div className="subtitle">
-        In order to continue, please unlock your wallet.
+        You locked your signing permissions or the session has timed out. In
+        order to continue, please unlock your wallet
       </div>
       <form onSubmit={dispatchUnlockWallet}>
         <div className="input_wrap">
@@ -51,22 +77,34 @@ export default function KeyringUnlock(): ReactElement {
           />
         </div>
         <SharedButton type="primary" size="large" isFormSubmit>
-          Unlock and Continue
+          Unlock
         </SharedButton>
       </form>
       <style jsx>
         {`
           ${titleStyle}
-          .full_logo {
-            background: url("./images/full_logo@2x.png");
+          .illustration_unlock {
+            background: url("./images/illustration_unlock@2x.png");
             background-size: cover;
-            width: 118px;
-            height: 120px;
+            width: 227.86px;
+            height: 214.21px;
             margin-bottom: 17px;
           }
           .input_wrap {
             width: 211px;
             margin-bottom: 30px;
+          }
+          .subtitle {
+            width: 321px;
+            text-align: center;
+          }
+          .back_button_wrap {
+            margin-top: -50px;
+            width: 100%;
+            display: flex;
+            justify-content: flex-end;
+            margin-bottom: -5px;
+            opacity: 0.7;
           }
         `}
       </style>
