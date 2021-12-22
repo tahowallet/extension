f0b816a11e6ea1939a384f3c638c99e6db3b3a5c
Henry Boldizsar <henrymb67@gmail.com>
2021-12-22 01:08:45 -0600

    Add fancy shared input placeholder/label animation

--- ui/components/Shared/SharedInput.tsx
+++ ui/components/Shared/SharedInput.tsx
@@ -18,13 +18,14 @@ export default function SharedInput(props: Props): ReactElement {
       <input
         id={id}
         type={type}
-        placeholder={placeholder}
+        placeholder=" "
         value={value}
         onChange={(event) => onChange?.(event.target.value)}
         className={classNames({
           error: errorMessage,
         })}
       />
+      <label htmlFor={id}>{placeholder}</label>
       {errorMessage && <div className="error_message">{errorMessage}</div>}
       <style jsx>
         {`
@@ -56,6 +57,30 @@ export default function SharedInput(props: Props): ReactElement {
             font-size: 14px;
             line-height: 20px;
             margin-top: 3px;
+          }
+          label {
+            position: absolute;
+            pointer-events: none;
+            display: flex;
+            width: fit-content;
+            margin-left: 16px;
+            transform: translateY(-32px);
+            background-color: var(--hunter-green);
+            border-radius: 5px;
+            box-sizing: border-box;
+            color: var(--green-40);
+            transition: font-size 0.2s ease, transform 0.2s ease,
+              font-weight 0.2s ease, padding 0.2s ease;
+          }
+          input:focus ~ label,
+          input:not(:placeholder-shown) ~ label {
+            transform: translateY(-57px) translateX(-5px);
+            font-size: 12px;
+            font-weight: 500;
+            padding: 0px 6px;
+          }
+          .error ~ label {
+            color: var(--error);
           }
         `}
       </style>
