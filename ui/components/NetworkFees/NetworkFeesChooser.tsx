24f5580a37dab5e7025fe301953909e765bedf39
Henry Boldizsar <henrymb67@gmail.com>
2021-12-22 12:37:08 -0600

    Update `NetworkFeesChooser`'s gas limit input usage
    It previously emulated part of what `SharedInput` can achieve now

--- ui/components/NetworkFees/NetworkFeesChooser.tsx
+++ ui/components/NetworkFees/NetworkFeesChooser.tsx
@@ -243,15 +243,14 @@ export default function NetworkFeesChooser({
             })}
             <div className="info">
               <div className="limit">
-                <label className="limit_label" htmlFor="gasLimit">
-                  Gas limit
-                </label>
                 <SharedInput
                   id="gasLimit"
                   value={gasLimit}
                   onChange={(val) => setGasLimit(val)}
+                  defaultValue="21000"
                   label="Gas limit"
                   type="number"
+                  focusedLabelBackgroundColor="var(--green-95)"
                 />
               </div>
               <div className="max_fee">
@@ -327,15 +326,6 @@ export default function NetworkFeesChooser({
             margin: 16px 0;
             width: 40%;
             position: relative;
-          }
-          .limit_label {
-            position: absolute;
-            top: -8px;
-            left: 10px;
-            font-size: 12px;
-            padding: 0 4px;
-            background-color: var(--green-95);
-            color: var(--green-40);
           }
           .title {
             font-size: 22px;
