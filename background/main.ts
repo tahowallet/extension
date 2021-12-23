a0e641cb64f214c568f4e977b190c9ce52ddf673
Matt Luongo <mhluongo@gmail.com>
2021-12-23 17:31:13 -0500

    Migrate Redux state from v1 to v2
    This migration handles the refactor and rename of
    state.ui.currentAccount from a custom `SelectedAccount` type to
    state.ui.selectedAccount, a bare `AddressNetwork`.
    
    As this is our first migration of this kind, I've tried to establish
    some best practices around handling types in a way that's
    forward-compatible. If we can get consensus around this migration
    mechanism, it should be extracted from the Main service into its own
    module.

--- background/main.ts
+++ background/main.ts
@@ -81,15 +81,62 @@ const devToolsSanitizer = (input: unknown) => {
 
 // The version of persisted Redux state the extension is expecting. Any previous
 // state without this version, or with a lower version, ought to be migrated.
-const REDUX_STATE_VERSION = 1
+const REDUX_STATE_VERSION = 2
+
+type Migration = (prevState: Record<string, unknown>) => Record<string, unknown>
+
+// An object mapping a version number to a state migration. Each migration for
+// version n is expected to take a state consistent with version n-1, and return
+// state consistent with version n.
+const REDUX_MIGRATIONS: { [version: number]: Migration } = {
+  2: (prevState: Record<string, unknown>) => {
+    // Migrate the old currentAccount SelectedAccount type to a bare
+    // selectedAccount AddressNetwork type. Note the avoidance of imported types
+    // so this migration will work in the future, regardless of other code changes
+    type BroadAddressNetwork = {
+      address: string
+      network: Record<string, unknown>
+    }
+    type OldState = {
+      ui: {
+        currentAccount?: {
+          addressNetwork: BroadAddressNetwork
+          truncatedAddress: string
+        }
+      }
+    }
+    const newState = { ...prevState }
+    const addressNetwork = (prevState as OldState)?.ui?.currentAccount
+      ?.addressNetwork
+    delete (newState as OldState)?.ui?.currentAccount
+    newState.selectedAccount = addressNetwork as BroadAddressNetwork
+    return newState
+  },
+}
 
 // Migrate a previous version of the Redux state to that expected by the current
 // code base.
 function migrateReduxState(
   previousState: Record<string, unknown>,
-  version?: number
+  previousVersion?: number
 ): Record<string, unknown> {
-  return previousState
+  const resolvedVersion = previousVersion ?? 1
+  let migratedState: Record<string, unknown> = previousState
+
+  if (resolvedVersion < REDUX_STATE_VERSION) {
+    const outstandingMigrations = Object.entries(REDUX_MIGRATIONS)
+      .sort()
+      .filter(([version]) => parseInt(version, 10) > resolvedVersion)
+      .map(([, migration]) => migration)
+    migratedState = outstandingMigrations.reduce(
+      (state: Record<string, unknown>, migration: Migration) => {
+        return migration(state)
+      },
+      migratedState
+    )
+  }
+
+  return migratedState
 }
 
 const reduxCache: Middleware = (store) => (next) => (action) => {
