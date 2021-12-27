cac9208b1477fc598607661c497e3038b6ee78f2
Henry Boldizsar <henrymb67@gmail.com>
2021-12-27 00:52:30 -0600

    Update tab bar w/ new animation & config
    Moving the tabs array outside of this component helps organization for use in other contexts

--- ui/components/TabBar/TabBarIcon.tsx
+++ ui/components/TabBar/TabBarIcon.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react"
 import classNames from "classnames"
-import { Link } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 
 interface Props {
   name: string
@@ -8,28 +8,72 @@ interface Props {
 }
 
 export default function TabBarIcon(props: Props): ReactElement {
+  const history = useHistory()
+
   const { name, isActive } = props
 
   return (
     <>
-      <Link to={`/${name}`}>
-        <div className={classNames("icon", { active: isActive })} />
-      </Link>
+      <button
+        type="button"
+        onClick={() => {
+          history.push(`/${name}`)
+        }}
+      >
+        <div className={classNames("tab_bar_icon_wrap", { active: isActive })}>
+          <div className={classNames("icon")} />
+          <span>{name}</span>
+        </div>
+      </button>
       <style jsx>
         {`
+          .tab_bar_icon_wrap {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            justify-items: center;
+            transform: translateY(0px);
+            transition: 0.3s ease;
+          }
           .icon {
             mask-image: url("./images/${name}.svg");
             mask-size: 24px 24px;
             width: 24px;
             height: 24px;
             cursor: pointer;
-            background-color: var(--green-60);
+            background-color: var(--green-40);
+            transition: 0.3s ease;
           }
           .icon:not(.active):hover {
             background-color: var(--green-20);
           }
-          .active {
+          span {
+            position: absolute;
+            opacity: 0;
+            text-align: center;
+            margin-bottom: -39px;
+            color: var(--green-40);
+            font-size: 12px;
+            font-weight: 600;
+            text-transform: capitalize;
+            transition: 0.3s ease;
+          }
+          .active .icon {
             background-color: var(--trophy-gold);
+            opacity: 1;
+          }
+          .tab_bar_icon_wrap:hover span,
+          .active span {
+            opacity: 1;
+          }
+
+          .tab_bar_icon_wrap:hover,
+          .active {
+            transform: translateY(-8px);
+          }
+          .active span {
+            color: #d08e39;
           }
         `}
       </style>
