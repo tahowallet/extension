a05cf1c81bb858269b3a36f49912099ff2c734e6
Henry Boldizsar <henrymb67@gmail.com>
2022-01-05 02:10:59 -0600

    Update add wallet page to also present creating a seed

--- ui/pages/Onboarding/OnboardingAddWallet.tsx
+++ ui/pages/Onboarding/OnboardingAddWallet.tsx
@@ -1,141 +1,143 @@
-import { HIDE_ADD_SEED } from "@tallyho/tally-background/features/features"
 import React, { ReactElement } from "react"
+import { HIDE_ADD_SEED } from "@tallyho/tally-background/features/features"
+import { generateNewKeyring } from "@tallyho/tally-background/redux-slices/keyrings"
+import { useBackgroundDispatch, useAreKeyringsUnlocked } from "../../hooks"
 import SharedBackButton from "../../components/Shared/SharedBackButton"
 import SharedButton from "../../components/Shared/SharedButton"
 
-export default function OnboardingAddWallet(): ReactElement {
+export default function OnboardingStartTheHunt(): ReactElement {
+  const dispatch = useBackgroundDispatch()
+  useAreKeyringsUnlocked(true)
+
   return (
-    <>
-      <section className="standard_width">
-        <div className="top">
-          <SharedBackButton />
-          <div className="wordmark" />
-        </div>
-        <div className="primary_wrap">
-          <div className="choice_wrap">
-            <h1 className="serif_header">Explore Tally</h1>
-            <div className="subtitle">
-              Use one of your existing addresses or ENS domain to explore Tally.
-              You will not be able to sign transactions until you import a seed.
-            </div>
+    <section className="start_wrap">
+      <div className="top">
+        <SharedBackButton />
+        <div className="wordmark" />
+      </div>
+      <h1 className="serif_header">Add Wallet</h1>
+      <div className="subtitle subtitle_hunt">
+        Let&apos;s set Tally up with a wallet. Select with what wallet you would
+        like to continue.
+      </div>
+      <ul>
+        <li className="label_small">Use an existing wallet</li>
+        <li className="option standard_width">
+          <div className="icon preview_icon" />
+          <SharedButton
+            type="tertiary"
+            size="medium"
+            linkTo="/onboarding/viewOnlyWallet"
+          >
+            Preview an address
+          </SharedButton>
+        </li>
+        {HIDE_ADD_SEED ? (
+          <></>
+        ) : (
+          <li className="option standard_width">
+            <div className="icon metamask_icon" />
             <SharedButton
-              type="primary"
+              type="tertiary"
               size="medium"
-              linkTo="/onboarding/viewOnlyWallet"
+              linkTo="/onboarding/importMetamask"
             >
-              Add address or ENS domain
+              Import MetaMask
             </SharedButton>
-          </div>
-          <div className="or_divider">or</div>
-          <div className="choice_wrap">
-            <h1 className="serif_header">Import seed</h1>
-            <div className="subtitle">
-              Use an existing MetaMask seed; other web3 wallets coming soon!
-            </div>
-            <SharedButton
-              type="primary"
-              linkTo={HIDE_ADD_SEED ? undefined : "/onboarding/importMetamask"}
-              isDisabled={HIDE_ADD_SEED}
-              size="medium"
-              onClick={() => {
-                if (HIDE_ADD_SEED) {
-                  alert("This feature is temporarily disabled.")
-                }
-              }}
-            >
-              Add account
-            </SharedButton>
-          </div>
-        </div>
-        <div className="forest" />
-        <style jsx>
-          {`
-            .top {
-              display: flex;
-              width: 100%;
-            }
-            .wordmark {
-              background: url("./images/wordmark@2x.png");
-              background-size: cover;
-              width: 52px;
-              height: 25px;
-              position: absolute;
-              left: 0px;
-              right: 0px;
-              margin: 0 auto;
-            }
-            section {
-              background-color: var(--hunter-green);
-            }
-            .choice_wrap {
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-            }
-            .primary_wrap {
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              text-align: center;
-              margin-top: 32px;
-            }
-            .or_divider {
-              width: 100%;
-              position: relative;
-              color: var(--green-40);
-              margin-top: 32px;
-              margin-bottom: 24px;
-            }
-            .or_divider:after {
-              content: "";
-              width: calc(50% - 52px / 2);
-              height: 1px;
-              position: absolute;
-              top: 50%;
-              transform: translateY(-50%);
-              background: var(--green-120);
-              right: 0;
-            }
-            .or_divider:before {
-              content: "";
-              width: calc(50% - 52px / 2);
-              height: 1px;
-              position: absolute;
-              top: 50%;
-              transform: translateY(-50%);
-              background: var(--green-120);
-              left: 0;
-            }
-            section {
-              padding-top: 25px;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-            }
-            .subtitle {
-              color: var(--green-60);
-              width: 307px;
-              text-align: center;
-              line-height: 24px;
-              margin-bottom: 24px;
-            }
-            .forest {
-              background: url("./images/dark_forest@2x.png");
-              background-size: cover;
-              width: 384px;
-              height: 141px;
-              filter: brightness(0) saturate(100%) invert(6%) sepia(70%)
-                saturate(701%) hue-rotate(132deg) brightness(94%) contrast(103%);
-              position: absolute;
-              left: 0;
-              bottom: 0;
-              pointer-events: none;
-              z-index: -1;
-            }
-          `}
-        </style>
-      </section>
-    </>
+          </li>
+        )}
+        <li className="label_small">Start Fresh</li>
+        <li className="option standard_width">
+          <div className="icon tally_icon" />
+          <SharedButton
+            type="secondary"
+            size="medium"
+            linkTo="/onboarding/saveSeed"
+            onClick={() => {
+              dispatch(generateNewKeyring())
+            }}
+          >
+            Create new wallet
+          </SharedButton>
+        </li>
+      </ul>
+      <style jsx>
+        {`
+          section {
+            padding-top: 25px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: var(--hunter-green);
+          }
+          .top {
+            display: flex;
+            width: 100%;
+          }
+          .wordmark {
+            background: url("./images/wordmark@2x.png");
+            background-size: cover;
+            width: 52px;
+            height: 25px;
+            position: absolute;
+            left: 0px;
+            right: 0px;
+            margin: 0 auto;
+          }
+          h1 {
+            margin-top: 26px;
+          }
+          .subtitle {
+            color: var(--green-60);
+            margin-bottom: 32px;
+          }
+          .subtitle_hunt {
+            width: 307px;
+            text-align: center;
+            line-height: 24px;
+          }
+          .label_small {
+            margin-bottom: 16px;
+            display: block;
+            color: var(--green-40);
+            font-size: 16px;
+            font-weight: 400;
+            line-height: 24px;
+            display: flex;
+            align-items: center;
+          }
+          .option {
+            display: flex;
+            height: 64px;
+            border-radius: 16px;
+            background-color: var(--green-95);
+            align-items: center;
+            padding: 16px;
+            box-sizing: border-box;
+            margin-bottom: 24px;
+            justify-content: space-between;
+          }
+          .icon {
+            width: 36px;
+            height: 36px;
+            background-color: var(--gold-20);
+            border-radius: 50%;
+          }
+          .tally_icon {
+            background: url("./images/tally_circle_icon@2x.png");
+            background-size: cover;
+          }
+          .metamask_icon {
+            background: url("./images/metamask_icon@2x.png");
+            background-size: cover;
+          }
+          .preview_icon {
+            background: url("./images/preview_icon@2x.png");
+            background-size: cover;
+          }
+        `}
+      </style>
+    </section>
   )
 }
