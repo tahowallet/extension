45b92175b5f049cb4f910f770bbcc442f31ee939
Wojciech Turek <wojciech.turek@tally.cash>
2022-01-06 17:35:19 +0100

    Completely separate the FeeSettings Button
    Previously the fee settings button was relying on data that was
    coming from the NetworkSettingsChooser, currently it fetches
    the data on its own provides extra data apart from being
    used to open the network settings modal.

--- ui/components/NetworkFees/FeeSettingsButton.tsx
+++ ui/components/NetworkFees/FeeSettingsButton.tsx
@@ -1,18 +1,39 @@
-import React, { ReactElement } from "react"
+import React, { ReactElement, useEffect, useState } from "react"
+import {
+  selectEstimatedFeesPerGas,
+  selectFeeType,
+} from "@tallyho/tally-background/redux-slices/transaction-construction"
+import { ESTIMATED_FEE_MULTIPLIERS_BY_TYPE } from "@tallyho/tally-background/constants/networkFees"
+import { weiToGwei } from "@tallyho/tally-background/lib/utils"
+import { useBackgroundSelector } from "../../hooks"
 
 interface FeeSettingsButtonProps {
   openModal: () => void
-  currentFeeSelected: string
 }
 
 export default function FeeSettingsButton({
   openModal,
-  currentFeeSelected,
 }: FeeSettingsButtonProps): ReactElement {
+  const estimatedFeesPerGas = useBackgroundSelector(selectEstimatedFeesPerGas)
+  const selectedFeeType = useBackgroundSelector(selectFeeType)
+  const [estimatedGweiPrice, setEstimatedGweiPrice] = useState("")
+
+  useEffect(() => {
+    if (estimatedFeesPerGas !== undefined && selectedFeeType !== undefined) {
+      const estimatedGweiAmount = weiToGwei(
+        (estimatedFeesPerGas?.baseFeePerGas *
+          ESTIMATED_FEE_MULTIPLIERS_BY_TYPE[selectedFeeType.toString()]) /
+          10n
+      ).split(".")[0]
+
+      setEstimatedGweiPrice(estimatedGweiAmount)
+    }
+  }, [estimatedFeesPerGas, selectedFeeType])
+
   return (
     <button className="settings" type="button" onClick={openModal}>
       <div>
-        ~{currentFeeSelected}
+        ~{estimatedGweiPrice}
         Gwei
       </div>
       <img className="settings_image" src="./images/cog@2x.png" alt="" />
