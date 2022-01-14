aae80e2935de278abf0e17bfcb11910c5a4712ee
Henry Boldizsar <henrymb67@gmail.com>
2022-01-14 00:21:21 -0600

    Update claim footer button and steps

--- ui/components/Claim/ClaimFooter.tsx
+++ ui/components/Claim/ClaimFooter.tsx
@@ -1,4 +1,5 @@
 import React, { Dispatch, ReactElement, SetStateAction } from "react"
+import SharedButton from "../Shared/SharedButton"
 
 interface ClaimFooterProps {
   step: number
@@ -11,9 +12,9 @@ export default function ClaimFooter({
   setStep,
   advanceStep,
 }: ClaimFooterProps): ReactElement {
-  const buttonText = ["Next step", "Next step", "Next step", "Claim"]
+  const buttonText = ["Get started", "Continue", "Continue", "Claim"]
   return (
-    <div className="footer__wrap">
+    <footer>
       <div className="steps">
         {new Array(4).fill(null).map((el, index) => {
           return (
@@ -26,13 +27,12 @@ export default function ClaimFooter({
           )
         })}
       </div>
-      {step === 2 && <div className="skip">Skip</div>}
-      <button className="claim__button" type="button" onClick={advanceStep}>
+      <SharedButton type="primary" size="medium" onClick={advanceStep}>
         {buttonText[step - 1]}
-      </button>
+      </SharedButton>
       <style jsx>
         {`
-          .footer__wrap {
+          footer {
             position: relative;
             width: 352px;
             z-index: 2;
@@ -45,15 +45,10 @@ export default function ClaimFooter({
             display: flex;
             width: 100px;
           }
-          .skip {
-            color: #667c7a;
-            font-size: 14px;
-            cursor: pointer;
-          }
           .active {
             width: 16px;
             height: 6px;
-            background: #d08e39;
+            background: var(--trophy-gold);
             border-radius: 100px;
             transition: all 0.5s ease-out;
             margin: 0 2px;
@@ -61,28 +56,13 @@ export default function ClaimFooter({
           .inactive {
             width: 6px;
             height: 6px;
-            background: #667c7a;
+            background: var(--green-60);
             border-radius: 100px;
             transition: all 0.5s ease-in;
             margin: 0 2px;
           }
-
-          .claim__button {
-            height: 40px;
-            border-radius: 4px;
-            background-color: var(--trophy-gold);
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            color: #002522;
-            font-size: 20px;
-            letter-spacing: 0.48px;
-            line-height: 24px;
-            text-align: center;
-            padding: 0px 17px;
-          }
         `}
       </style>
-    </div>
+    </footer>
   )
 }
