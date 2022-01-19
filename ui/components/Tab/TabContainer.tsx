1179f4115bafdab65575c274857f78a9158bee4e
Massimo Cairo <cairomassimo@gmail.com>
2022-01-19 17:41:18 +0200

    Add container component for tab UI

---
+++ ui/components/Tab/TabContainer.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement } from "react"
+
+export default function TabContainer({
+  children,
+}: {
+  children: React.ReactNode
+}): ReactElement {
+  return (
+    <>
+      <div className="container">{children}</div>
+      <style jsx>{`
+        .container {
+          height: 100%;
+          background: no-repeat 2rem 2rem / auto
+              url("/images/logo_horizontal.svg"),
+            no-repeat bottom / cover url("/images/tab_background.svg"),
+            linear-gradient(to top, #10322f, var(--hunter-green) 100%);
+        }
+      `}</style>
+    </>
+  )
+}
