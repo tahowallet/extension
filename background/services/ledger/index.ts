4a1104087f48242466614796c911118d03cafc55
Balint Ferencz <balint.ferencz@tally.cash>
2022-01-21 10:04:31 +0100

    Implement deriveAddress happy path from RFC
    I made the following shortcuts:
      * The extension only have an Ethereum-app active Ledger paired
      * The custom ID of the Ledger is the address derived from a fixed path
    
    No error handling in this commit.

--- background/services/ledger/index.ts
+++ background/services/ledger/index.ts
@@ -1,4 +1,9 @@
 import { TypedDataDomain, TypedDataField } from "@ethersproject/abstract-signer"
+import Transport from "@ledgerhq/hw-transport"
+import TransportWebUSB from "@ledgerhq/hw-transport-webusb"
+import Eth from "@ledgerhq/hw-app-eth"
+import eip55 from "eip55"
+import { DeviceModelId } from "@ledgerhq/devices"
 import { EIP1559TransactionRequest, SignedEVMTransaction } from "../../networks"
 import { HexString } from "../../types"
 import BaseService from "../base"
@@ -34,12 +39,48 @@ type Events = ServiceLifecycleEvents & {
   signedTransaction: SignedEVMTransaction
 }
 
+const UnknownLedgerId = "unrecognizable"
+
+const idGeneratorPath = "44'/60'/0'/0/0"
+
 async function requireAvailableLedger() {
   const devs = await navigator.usb.getDevices()
 
   if (devs.length === 0) {
     throw new Error("No available USB devices to use!")
   }
+}
+
+async function deriveAddressOnLedger(path: string, eth: Eth) {
+  const a = await eth.getAddress(path)
+  const address = eip55.encode(a.address)
+  return address
+}
+
+async function generateLedgerId(
+  transport: Transport,
+  eth: Eth
+): Promise<[string, LedgerType]> {
+  let extensionDeviceType = LedgerType.UNKNOWN
+
+  switch (transport.deviceModel!.id) {
+    case DeviceModelId.nanoS:
+      extensionDeviceType = LedgerType.LEDGER_NANO_S
+      break
+    case DeviceModelId.nanoX:
+      extensionDeviceType = LedgerType.LEDGER_NANO_X
+      break
+    default:
+      extensionDeviceType = LedgerType.UNKNOWN
+  }
+
+  if (extensionDeviceType === LedgerType.UNKNOWN) {
+    return [UnknownLedgerId, extensionDeviceType]
+  }
+
+  const address = await deriveAddressOnLedger(idGeneratorPath, eth)
+
+  return [address, extensionDeviceType]
 }
 
 /**
@@ -71,9 +112,43 @@ export default class LedgerService extends BaseService<Events> {
   async deriveAddress(accountID: string): Promise<HexString> {
     requireAvailableLedger()
 
-    this.deriveAddress = this.deriveAddress.bind(this)
+    let transport
 
-    throw new Error("Unimplemented")
+    try {
+      transport = await TransportWebUSB.create()
+
+      const eth = new Eth(transport)
+
+      const [id, type] = await generateLedgerId(transport, eth)
+
+      const accountAddress = await deriveAddressOnLedger(accountID, eth)
+
+      const ethVersion = (await eth.getAppConfiguration()).version
+
+      this.emitter.emit("connected", { id, type })
+
+      this.emitter.emit("ledgerAdded", {
+        id,
+        type,
+        accountIDs: [idGeneratorPath, accountID],
+        metadata: { ethereumVersion: ethVersion },
+      })
+
+      this.emitter.emit("ledgerAccountAdded", {
+        id,
+        ledgerID: "",
+        derivationPath: accountID,
+        addresses: [accountAddress],
+      })
+
+      return accountAddress
+    } catch (err) {
+      // handle transport open error
+    } finally {
+      if (transport) transport.close()
+    }
+
+    throw new Error("Address derivation is unsuccessful!")
   }
 
   async signTransaction(
