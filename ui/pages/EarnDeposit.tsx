9457f3d1bfcb838d9245008c8cfa61a0c0e1f8cf
Wojciech Turek <wojciech.turek@tally.cash>
2022-01-25 15:41:09 -0500

    Update Earn UI

--- ui/pages/EarnDeposit.tsx
+++ ui/pages/EarnDeposit.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useState } from "react"
-import CorePage from "../components/Core/CorePage"
+import { Link } from "react-router-dom"
 import BackButton from "../components/Shared/SharedBackButton"
 import SharedAssetIcon from "../components/Shared/SharedAssetIcon"
 import SharedButton from "../components/Shared/SharedButton"
@@ -15,30 +15,45 @@ export default function EarnDeposit(): ReactElement {
         <BackButton />
         <header>
           <div className="left side_column">
-            <div className="side_title">Reward</div>
-            <span className="reward_wrap">
-              <div className="icon_reward" />
-              TALLY
-            </span>
+            <div className="type">VAULT</div>
+            <div className="content">
+              <div>Estimated APR</div>
+              <div>Total value locked</div>
+              <div>Rewards</div>
+            </div>
           </div>
           <div className="center">
             <SharedAssetIcon size="large" />
             <span className="asset_name">USDT</span>
           </div>
           <div className="right side_column">
-            <div className="side_title">est. APR</div>
-            250%
+            <div>
+              <Link to="www.onet.pl" target="_blank">
+                <div className="contract">
+                  <div className="name">Contract</div>
+                  <span className="external" />
+                </div>
+              </Link>
+            </div>
+            <div className="content data">
+              <div>250%</div>
+              <div>$20,283,219</div>
+              <div className="rewards">
+                <img className="lock" src="./images/lock@2.png" alt="Locked" />
+                TALLY
+              </div>
+            </div>
           </div>
         </header>
       </section>
       <SharedPanelSwitcher
         setPanelNumber={setPanelNumber}
         panelNumber={panelNumber}
-        panelNames={["Deposit", "Withdraw", "Risk"]}
+        panelNames={["Deposit", "Withdraw", "Pool Info"]}
       />
       <div className="deposit_wrap">
         <SharedAssetInput
-          label="Deposit amount"
+          label="Deposit asset"
           defaultAsset={{ symbol: "ETH", name: "Ether" }}
         />
         <SharedButton type="primary" size="large">
@@ -49,16 +64,52 @@ export default function EarnDeposit(): ReactElement {
         {`
           .primary_info {
             margin-top: 15px;
+            width: 90%;
+          }
+          .content {
+            padding-top: 48px;
+            font-size: 14px;
+            line-height: 18px;
+            display: flex;
+            flex-flow: column;
+            gap: 16px;
+            margin-bottom: 12px;
+            color: var(--green-40);
+          }
+          .content.data {
+            color: white;
+            align-items: flex-end;
+            font-size: 18px;
+          }
+          .contract {
+            display: flex;
+            align-items: center;
+            gap: 4px;
+            justify-content: flex-end;
+          }
+          .name {
+            text-decoration: none;
+            color: var(--green-40);
+            font-size: 16px;
+            font-weight: 400;
+            height: 17px;
+          }
+          .external {
+            mask-image: url("./images/external@2x.png");
+            mask-size: 12px 12px;
+            width: 12px;
+            height: 12px;
+            background-color: var(--green-40);
           }
           header {
             width: 100%;
-            height: 89px;
-            background-color: var(--green-95);
+            margin: 0 auto;
             display: flex;
             padding: 0px 16px;
             box-sizing: border-box;
             justify-content: space-between;
-            margin-bottom: 23px;
+            margin-bottom: 24px;
+            border: 1px solid #33514e;
           }
           .asset_name {
             color: #fff;
@@ -66,7 +117,7 @@ export default function EarnDeposit(): ReactElement {
             font-weight: 500;
             line-height: 32px;
             text-transform: uppercase;
-            nargin-top: 7px;
+            margin-top: 7px;
           }
           .center {
             display: flex;
@@ -78,18 +129,23 @@ export default function EarnDeposit(): ReactElement {
             right: 0px;
             pointer-events: none;
           }
-          .side_title {
-            margin-top: 16px;
+          .type {
             height: 17px;
-            color: var(--green-40);
-            font-size: 14px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #a4cfff;
+            background: #0b4789;
+            font-size: 12px;
+            padding: 0 4px;
             line-height: 16px;
+            max-width: 40px;
           }
           .side_column {
+            margin-top: 16px;
             color: #fff;
             font-size: 18px;
             font-weight: 600;
-            line-height: 24px;
           }
           .right {
             text-align: right;
@@ -102,17 +158,17 @@ export default function EarnDeposit(): ReactElement {
             align-items: center;
             justify-content: space-between;
           }
-          .reward_wrap {
-            display: flex;
-            align-content: flex-start;
+          .lock {
+            width: 16px;
+            display: inline-block;
           }
-          .icon_reward {
-            background: url("./images/tally_reward@2x.png");
-            background-size: cover;
-            width: 24px;
-            height: 24px;
-            margin-right: 8px;
-            margin-top: 2px;
+          .rewards {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 4px;
+            padding: 4px;
+            background-color: var(--green-120);
           }
         `}
       </style>
