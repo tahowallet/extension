2384b3966e881c634c5111132571be692327643e
Massimo Cairo <cairomassimo@gmail.com>
2022-01-26 15:21:11 +0200

    Refactor switching among different sign-type UIs
    Refactors as follows:
     * extracts common layout to `SignTransactionContainer` component,
     * uses a `switch` statement to instantiate the component,
     depending on SignType, from `SignTransaction`.
    
    This makes the code of `SignTransaction` shorter, cleaner, and prepares
    for type-specific code to be added more easily in the future.

--- ui/pages/SignTransaction.tsx
+++ ui/pages/SignTransaction.tsx
@@ -10,19 +10,15 @@ import {
 } from "@tallyho/tally-background/redux-slices/transaction-construction"
 import { getAccountTotal } from "@tallyho/tally-background/redux-slices/selectors"
 import { parseERC20Tx } from "@tallyho/tally-background/lib/erc20"
-import SharedButton from "../components/Shared/SharedButton"
-import SharedPanelSwitcher from "../components/Shared/SharedPanelSwitcher"
 import SignTransactionSwapAssetBlock from "../components/SignTransaction/SignTransactionSwapAssetBlock"
-import SignTransactionApproveSpendAssetBlock from "../components/SignTransaction/SignTransactionApproveSpendAssetBlock"
 import SignTransactionSignBlock from "../components/SignTransaction/SignTransactionSignBlock"
-import SignTransactionNetworkAccountInfoTopBar from "../components/SignTransaction/SignTransactionNetworkAccountInfoTopBar"
 import {
   useBackgroundDispatch,
   useBackgroundSelector,
   useAreKeyringsUnlocked,
 } from "../hooks"
 import SignTransactionTransferBlock from "../components/SignTransaction/SignTransactionTransferBlock"
-import SignTransactionDetailPanel from "../components/SignTransaction/SignTransactionDetailPanel"
+import SignTransactionContainer from "../components/SignTransaction/SignTransactionContainer"
 
 export enum SignType {
   Sign = "sign",
@@ -77,7 +73,6 @@ export default function SignTransaction({
     signerAccountTotal?.signingMethod?.type === "keyring"
   )
 
-  const [panelNumber, setPanelNumber] = useState(0)
   const [isTransactionSigning, setIsTransactionSigning] = useState(false)
 
   useEffect(() => {
@@ -117,49 +112,6 @@ export default function SignTransaction({
     return <></>
   }
 
-  const signContent: {
-    [signType in SignType]: {
-      title: string
-      component: () => ReactElement
-      confirmButtonText: string
-    }
-  } = {
-    [SignType.SignSwap]: {
-      title: "Swap assets",
-      component: () => <SignTransactionSwapAssetBlock />,
-      confirmButtonText: "Confirm",
-    },
-    [SignType.SignSpend]: {
-      title: "Approve asset spend",
-      component: () => (
-        <SignTransactionApproveSpendAssetBlock
-          transactionDetails={transactionDetails}
-          parsedTx={parsedTx}
-        />
-      ),
-      confirmButtonText: "Approve",
-    },
-    [SignType.SignTransfer]: {
-      title: "Sign Transfer",
-      component: () => (
-        <SignTransactionTransferBlock
-          token={assetSymbol ?? ""}
-          amount={amount ?? 0}
-          destination={to ?? ""}
-          localizedValue={value ?? ""}
-        />
-      ),
-      confirmButtonText: "Sign",
-    },
-    [SignType.Sign]: {
-      title: "Sign Transaction",
-      component: () => (
-        <SignTransactionSignBlock transactionDetails={transactionDetails} />
-      ),
-      confirmButtonText: "Sign",
-    },
-  }
-
   const handleReject = async () => {
     await dispatch(rejectTransactionSignature())
     history.goBack()
@@ -171,87 +123,61 @@ export default function SignTransaction({
     }
   }
 
-  return (
-    <section>
-      <SignTransactionNetworkAccountInfoTopBar
-        accountTotal={signerAccountTotal}
-      />
-      <h1 className="serif_header title">{signContent[signType].title}</h1>
-      <div className="primary_info_card standard_width">
-        {signContent[signType].component()}
-      </div>
-      <SharedPanelSwitcher
-        setPanelNumber={setPanelNumber}
-        panelNumber={panelNumber}
-        panelNames={["Details"]}
-      />
-      {panelNumber === 0 ? <SignTransactionDetailPanel /> : null}
-      <div className="footer_actions">
-        <SharedButton
-          iconSize="large"
-          size="large"
-          type="secondary"
-          onClick={handleReject}
-        >
-          Reject
-        </SharedButton>
-        {signerAccountTotal.signingMethod ? (
-          <SharedButton
-            type="primary"
-            iconSize="large"
-            size="large"
-            onClick={handleConfirm}
-            showLoadingOnClick
-          >
-            {signContent[signType].confirmButtonText}
-          </SharedButton>
-        ) : (
-          <span className="no-signing">Read-only accounts cannot sign</span>
-        )}
-      </div>
-      <style jsx>
-        {`
-          section {
-            width: 100%;
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            background-color: var(--green-95);
-            z-index: 5;
+  switch (signType) {
+    case SignType.SignSwap:
+      return (
+        <SignTransactionContainer
+          signerAccountTotal={signerAccountTotal}
+          title="Swap assets"
+          infoBlock={<SignTransactionSwapAssetBlock />}
+          confirmButtonLabel="Confirm"
+          handleConfirm={handleConfirm}
+          handleReject={handleReject}
+        />
+      )
+    case SignType.SignSpend:
+      return (
+        <SignTransactionContainer
+          signerAccountTotal={signerAccountTotal}
+          title="Swap assets"
+          infoBlock={<SignTransactionSwapAssetBlock />}
+          confirmButtonLabel="Confirm"
+          handleConfirm={handleConfirm}
+          handleReject={handleReject}
+        />
+      )
+    case SignType.SignTransfer:
+      return (
+        <SignTransactionContainer
+          signerAccountTotal={signerAccountTotal}
+          title="Sign Transfer"
+          infoBlock={
+            <SignTransactionTransferBlock
+              token={assetSymbol ?? ""}
+              amount={amount ?? 0}
+              destination={to ?? ""}
+              localizedValue={value ?? ""}
+            />
           }
-          .title {
-            color: var(--trophy-gold);
-            font-size: 36px;
-            font-weight: 500;
-            line-height: 42px;
-            text-align: center;
+          confirmButtonLabel="Sign"
+          handleConfirm={handleConfirm}
+          handleReject={handleReject}
+        />
+      )
+    case SignType.Sign:
+      return (
+        <SignTransactionContainer
+          signerAccountTotal={signerAccountTotal}
+          title="Sign Transaction"
+          infoBlock={
+            <SignTransactionSignBlock transactionDetails={transactionDetails} />
           }
-          .primary_info_card {
-            display: block;
-            height: fit-content;
-            border-radius: 16px;
-            background-color: var(--hunter-green);
-            margin: 16px 0px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-          }
-          .footer_actions {
-            position: fixed;
-            bottom: 0px;
-            display: flex;
-            width: 100%;
-            padding: 0px 16px;
-            box-sizing: border-box;
-            align-items: center;
-            height: 80px;
-            justify-content: space-between;
-            box-shadow: 0 0 5px rgba(0, 20, 19, 0.5);
-            background-color: var(--green-95);
-          }
-        `}
-      </style>
-    </section>
-  )
+          confirmButtonLabel="Sign"
+          handleConfirm={handleConfirm}
+          handleReject={handleReject}
+        />
+      )
+    default:
+      return <></>
+  }
 }
