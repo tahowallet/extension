e294112c24b967676f21d94e58593c85f41d38ae
Antonio Salazar Cardozo <antonio@thesis.co>
2022-02-10 11:43:08 -0500

    Clear signed transaction and broadcastOnSign in clearTransactionState
    Having these two set to old values can trigger unexpected re-broadcasts
    of transactions, which can lead to unpleasant, seemingly-unpleasant, and
    unexpected results.

--- background/redux-slices/transaction-construction.ts
+++ background/redux-slices/transaction-construction.ts
@@ -135,6 +135,8 @@ const transactionSlice = createSlice({
       lastGasEstimatesRefreshed: state.lastGasEstimatesRefreshed,
       status: TransactionConstructionStatus.Idle,
       feeTypeSelected: NetworkFeeTypeChosen.Regular,
+      broadcastOnSign: false,
+      signedTransaction: undefined,
     }),
     setFeeType: (
       state,
