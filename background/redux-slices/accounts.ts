d7e7bbb94352da8df9d96e6a1df4280e9c3115dd
0xDaedalus <0xDaedalus@users.noreply.github.com>
2022-02-15 12:12:07 -0700

    make AccountType.NewSeed casing consistent with other newSeed types

--- background/redux-slices/accounts.ts
+++ background/redux-slices/accounts.ts
@@ -17,7 +17,7 @@ export const enum AccountType {
   ReadOnly = "read-only",
   Imported = "imported",
   Ledger = "ledger",
-  NewSeed = "new-seed",
+  NewSeed = "newSeed",
 }
 
 const availableDefaultNames = [
