205af5be7db42a30d073230e3761091ce77f230b
Antonio Salazar Cardozo <antonio@thesis.co>
2022-02-18 16:24:24 -0500

    Rework transaction signing to rely on enrichment annotations
    This removes the dependency on bespoke metadata lookup, and will remove
    the in-UI Ethereum provider for everything that isn't an internal dApp.
    
    Additionally, this reuses existing resolution pathways so that the
    system can consistently interpret transaction data in one place. The
    existing SignType abstraction, which preceded annotations, is removed
    in favor of resolving particular signing rendering components based on
    the annotation type.
    
    The biggest refactor here is that SignTransaction only takes a redirect
    location for the location state. Everything else is expected to be
    resolved via enrichment. This is currently only used in the Send
    component, which now sends the request to redirect back to the
    single-asset activity list.

--- ui/components/SignTransaction/SignTransactionTransferInfoProvider.tsx
+++ ui/components/SignTransaction/SignTransactionTransferInfoProvider.tsx
@@ -2,7 +2,15 @@ import {
   truncateAddress,
   truncateDecimalAmount,
 } from "@tallyho/tally-background/lib/utils"
+import { selectAssetPricePoint } from "@tallyho/tally-background/redux-slices/assets"
+import {
+  getAssetsState,
+  selectMainCurrencySymbol,
+} from "@tallyho/tally-background/redux-slices/selectors"
+import { enrichAssetAmountWithMainCurrencyValues } from "@tallyho/tally-background/redux-slices/utils/asset-utils"
+import { TransactionAnnotation } from "@tallyho/tally-background/services/enrichment"
 import React, { ReactElement } from "react"
+import { useBackgroundSelector } from "../../hooks"
 import FeeSettingsText from "../NetworkFees/FeeSettingsText"
 import TransactionDetailAddressValue from "../TransactionDetail/TransactionDetailAddressValue"
 import TransactionDetailContainer from "../TransactionDetail/TransactionDetailContainer"
@@ -12,17 +20,23 @@ import SignTransactionBaseInfoProvider, {
 } from "./SignTransactionInfoBaseProvider"
 
 export default function SignTransactionTransferInfoProvider({
-  token,
-  amount,
-  destination,
-  localizedValue,
+  transactionDetails,
+  annotation: { assetAmount, recipientAddress },
   inner,
 }: SignTransactionInfoProviderProps & {
-  token: string
-  amount: number
-  destination: string
-  localizedValue: string | number
+  annotation: TransactionAnnotation & { type: "asset-transfer" }
 }): ReactElement {
+  const assets = useBackgroundSelector(getAssetsState)
+  const mainCurrencySymbol = useBackgroundSelector(selectMainCurrencySymbol)
+  const assetPricePoint = selectAssetPricePoint(
+    assets,
+    assetAmount.asset.symbol,
+    mainCurrencySymbol
+  )
+  const localizedMainCurrencyAmount =
+    enrichAssetAmountWithMainCurrencyValues(assetAmount, assetPricePoint, 2)
+      .localizedMainCurrencyAmount ?? "-"
+
   return (
     <SignTransactionBaseInfoProvider
       title="Sign Transfer"
@@ -31,15 +45,15 @@ export default function SignTransactionTransferInfoProvider({
         <div className="sign_block">
           <div className="container">
             <div className="label">Send to</div>
-            <div className="send_to">{truncateAddress(destination)}</div>
+            <div className="send_to">{truncateAddress(recipientAddress)}</div>
           </div>
           <div className="divider" />
           <div className="container">
             <span className="label">Spend Amount</span>
             <span className="spend_amount">
-              {truncateDecimalAmount(amount, 4)} {token}
+              {assetAmount.localizedDecimalAmount} {assetAmount.asset.symbol}
             </span>
-            <span className="label">{`$${localizedValue}`}</span>
+            <span className="label">${`${localizedMainCurrencyAmount}`}</span>
           </div>
 
           <style jsx>
@@ -95,13 +109,18 @@ export default function SignTransactionTransferInfoProvider({
             name="Spend amount"
             value={
               <>
-                {truncateDecimalAmount(amount, 4)} {token}
+                {truncateDecimalAmount(assetAmount.decimalAmount, 4)}{" "}
+                {assetAmount.asset.symbol}
               </>
             }
           />
           <TransactionDetailItem
             name="To:"
-            value={<TransactionDetailAddressValue address={destination} />}
+            value={
+              <TransactionDetailAddressValue
+                address={transactionDetails.to ?? "-"}
+              />
+            }
           />
         </TransactionDetailContainer>
       }
