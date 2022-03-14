3bc0fec4da0706cccfc4c7aed1007767b99d1343
Henry Boldizsar <henrymb67@gmail.com>
2022-03-14 18:39:02 -0500

    Update claim service to binary search + stream IPFS
    Through an HTTP gateway for now

--- background/services/claim/index.ts
+++ background/services/claim/index.ts
@@ -1,5 +1,103 @@
 import { ServiceLifecycleEvents, ServiceCreatorFunction } from "../types"
+import { Eligible, IPFSLinkItem } from "./types"
 import BaseService from "../base"
+
+const IPFSFileDirectoryIPFSHash = process.env.FILE_DIRECTORY_IPFS_HASH
+const partGlossaryIPFSHash = process.env.PART_GLOSSARY_IPFS_HASH
+
+const IPFSHTTPGatewayPrefix = "https://ipfs.io/ipfs/"
+const IPFSHTTPGet = "https://ipfs.io/api/v0/dag/get?arg="
+
+/*
+ * Binary search to find which segment file to look in.
+ * For context, the eligibility data is split up into files:
+ * claim-0x90F79bf6EB2c4f870365E785982E1f101E93b906.ndjson
+ * claim-0x9eef87f4c08d8934cb2a3309df4dec5635338115.ndjson
+ * claim-0xa0Ee7A142d267C1f36714E4a8F75612F20a79720.ndjson
+ * claim-0xdD2FD4581271e230360230F9337D5c0430Bf44C0.ndjson
+ * claim-0xf6ff2962af467ca09d27378559b92ad912006719.ndjson
+ * ...
+ * To find which file to look in, we reference claim-index.json
+ */
+function findIndexOfPartToLookIn(
+  addressEligibilityPartGlossary: string[],
+  targetAddressNumber: number
+) {
+  const n = addressEligibilityPartGlossary.length
+  let start = 0
+  let end = n - 1
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2)
+    const current = Number(addressEligibilityPartGlossary[mid])
+
+    if (current === targetAddressNumber) {
+      return mid
+    }
+    if (current < targetAddressNumber) {
+      start = mid + 1
+    } else {
+      end = mid - 1
+    }
+  }
+  return end + 1
+}
+
+async function getFileHashProspect(targetAddress: string) {
+  const IPFSFileDirectory = await fetch(
+    `${IPFSHTTPGet}${IPFSFileDirectoryIPFSHash}`
+  )
+  const partGlossary = await fetch(
+    `${IPFSHTTPGatewayPrefix}${partGlossaryIPFSHash}`
+  )
+  const IPFSFileDirectoryJson = await IPFSFileDirectory.json()
+  const partGlossaryJson = await partGlossary.json()
+
+  const fileIndex = findIndexOfPartToLookIn(
+    partGlossaryJson.map((item: { startAddress: string; file: string }) => {
+      return item.startAddress
+    }),
+    Number(targetAddress)
+  )
+  const inClaimFileName = partGlossaryJson[fileIndex].file
+
+  const IPFSHashForFoundFile = IPFSFileDirectoryJson.Links.find(
+    (linkItem: IPFSLinkItem) => {
+      return linkItem.Name === inClaimFileName
+    }
+  )
+
+  return IPFSHashForFoundFile.Hash["/"]
+}
+
+async function getClaimFromFileHash(targetAddress: string, hash: string) {
+  const res = await fetch(`${IPFSHTTPGatewayPrefix}${hash}`)
+
+  let claim
+  const reader = res?.body?.getReader()
+  let result
+  const decoder = new TextDecoder("utf8")
+  while (!result?.done) {
+    // Seems reasonable to await inside of the while loop
+    // with the idea of avoiding reading everything.
+    // eslint-disable-next-line no-await-in-loop
+    result = await reader?.read()
+    const chunk = decoder
+      .decode(result?.value, { stream: true })
+      .split(/[\r\n]/)
+      .filter((chunkSegment) => chunkSegment.length > 0)
+      .map((chunkSegment) => JSON.parse(chunkSegment))
+
+    const foundClaim = chunk.find((item) => {
+      return item.account === targetAddress
+    })
+    if (foundClaim) {
+      claim = foundClaim
+      break
+    }
+  }
+  return claim
+}
 
 interface Events extends ServiceLifecycleEvents {
   newEligibility: Eligible
@@ -12,12 +110,10 @@ interface Events extends ServiceLifecycleEvents {
 export default class ClaimService extends BaseService<Events> {
   static create: ServiceCreatorFunction<Events, ClaimService, []> =
     async () => {
-      const db = await getOrCreateDB()
-
-      return new this(db)
+      return new this()
     }
 
-  private constructor(private db: ClaimDatabase) {
+  private constructor() {
     super()
   }
 
@@ -26,13 +122,19 @@ export default class ClaimService extends BaseService<Events> {
   }
 
   protected async internalStopService(): Promise<void> {
-    this.db.close()
-
     await super.internalStopService()
   }
 
   async getEligibility(address: string): Promise<Eligible> {
-    const claim = await this.db.getClaim(address)
+    const fileHash = await getFileHashProspect(address)
+    const { account, amount } = await getClaimFromFileHash(address, fileHash)
+
+    // TEMP: Place into current structure to avoid conflict with claim slice PR
+    const claim = {
+      address: account,
+      earnings: BigInt(amount),
+      reasons: "",
+    }
     this.emitter.emit("newEligibility", claim)
     return claim
   }
