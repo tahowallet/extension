78eef0d40a6b21edf8f89e15e5cb5c9f0f9fcf94
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2022-03-16 13:37:24 +0100

    Make SharedIcon more customizable
    SharedIcon component can handle both png and svg
    Given image can be used as regular background of the icon
    or it can be used as mask-image
    Allow to add hover effect to SharedIcon

--- ui/components/Shared/SharedIcon.tsx
+++ ui/components/Shared/SharedIcon.tsx
@@ -1,34 +1,62 @@
+import classNames from "classnames"
 import React, { ReactElement } from "react"
 
 type Props = {
+  type: "mask" | "background"
   icon: string
   width: number
   height?: number
   color?: string
+  hoverColor?: string
   customStyles?: string
-  onClick?: () => void
+  ariaLabel?: string
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 export default function SharedIcon(props: Props): ReactElement {
   const {
+    type,
     icon,
     width,
     height = width,
-    color,
+    color = "transparent",
+    hoverColor = color,
     customStyles = "",
+    ariaLabel,
     onClick,
   } = props
 
   return (
-    <button className="icon" type="button" onClick={onClick}>
+    <button
+      className={classNames("icon", {
+        mask: type === "mask",
+        background: type === "background",
+      })}
+      type="button"
+      onClick={onClick}
+      aria-label={ariaLabel}
+    >
       <style jsx>{`
-        mask-image: url("./images/${icon}.svg");
-        mask-size: cover;
-        background-color: ${color};
-        width: ${width}px;
-        height: ${height}px;
-        cursor: ${onClick ? "pointer" : "auto"};
-        ${customStyles};
+        .mask {
+          mask-image: url("./images/${icon}");
+          mask-size: cover;
+        }
+        .background {
+          background-image: url("./images/${icon}");
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+        .icon {
+          width: ${width}px;
+          height: ${height}px;
+          cursor: ${onClick ? "pointer" : "auto"};
+          background-color: ${color};
+          ${customStyles};
+        }
+        .icon:hover {
+          background-color: ${hoverColor};
+        }
       `}</style>
     </button>
   )
