0ac2cf55440b577fe537017f703f13d6ae47d07d
Henry Boldizsar <henrymb67@gmail.com>
2022-03-23 14:29:49 -0500

    Remove setting overflow-y attribute in slide up menu

--- ui/components/Shared/SharedSlideUpMenu.tsx
+++ ui/components/Shared/SharedSlideUpMenu.tsx
@@ -84,7 +84,6 @@ export default function SharedSlideUpMenu(props: Props): ReactElement {
           .slide_up_menu {
             width: 100%;
             height: var(--menu-height);
-            overflow-y: hidden;
             overflow-x: hidden;
             border-radius: 16px;
             background-color: var(--green-95);
