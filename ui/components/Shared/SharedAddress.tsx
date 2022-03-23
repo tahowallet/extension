6b007518d7613cfdabb25c04bd5fa69a117d4df8
Antonio Salazar Cardozo <antonio@thesis.co>
2022-03-22 21:48:49 -0400

    Add SharedAddress component
    The JSDocs on the component explain its functioning, but the short
    version is it displays an optional name, always tooltips the address
    (currently as an OS tooltip), and always copies to clipboard on click.

---
+++ ui/components/Shared/SharedAddress.tsx
@@ -0,0 +1,68 @@
+import { truncateAddress } from "@tallyho/tally-background/lib/utils"
+import { setSnackbarMessage } from "@tallyho/tally-background/redux-slices/ui"
+import { NameResolverSystem } from "@tallyho/tally-background/services/name"
+import React, { ReactElement, useCallback } from "react"
+import { useBackgroundDispatch } from "../../hooks"
+import SharedTooltip from "./SharedTooltip"
+
+type SharedAddressProps = {
+  address: string
+  name?: string | undefined
+  nameResolverSystem?: NameResolverSystem
+}
+
+/**
+ * The SharedAddress component is used to render addresses that can optionally
+ * be represented as resolved names, and that are copiable by clicking.
+ *
+ * The component always expects an `address` prop. If an optional `name` prop
+ * is passed, it is shown; otherwise, a truncated version of the address is
+ * shown. The component tooltip always has a tooltip with the full address.
+ * Additionally, clicking the component will copy the address to the clipboard
+ * and present the user with a snackbar message indicating this has occurred.
+ *
+ * If the optional `nameResolverSystem` property is provided, an info tooltip
+ * is included in the component to inform the user of which name resolver
+ * system was used to resolve the passed `name`. If no `name` is passed, the
+ * `nameResolverSystem` prop is ignored.
+ */
+export default function SharedAddress({
+  name,
+  address,
+  nameResolverSystem: nameSourceSystem,
+}: SharedAddressProps): ReactElement {
+  const dispatch = useBackgroundDispatch()
+
+  const primaryText = name ?? truncateAddress(address)
+
+  const copyAddress = useCallback(() => {
+    navigator.clipboard.writeText(address)
+    dispatch(setSnackbarMessage("Address copied to clipboard"))
+  }, [address, dispatch])
+
+  return (
+    <button type="button" onClick={copyAddress} title={address}>
+      {primaryText}
+      {name === undefined || nameSourceSystem === undefined ? (
+        <></>
+      ) : (
+        <>
+          <SharedTooltip width={130}>
+            <p className="name_source_tooltip">
+              Resolved using {nameSourceSystem}
+            </p>
+          </SharedTooltip>{" "}
+        </>
+      )}
+      <style jsx>{`
+        button :last-child {
+          top: 3px;
+        }
+        .name_source_tooltip {
+          margin: 0;
+          text-align: center;
+        }
+      `}</style>
+    </button>
+  )
+}
