b65102946476140588f694e643b37198c6e814fa
Wojciech Turek <wojciech.turek@tally.cash>
2022-03-28 12:28:09 +0200

    Use selector for main currency symbol

--- ui/components/Claim/ClaimReferralByUser.tsx
+++ ui/components/Claim/ClaimReferralByUser.tsx
@@ -1,3 +1,4 @@
+import { selectMainCurrencySymbol } from "@tallyho/tally-background/redux-slices/selectors"
 import { formatCurrencyAmount } from "@tallyho/tally-background/redux-slices/utils/asset-utils"
 import React, { ReactElement } from "react"
 import { useBackgroundSelector } from "../../hooks"
@@ -10,9 +11,17 @@ export default function ClaimReferralByUser({
 }: {
   claimAmount: number
 }): ReactElement {
-  const amountWithBonus = formatCurrencyAmount("USD", claimAmount * 0.05, 2)
+  const mainCurrency = useBackgroundSelector(selectMainCurrencySymbol)
+  const amountWithBonus = formatCurrencyAmount(
+    mainCurrency,
+    claimAmount * 0.05,
+    2
+  )
   const referrer = useBackgroundSelector((state) => state.claim.referrer)
 
+  if (referrer === null) {
+    return <></>
+  }
   return (
     <div className="wrap standard_width">
       <ClaimAmountBanner amount={claimAmount} />
@@ -24,7 +33,7 @@ export default function ClaimReferralByUser({
         {`You were referred by somebody, and to reward that you each get ${amountWithBonus} DOGGO`}
       </div>
       <ClaimReferralBridge />
-      <ClaimDelegateChoiceProfile name={referrer ?? "henryboldi.eth"} />
+      <ClaimDelegateChoiceProfile name={referrer} />
       <style jsx>
         {`
           .wrap {
