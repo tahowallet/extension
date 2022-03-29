af4a1b098101fbed080ffa267a7d804cb9c936fe
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2022-03-29 11:44:31 +0200

    Add custom path for BackButton where needed

--- ui/pages/SingleAsset.tsx
+++ ui/pages/SingleAsset.tsx
@@ -80,7 +80,7 @@ export default function SingleAsset(): ReactElement {
   return (
     <>
       <div className="back_button_wrap standard_width_padded">
-        <SharedBackButton />
+        <SharedBackButton path="/" />
       </div>
       {typeof asset === "undefined" ? (
         <></>
