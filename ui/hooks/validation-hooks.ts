cf0a1dfe59373e33cd75c497ad37e1eadcfdbedb
Antonio Salazar Cardozo <antonio@thesis.co>
2022-03-31 22:03:26 -0400

    Add async validation hook + address/name validation instance
    Async validation hooks work similarly to regular validation hooks, but
    use asynchronous operations to validate and provide access to whether a
    validation is currently in progress as a return value so it can be used
    to fuel e.g. spinner state.

--- ui/hooks/validation-hooks.ts
+++ ui/hooks/validation-hooks.ts
@@ -37,6 +37,15 @@ export type ValidationHookProperties = {
   handleInputChange: (newValue: string) => void
 }
 
+export type AsyncValidationHookProperties = ValidationHookProperties & {
+  /**
+   * A boolean indicating if the async validator is currently validating the
+   * passed input. The most straightforward use of this is to show a spinner
+   * while the validator is running.
+   */
+  isValidating: boolean
+}
+
 /**
  * A hook that provides validation for a string input that should produce a
  * type T. Validation hooks typically handle parsing internally and may do
@@ -59,6 +68,18 @@ export type ValidationHook<T> = (
   onValidChange: ValidDataChangeHandler<T>,
   ...additionalValidators: AdditionalDataValidator<T>[]
 ) => ValidationHookProperties
+
+/**
+ * A hook that provides asynchronous validation for a string input that should
+ * produce a type T. These hooks function the same as {@see ValidationHook},
+ * but may perform asynchronous operations. As such, they return an additional
+ * property to indicate whether an asynchronous validation is currently in
+ * progress, which can be used to reflect the pending operation in the UI.
+ */
+export type AsyncValidationHook<T> = (
+  onValidChange: ValidDataChangeHandler<T>,
+  ...additionalValidators: AdditionalDataValidator<Promise<T>>[]
+) => AsyncValidationHookProperties
 
 /**
  * A ValidationHook that parses string values using a parser and allows for
@@ -102,4 +123,65 @@ export const useParsedValidation = <T>(
   }
 
   return { rawValue, errorMessage, handleInputChange }
+}
+
+/**
+ * An AsyncValidationHook that attempts to resolve strings as either addresses
+ * or names resolvable via internal name resolution. Empty strings or strings
+ * of entirely whitespace do not trigger an error, but do trigger the change
+ * handler with a value of `undefined`.
+ *
+ * Address-like strings are immediately considered valid, while non-address
+ * strings are resolved asynchronously.
+ */
+export const useAddressOrNameValidation: AsyncValidationHook<
+  HexString | undefined
+> = (onValidChange) => {
+  const [errorMessage, setErrorMessage] = useState<string | undefined>()
+  const [rawValue, setRawValue] = useState<string>("")
+  // Flag and value tracked separately due to async handling.
+  const [isValidating, setIsValidating] = useState(false)
+  const validatingValue = useRef<string | undefined>(undefined)
+  const dispatch = useBackgroundDispatch()
+
+  const handleInputChange = async (newValue: string) => {
+    setRawValue(newValue)
+
+    const trimmed = newValue.trim()
+
+    setErrorMessage(undefined)
+    if (trimmed === "") {
+      onValidChange(undefined)
+    } else if (isProbablyEVMAddress(trimmed)) {
+      onValidChange(trimmed)
+    } else {
+      setIsValidating(true)
+      validatingValue.current = trimmed
+
+      const resolved = (await dispatch(
+        resolveNameOnNetwork({ name: trimmed, network: ETHEREUM })
+      )) as unknown as string
+
+      // Asynchronicity means we could already have started validating another
+      // value before this validation completed; ignore those cases.
+      if (validatingValue.current === trimmed) {
+        if (resolved === undefined) {
+          onValidChange(undefined)
+          setErrorMessage("Address could not be found")
+        } else {
+          onValidChange(resolved)
+        }
+
+        setIsValidating(false)
+        validatingValue.current = undefined
+      }
+    }
+  }
+
+  return {
+    rawValue,
+    errorMessage,
+    isValidating,
+    handleInputChange,
+  }
 }
