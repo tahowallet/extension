d561776e6f675f66db2b9361ed643a2e9d00ea30
Henry Boldizsar <henrymb67@gmail.com>
2022-04-05 21:39:46 -0500

    Move name service `storageGatewayURL` to lib

--- background/services/name/index.ts
+++ background/services/name/index.ts
@@ -3,6 +3,7 @@ import { EVMNetwork } from "../../networks"
 import { normalizeEVMAddress, sameEVMAddress } from "../../lib/utils"
 import { ETHEREUM } from "../../constants/networks"
 import { getTokenMetadata } from "../../lib/erc721"
+import { storageGatewayURL } from "../../lib/storage-gateway"
 
 import { ServiceCreatorFunction, ServiceLifecycleEvents } from "../types"
 import BaseService from "../base"
@@ -48,46 +49,6 @@ type Events = ServiceLifecycleEvents & {
   resolvedAddress: ResolvedAddressRecord
   resolvedName: ResolvedNameRecord
   resolvedAvatar: ResolvedAvatarRecord
-}
-
-const ipfsGateway = new URL("https://ipfs.io/ipfs/")
-const arweaveGateway = new URL("https://arweave.net/")
-
-/**
- * Given a url and a base URL, adjust the url to match the protocol and
- * hostname of the base URL, and append the hostname and remaining path of the
- * original url as path components in the base URL. Preserves querystrings and
- * hash data if present.
- *
- * @example
- * url: `ipfs://CID/path/to/resource`
- * baseURL: `https://ipfs.io/ipfs/`
- * result: `https://ipfs.io/ipfs/CID/path/to/resource`
- *
- * @example
- * url: `ipfs://CID/path/to/resource?parameters#hash`
- * baseURL: `https://ipfs.io/ipfs/`
- * result: `https://ipfs.io/ipfs/CID/path/to/resource?parameters#hash`
- */
-function changeURLProtocolAndBase(url: URL, baseURL: URL) {
-  const newURL = new URL(url)
-  newURL.protocol = baseURL.protocol
-  newURL.hostname = baseURL.hostname
-  newURL.pathname = `${baseURL.pathname}/${url.hostname}/${url.pathname}`
-
-  return newURL
-}
-
-// TODO eventually we want proper IPFS and Arweave support
-function storageGatewayURL(url: URL): URL {
-  switch (url.protocol) {
-    case "ipfs":
-      return changeURLProtocolAndBase(url, ipfsGateway)
-    case "ar":
-      return changeURLProtocolAndBase(url, arweaveGateway)
-    default:
-      return url
-  }
 }
 
 /**
