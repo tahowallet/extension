bee5077cf4e9e27688a63d842a3cde97a5b7de72
Henry Boldizsar <henrymb67@gmail.com>
2022-04-07 20:45:45 -0500

    Create new export logs page

---
+++ ui/pages/Menu/MenuExportLogs.tsx
@@ -0,0 +1,72 @@
+import React, { ReactElement } from "react"
+import SharedButton from "../../components/Shared/SharedButton"
+import SharedPageHeader from "../../components/Shared/SharedPageHeader"
+
+export default function MenuExportLogs(): ReactElement {
+  return (
+    <div className="standard_width_padded">
+      <SharedPageHeader>Bug report</SharedPageHeader>
+      <section>
+        <h2>1. Export logs</h2>
+        <p>
+          In order for us to better understand your bug we need you to export
+          your logs
+        </p>
+        <a
+          href={`data:application/octet-stream;charset=utf-16le;base64,${Buffer.from(
+            localStorage.getItem("logs") || ""
+          ).toString("base64")}`}
+          download={`logs_${(process.env.VERSION || "").replace(
+            /\./g,
+            "_"
+          )}.txt`}
+        >
+          <SharedButton
+            type="secondary"
+            size="medium"
+            iconSmall="download"
+            iconPosition="left"
+          >
+            Export logs
+          </SharedButton>
+        </a>
+      </section>
+      <section>
+        <h2>2. Join #bug-reports on Discord</h2>
+        <p>
+          Visit us on our Discord channel, and report your bug there, make sure
+          to include the logs.
+        </p>
+        <SharedButton
+          type="secondary"
+          size="medium"
+          iconSmall="discord"
+          iconPosition="left"
+          onClick={() => {
+            window.open(`https://chat.tally.cash/`, "_blank")?.focus()
+          }}
+        >
+          Join Discord
+        </SharedButton>
+      </section>
+      <style jsx>{`
+        h2 {
+          color: var(--green-20);
+          font-size: 18px;
+          font-weight: 600;
+          line-height: 24px;
+          margin-top: 20px;
+        }
+        p {
+          color: var(--green-40);
+          font-size: 16px;
+          font-weight: 500;
+          line-height: 24px;
+        }
+        section {
+          margin-bottom: 35px;
+        }
+      `}</style>
+    </div>
+  )
+}
