9b51c9de14b5631730d0266cbd7906a3141eb514
Wojciech Turek <wojciech.turek@tally.cash>
2022-04-29 11:30:00 +0200

    Align implementation closer to the design

--- ui/components/EmptyBowl/EmptyBowl.tsx
+++ ui/components/EmptyBowl/EmptyBowl.tsx
@@ -5,22 +5,23 @@ export default function EmptyBowl(): ReactElement {
     <div className="standard_width container">
       <img className="bowlImage" src="./images/empty_bowl_@2.png" alt="" />
       <div className="title">Just an empty bowl</div>
-      <div className="text">You don&apos;t have an active deposit</div>
+      <div className="text">You don&apos;t have an active deposit.</div>
       <style jsx>
         {`
           .container {
             display: flex;
             flex-direction: column;
             align-items: center;
-            justify-content: center;
+            margin-top: 16px;
             height: 100%;
           }
           .bowlImage {
-            width: 80px;
+            width: 90px;
           }
           .title {
             color: var(--trophy-gold);
             font-size: 28px;
+            margin-bottom: 10px;
           }
           .text {
             color: var(--green-40);
