2019df1345f5456cc3b28cb298c7ae72e8ae5700
Wojciech Turek <wojciech.turek@tally.cash>
2022-05-09 14:51:18 +0200

    Move the icons out of the vault data
    Since most of the vault data is coming from the contracts it
    felt necessary to move the hardcoded token links to a separate file.

---
+++ background/redux-slices/earn-utils/tokenIcons.ts
@@ -0,0 +1,28 @@
+const tokenIcons = {
+  AAVE: ["https://cryptologos.cc/logos/thumbs/aave.png?v=022"],
+  UNI: ["ipfs://QmXttGpZrECX5qCyXbBQiqgQNytVGeZW5Anewvh2jc4psg"],
+  crvCVXETH: [
+    "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880",
+    "ipfs://bafybeicpyu35mpptkxvbcfbzmlaefiqlj7layt5oolcbjh5cc5vfkyi7c4",
+  ],
+  "YFIETH-f": [
+    "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880",
+    "https://cryptologos.cc/logos/thumbs/yearn-finance.png?v=022",
+  ],
+  SNX: [
+    "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC011a73ee8576Fb46F5E1c5751cA3B9Fe0af2a6F/logo.png",
+  ],
+  SUSHI: ["https://cryptologos.cc/logos/thumbs/sushiswap.png?v=022"],
+  LOOKS: ["ipfs://bafybeia64nloazn7cxgwema5o26l2zjbxufb5kb5dxea6dbpretuxoappq"],
+  KEEP: ["https://s2.coinmarketcap.com/static/img/coins/64x64/5566.png"],
+  crvTETH: [
+    "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880",
+    "ipfs://bafybeib53hx56um4xexwtaqjo3u3ridppnpblfucqdbotwlqcadlfyc2a4",
+  ],
+  DOGGOETH: [
+    "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880",
+    "https://avatars.githubusercontent.com/u/77035304?s=200&v=4",
+  ],
+}
+
+export default tokenIcons
