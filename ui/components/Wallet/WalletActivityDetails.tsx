0a900da544d1497316e801794b5e605a19111f39
0xDaedalus <0xDaedalus@users.noreply.github.com>
2022-05-19 10:52:40 -0500

    Network-specific activity detail link

--- ui/components/Wallet/WalletActivityDetails.tsx
+++ ui/components/Wallet/WalletActivityDetails.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback, ReactElement } from "react"
 import { ActivityItem } from "@tallyho/tally-background/redux-slices/activities"
 import { getRecipient } from "@tallyho/tally-background/redux-slices/utils/activity-utils"
+import { selectCurrentNetwork } from "@tallyho/tally-background/redux-slices/selectors"
 import SharedButton from "../Shared/SharedButton"
 import SharedAddress from "../Shared/SharedAddress"
+import { useBackgroundSelector } from "../../hooks"
 
 interface DetailRowItemProps {
   label: string
@@ -109,11 +111,18 @@ export default function WalletActivityDetails(
 ): ReactElement {
   const { activityItem } = props
 
+  const network = useBackgroundSelector(selectCurrentNetwork)
+
   const openExplorer = useCallback(() => {
     window
-      .open(`https://etherscan.io/tx/${activityItem.hash}`, "_blank")
+      .open(
+        `https://${
+          network.name === "Polygon" ? "polygonscan.com" : "etherscan.io"
+        }/tx/${activityItem.hash}`,
+        "_blank"
+      )
       ?.focus()
-  }, [activityItem?.hash])
+  }, [activityItem?.hash, network.name])
 
   if (!activityItem) return <></>
 
@@ -130,7 +139,7 @@ export default function WalletActivityDetails(
             iconMedium="new-tab"
             onClick={openExplorer}
           >
-            Etherscan
+            {network.name === "Polygon" ? "Polygonscan" : "Etherscan"}
           </SharedButton>
         </div>
       </div>
