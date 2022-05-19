9765f770ac14cdbcc49d51a7772c27c30ca4f6e0
0xDaedalus <0xDaedalus@users.noreply.github.com>
2022-05-19 10:01:40 -0500

    Get Polygon Gas Estimates Working

--- background/lib/gas.ts
+++ background/lib/gas.ts
@@ -4,8 +4,10 @@ import logger from "./logger"
 import Blocknative, {
   BlocknativeNetworkIds,
 } from "../third-party-data/blocknative"
+import { BigNumber } from "ethers"
 import { BlockPrices, EVMNetwork } from "../networks"
-import { ETHEREUM } from "../constants/networks"
+import { ETHEREUM, POLYGON } from "../constants/networks"
+import { gweiToWei } from "./utils"
 
 // We can't use destructuring because webpack has to replace all instances of
 // `process.env` variables in the bundled output
@@ -31,15 +33,25 @@ export default async function getBlockPrices(
     }
   }
 
-  // otherwise, we're going it alone!
-
   const [currentBlock, feeData] = await Promise.all([
     provider.getBlock("latest"),
     provider.getFeeData(),
   ])
 
-  const baseFeePerGas =
-    (currentBlock?.baseFeePerGas?.toBigInt() || 0n) * 1_000_000_000n
+  const baseFeePerGas = currentBlock?.baseFeePerGas?.toBigInt()
+
+  if (network.name === "Polygon") {
+    try {
+      return await getPolygonGasPrices(
+        feeData?.gasPrice?.toBigInt() ||
+          0n /* @TODO what do we do if this is 0n */
+      )
+    } catch (e) {
+      logger.error("Error getting block prices from Polygon", e)
+    }
+  }
+
+  // otherwise, we're going it alone!
 
   if (feeData.gasPrice === null) {
     logger.warn("Not receiving accurate gas prices from provider", feeData)
@@ -56,20 +68,20 @@ export default async function getBlockPrices(
       estimatedPrices: [
         {
           confidence: 99,
-          maxPriorityFeePerGas: 2500000000n,
-          maxFeePerGas: baseFeePerGas * 2n + 2500000000n,
+          maxPriorityFeePerGas: 2_500_000_000n,
+          maxFeePerGas: baseFeePerGas * 2n + 2_500_000_000n,
           price: gasPrice, // this estimate isn't great
         },
         {
           confidence: 95,
-          maxPriorityFeePerGas: 1500000000n,
-          maxFeePerGas: (baseFeePerGas * 15n) / 10n + 1500000000n,
+          maxPriorityFeePerGas: 1_500_000_000n,
+          maxFeePerGas: (baseFeePerGas * 15n) / 10n + 1_500_000_000n,
           price: (gasPrice * 9n) / 10n,
         },
         {
           confidence: 70,
-          maxPriorityFeePerGas: 2500000000n,
-          maxFeePerGas: (baseFeePerGas * 13n) / 10n + 1100000000n,
+          maxPriorityFeePerGas: 1_100_000_000n,
+          maxFeePerGas: (baseFeePerGas * 13n) / 10n + 1_100_000_000n,
           price: (gasPrice * 8n) / 10n,
         },
       ],
@@ -98,6 +110,65 @@ export default async function getBlockPrices(
         maxPriorityFeePerGas,
         maxFeePerGas,
         price: gasPrice,
+      },
+    ],
+    dataSource: "local",
+  }
+}
+
+type PolygonFeeDetails = {
+  maxPriorityFee: number // gwei
+  maxFee: number // gwei
+}
+
+type PolygonGasResponse = {
+  safeLow: PolygonFeeDetails
+  standard: PolygonFeeDetails
+  fast: PolygonFeeDetails
+  estimatedBaseFee: number // gwei
+  blockTime: number
+  blockNumber: number
+}
+
+// Not perfect but works most of the time.  Our fallback method does not work at all for polygon.
+const getPolygonGasPrices = async (price: bigint): Promise<BlockPrices> => {
+  const res = await fetch("https://gasstation-mainnet.matic.network/v2", {
+    method: "get",
+  })
+
+  const gasEstimates = (await res.json()) as PolygonGasResponse
+
+  const baseFeePerGas = gweiToWei(gasEstimates.estimatedBaseFee)
+
+  return {
+    network: POLYGON,
+    blockNumber: gasEstimates.blockNumber,
+    baseFeePerGas,
+    estimatedTransactionCount: null,
+    estimatedPrices: [
+      {
+        confidence: 99,
+        maxPriorityFeePerGas: gweiToWei(
+          Math.ceil(gasEstimates.fast.maxPriorityFee)
+        ),
+        maxFeePerGas: gweiToWei(Math.ceil(gasEstimates.fast.maxFee)),
+        price, // this estimate isn't great
+      },
+      {
+        confidence: 95,
+        maxPriorityFeePerGas: gweiToWei(
+          Math.ceil(gasEstimates.standard.maxPriorityFee)
+        ),
+        maxFeePerGas: gweiToWei(Math.ceil(gasEstimates.standard.maxFee)),
+        price,
+      },
+      {
+        confidence: 70,
+        maxPriorityFeePerGas: gweiToWei(
+          Math.ceil(gasEstimates.safeLow.maxPriorityFee)
+        ),
+        maxFeePerGas: gweiToWei(Math.ceil(gasEstimates.safeLow.maxFee)),
+        price,
       },
     ],
     dataSource: "local",
