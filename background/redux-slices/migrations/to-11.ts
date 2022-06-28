6c9792ed1d80664a502ee1647d113ecbd471bf14
Gergo Nagy <gergo.nagy@tally.cash>
2022-06-28 14:13:56 +0200

    migration reordering to resolve conflicts

--- background/redux-slices/migrations/to-11.ts
+++ background/redux-slices/migrations/to-11.ts
@@ -1,80 +1,26 @@
-import { POLYGON } from "../../constants"
-
-type PermissionRequest = {
-  key: string
-  origin: string
-  faviconUrl: string
-  chainID: string
-  title: string
-  state: "request" | "allow" | "deny"
-  accountAddress: string
-}
-
-type PermissionMap = {
-  evm: {
-    [chainID: string]: {
-      [address: string]: {
-        [origin: string]: PermissionRequest
-      }
-    }
-  }
-}
-
 type OldState = {
-  dappPermission: {
-    allowedPages: { [origin_accountAddress_chainId: string]: PermissionRequest }
+  ui: {
+    [sliceKey: string]: unknown
   }
+  [otherSlice: string]: unknown
 }
 
 type NewState = {
-  dapp: {
-    allowed: PermissionMap
+  ui: {
+    slippageTolerance: number
+    [sliceKey: string]: unknown
   }
+  [otherSlice: string]: unknown
 }
 
 export default (prevState: Record<string, unknown>): NewState => {
-  // Migrate the by-origin-and-address-keyed permission data in the dapp slice to be
-  // a nested map of evm => chainId => address => origin.
-
-  const oldDappPermissionsState = prevState as OldState
-
-  const allowed: PermissionMap = { evm: {} }
-
-  Object.values(oldDappPermissionsState.dappPermission.allowedPages).forEach(
-    (permission) => {
-      // Ethereum Permissions
-      allowed.evm[permission.chainID] ??= {}
-      allowed.evm[permission.chainID][permission.accountAddress] ??= {}
-      allowed.evm[permission.chainID][permission.accountAddress][
-        permission.origin
-      ] ??= permission
-
-      // We've made the product decision to copy Ethereum permissions to polygon as part of the
-      // multi-chain feature so we do that below.
-
-      // Polygon Permissions
-      allowed.evm[POLYGON.chainID] ??= {}
-      allowed.evm[POLYGON.chainID][permission.accountAddress] ??= {}
-      allowed.evm[POLYGON.chainID][permission.accountAddress][
-        permission.origin
-      ] ??= permission
-    }
-  )
-
-  const { allowedPages: _, ...restOfOldDappPermission } =
-    oldDappPermissionsState.dappPermission
-
-  const newDappPermissionsState: NewState = {
-    dapp: {
-      ...restOfOldDappPermission,
-      allowed,
-    },
-  }
-
-  const { dappPermission: __, ...restOfPrevState } = prevState
+  const typedPrevState = prevState as OldState
 
   return {
-    ...restOfPrevState,
-    ...newDappPermissionsState,
+    ...prevState,
+    ui: {
+      ...typedPrevState.ui,
+      slippageTolerance: 0.01,
+    },
   }
 }
