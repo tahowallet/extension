6ce9923e28d2e3cdf033fbb68a2d9fbe2ab9a0bb
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2022-07-25 16:16:01 +0200

    Fix Ledger onboarding on different chains
    * Ledger balance fetched during onboarding
    should be saved for given chain
    * Show proper scan website for Ethereum and Polygon

--- ui/utils/constants.ts
+++ ui/utils/constants.ts
@@ -1,3 +1,8 @@
+import { ETHEREUM, POLYGON } from "@tallyho/tally-background/constants"
+
 export const doggoTokenDecimalDigits = 18
 
-export default doggoTokenDecimalDigits
+export const scanWebsite = {
+  [ETHEREUM.chainID]: { title: "Etherscan", url: "https://etherscan.io" },
+  [POLYGON.chainID]: { title: "Polygonscan", url: "https://polygonscan.com" },
+}
