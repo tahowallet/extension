cbbd247fe0f659d98ca0c4eafcd0ae6c30de8a84
Henry Boldizsar <henrymb67@gmail.com>
2022-07-27 01:37:16 -0500

    Add NFTs Wallet component to feed list on wallet page

---
+++ ui/components/NFTs/NFTsWallet.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react"
+import { fetchThenUpdateNFTsByNetwork } from "@tallyho/tally-background/redux-slices/nfts"
+import {
+  selectCurrentAccount,
+  selectCurrentNetwork,
+} from "@tallyho/tally-background/redux-slices/selectors"
+import selectNFTs from "@tallyho/tally-background/redux-slices/selectors/nftsSelectors"
+import { useBackgroundDispatch, useBackgroundSelector } from "../../hooks"
+import NFTsList from "./NFTsList"
+
+export default function NFTsWallet() {
+  const NFTs = useBackgroundSelector(selectNFTs)
+  const { address } = useBackgroundSelector(selectCurrentAccount) ?? {}
+  const currentNetwork = useBackgroundSelector(selectCurrentNetwork)
+  const dispatch = useBackgroundDispatch()
+
+  useEffect(() => {
+    dispatch(fetchThenUpdateNFTsByNetwork({ address, currentNetwork }))
+  }, [address, currentNetwork, dispatch])
+
+  const currentOwnedNFTsList =
+    NFTs &&
+    NFTs.evm[currentNetwork.chainID] &&
+    NFTs.evm[currentNetwork.chainID][address]
+
+  return <>{currentOwnedNFTsList && <NFTsList NFTs={currentOwnedNFTsList} />}</>
+}
