b1c37d027ea337a42d951493cd7166146124eb45
Henry Boldizsar <henrymb67@gmail.com>
2022-07-28 01:11:51 -0500

    Fix empty bowl file name
    Had a typo basically

--- ui/components/Earn/EmptyBowl.tsx
+++ ui/components/Earn/EmptyBowl.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from "react"
 export default function EmptyBowl(): ReactElement {
   return (
     <div className="standard_width container">
-      <img className="bowl_image" src="./images/empty_bowl_@2.png" alt="" />
+      <img className="bowl_image" src="./images/empty_bowl@2x.png" alt="" />
       <div className="title">Just an empty bowl</div>
       <div className="text">You don&apos;t have an active deposit.</div>
       <style jsx>
--- ui/components/NFTs/NFTsEmpty.tsx
+++ ui/components/NFTs/NFTsEmpty.tsx
@@ -3,10 +3,10 @@ import React, { ReactElement } from "react"
 export default function NFTsEmpty(): ReactElement {
   return (
     <div className="standard_width container">
-      <img className="bowl_image" src="./images/empty_bowl_@2.png" alt="" />
       <div className="text">
         Looks rare, but there are no NFT to see here, why not get some?
       </div>
+      <img className="bowl_image" src="./images/empty_bowl@2x.png" alt="" />
       <style jsx>
         {`
           .container {
