8b09a8db08afd4c524bec5cba22d28abfca2ecf7
0xDaedalus <0xDaedalus@users.noreply.github.com>
2022-08-11 15:06:28 -0600

    Add Optimism etherscan link

--- ui/utils/constants.ts
+++ ui/utils/constants.ts
@@ -1,8 +1,16 @@
-import { ETHEREUM, POLYGON } from "@tallyho/tally-background/constants"
+import {
+  ETHEREUM,
+  OPTIMISM,
+  POLYGON,
+} from "@tallyho/tally-background/constants"
 
 export const doggoTokenDecimalDigits = 18
 
 export const scanWebsite = {
   [ETHEREUM.chainID]: { title: "Etherscan", url: "https://etherscan.io" },
+  [OPTIMISM.chainID]: {
+    title: "Etherscan",
+    url: "https://optimistic.etherscan.io",
+  },
   [POLYGON.chainID]: { title: "Polygonscan", url: "https://polygonscan.com" },
 }
