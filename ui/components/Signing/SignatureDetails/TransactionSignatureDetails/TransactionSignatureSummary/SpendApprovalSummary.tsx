cffb69ff883432bf9cca4dcb5afd619b413bb885
Antonio Salazar Cardozo <antonio@thesis.co>
2022-08-14 18:10:39 -0400

    Add signing data for transactions in new signing flow
    These components are functionally the same as their equivalents in
    SignTransaction*InfoProvider components, and use enrichment data to
    present human-understandable details about the transaction being signed.

---
+++ ui/components/Signing/SignatureDetails/TransactionSignatureDetails/TransactionSignatureSummary/SpendApprovalSummary.tsx
@@ -0,0 +1,285 @@
+import {
+  ERC20_FUNCTIONS,
+  ERC20_INTERFACE,
+} from "@tallyho/tally-background/lib/erc20"
+import {
+  convertFixedPointNumber,
+  fixedPointNumberToString,
+  parseToFixedPointNumber,
+} from "@tallyho/tally-background/lib/fixed-point"
+import { isMaxUint256 } from "@tallyho/tally-background/lib/utils"
+import { updateTransactionData } from "@tallyho/tally-background/redux-slices/transaction-construction"
+import { AssetApproval } from "@tallyho/tally-background/services/enrichment"
+import { ethers } from "ethers"
+import { hexlify } from "ethers/lib/utils"
+import React, { ReactElement, useState } from "react"
+import { useDispatch } from "react-redux"
+import classNames from "classnames"
+import { Trans, useTranslation } from "react-i18next"
+import SharedAssetIcon from "../../../../Shared/SharedAssetIcon"
+import SharedButton from "../../../../Shared/SharedButton"
+import SharedInput from "../../../../Shared/SharedInput"
+import SharedTooltip from "../../../../Shared/SharedTooltip"
+import SharedAddress from "../../../../Shared/SharedAddress"
+import { TransactionSignatureSummaryProps } from "./TransactionSignatureSummaryProps"
+import TransactionSignatureSummaryBody from "./TransactionSignatureSummaryBody"
+
+export default function SpendApprovalSummary({
+  transactionRequest,
+  annotation,
+}: TransactionSignatureSummaryProps<AssetApproval>): ReactElement {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "signTransaction.spendApproval",
+  })
+
+  const { t: sharedT } = useTranslation("translation", { keyPrefix: "shared" })
+
+  const dispatch = useDispatch()
+  const {
+    assetAmount: { asset, amount: approvalLimit },
+    spender: { address: spenderAddress },
+  } = annotation
+  // `null` means no limit
+  const approvalLimitString = isMaxUint256(approvalLimit)
+    ? null
+    : fixedPointNumberToString({
+        amount: approvalLimit,
+        decimals: asset.decimals,
+      })
+
+  const approvalLimitDisplayValue = `${
+    approvalLimitString ?? "Infinite"
+  } ${asset.symbol.toUpperCase()}`
+
+  const [approvalLimitInput, setApprovalLimitInput] = useState<string | null>(
+    null
+  )
+
+  const [hasError, setHasError] = useState(false)
+
+  const changing = approvalLimitInput !== null
+
+  const handleChangeClick = () => {
+    setApprovalLimitInput(approvalLimitString ?? "")
+  }
+
+  const handleCancelClick = () => {
+    setApprovalLimitInput(null)
+  }
+
+  const handleSaveClick = () => {
+    if (!changing) return
+
+    if (
+      approvalLimitInput === "" ||
+      approvalLimitInput === approvalLimitString
+    ) {
+      return
+    }
+
+    const decimalAmount = parseToFixedPointNumber(approvalLimitInput)
+
+    if (
+      decimalAmount === undefined ||
+      (decimalAmount !== null && decimalAmount.amount < 0n)
+    ) {
+      setHasError(true)
+      return
+    }
+
+    const bigintAmount =
+      decimalAmount === null
+        ? ethers.constants.MaxUint256.toBigInt()
+        : convertFixedPointNumber(decimalAmount, asset.decimals).amount
+
+    setApprovalLimitInput(null)
+
+    const updatedInput = ERC20_INTERFACE.encodeFunctionData(
+      ERC20_FUNCTIONS.approve,
+      [spenderAddress, hexlify(bigintAmount)]
+    )
+    dispatch(
+      updateTransactionData({
+        ...transactionRequest,
+        input: updatedInput,
+      })
+    )
+  }
+
+  return (
+    <>
+      <h1 className="serif_header title">{t("title")}</h1>
+      <TransactionSignatureSummaryBody>
+        <div className="spend_destination_icons">
+          <div className="site_icon" />
+          <div className="asset_icon_wrap">
+            <SharedAssetIcon
+              size="large"
+              symbol={asset.symbol}
+              logoURL={asset.metadata?.logoURL}
+            />
+          </div>
+        </div>
+        <span className="site">
+          <Trans
+            t={t}
+            i18nKey="approveSpender"
+            components={{
+              spenderAddress: (
+                <SharedAddress
+                  address={spenderAddress}
+                  name={
+                    annotation.spender.annotation.nameRecord?.resolved
+                      ?.nameOnNetwork.name
+                  }
+                />
+              ),
+            }}
+          />
+        </span>
+        <span className="spending_label">
+          {t("spendAmount", { assetSymbol: asset.symbol })}
+        </span>
+        <form onSubmit={(event) => event.preventDefault()}>
+          <div className="spend_limit_header">
+            <span className="spend_limit_label">{t("spendLimit")}</span>
+            <SharedTooltip width={250}>
+              <p className="spend_limit_tooltip">{t("tooltip1")}</p>
+              <p className="spend_limit_tooltip">{t("tooltip2")}</p>
+            </SharedTooltip>
+          </div>
+          {changing ? (
+            <div>
+              <SharedInput
+                label=""
+                value={approvalLimitInput}
+                placeholder={approvalLimitString ?? ""}
+                onChange={(value) => {
+                  setApprovalLimitInput(value)
+                  setHasError(false)
+                }}
+                errorMessage={hasError ? t("invalidAmount") : undefined}
+                autoSelect
+              />
+              <div
+                className={classNames("change_limit_actions", {
+                  has_error: hasError,
+                })}
+              >
+                <SharedButton
+                  size="small"
+                  type="tertiary"
+                  onClick={handleCancelClick}
+                >
+                  {sharedT("cancelBtn")}
+                </SharedButton>
+                <SharedButton
+                  size="small"
+                  isFormSubmit
+                  type="tertiary"
+                  onClick={handleSaveClick}
+                  isDisabled={
+                    approvalLimitInput === "" ||
+                    approvalLimitInput === approvalLimitString
+                  }
+                >
+                  {sharedT("saveBtn")}
+                </SharedButton>
+              </div>
+            </div>
+          ) : (
+            <>
+              <span
+                className={classNames("spend_amount", {
+                  has_error: approvalLimitString === null,
+                })}
+              >
+                {approvalLimitDisplayValue}
+              </span>
+              <SharedButton
+                size="small"
+                isFormSubmit
+                type="tertiary"
+                onClick={handleChangeClick}
+              >
+                {t("changeLimit")}
+              </SharedButton>
+            </>
+          )}
+        </form>
+        <div className="spacer" />
+      </TransactionSignatureSummaryBody>
+      <style jsx>
+        {`
+          .site_icon {
+            background: url("./images/dapp_favicon_default@2x.png");
+            background-size: cover;
+            width: 48px;
+            height: 48px;
+            margin-right: -16px;
+          }
+          .spend_destination_icons {
+            display: flex;
+            margin-top: 22px;
+            margin-bottom: 16px;
+          }
+          .asset_icon_wrap {
+            z-index: 1;
+          }
+          .site {
+            color: #fff;
+            font-size: 16px;
+            font-weight: 500;
+            line-height: 24px;
+            text-align: center;
+          }
+          .spending_label {
+            width: 272px;
+            color: var(--green-40);
+            font-size: 16px;
+            line-height: 24px;
+            text-align: center;
+            border-bottom: 1px solid var(--green-120);
+            padding-bottom: 16px;
+            margin-bottom: 16px;
+            text-align: center;
+          }
+          .spend_limit_header {
+            display: flex;
+            justify-content: center;
+          }
+          .spend_limit_label {
+            color: var(--green-40);
+            font-size: 14px;
+            line-height: 16px;
+            margin-bottom: 4px;
+          }
+          .spend_limit_tooltip {
+            margin: 0 0 5px;
+          }
+          .spend_limit_tooltip:last-child {
+            margin-bottom: 0;
+          }
+          .change_limit_actions {
+            display: flex;
+            justify-content: space-between;
+          }
+          .change_limit_actions.has_error {
+            margin-top: 24px;
+          }
+          .spend_amount {
+            color: #fff;
+            font-size: 16px;
+            line-height: 24px;
+          }
+          .spend_amount.has_error {
+            color: var(--error);
+          }
+          .spacer {
+            margin-bottom: 18px;
+          }
+        `}
+      </style>
+    </>
+  )
+}
