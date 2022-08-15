6fb77142c047f0f36cc15ee4b0c60c0253895649
Matt Luongo <mhluongo@gmail.com>
2022-08-14 22:56:01 -0400

    Wrap the 4byte.directory function signature API
    Refs #492

---
+++ background/lib/four-byte.ts
@@ -0,0 +1,57 @@
+import { fetchJson } from "@ethersproject/web"
+import { HexString } from "../types"
+import logger from "./logger"
+
+export type FourByteSignature = {
+  id: string
+  createdAt: string
+  functionSignature: string
+  functionSelector: string
+}
+
+/**
+ * Look up and retrieve a human-readable function signature from 4byte.directory.
+ *
+ * Learn more at https://www.4byte.directory/docs/
+ *
+ * @param selector the hex function selector found in EVM transactions
+ */
+export async function lookupFunctionSelector(
+  selector: HexString
+): Promise<FourByteSignature | null> {
+  try {
+    const results = await fetchJson(
+      `https://www.4byte.directory/api/v1/signatures/?hex_signature=${selector}`
+    )
+    if (
+      "count" in results &&
+      results.count !== 0 &&
+      "results" in results &&
+      results.results.length > 0
+    ) {
+      const {
+        id,
+        created_at: createdAt,
+        function_signature: functionSignature,
+        hex_signature: functionSelector,
+      } = results.results[0] as {
+        id: string
+        created_at: string
+        function_signature: string
+        hex_signature: string
+      }
+      return {
+        id,
+        createdAt,
+        functionSignature,
+        functionSelector,
+      }
+    }
+  } catch (err) {
+    logger.error(
+      "Error looking up function selector from 4byte.directory.",
+      err
+    )
+  }
+  return null
+}
