057e99ee1ad8015d454953e67032c24d561e1d93
Matt Luongo <mhluongo@gmail.com>
2022-08-21 21:53:02 -0400

    Limit function selector lookups to first 4 bytes
    It's easier on the API consumer if they can send along an arbitrary
    string.

--- background/lib/four-byte.ts
+++ background/lib/four-byte.ts
@@ -19,39 +19,44 @@ export type FourByteSignature = {
 export async function lookupFunctionSelector(
   selector: HexString
 ): Promise<FourByteSignature | null> {
-  try {
-    const results = await fetchJson(
-      `https://www.4byte.directory/api/v1/signatures/?hex_signature=${selector}`
-    )
-    if (
-      "count" in results &&
-      results.count !== 0 &&
-      "results" in results &&
-      results.results.length > 0
-    ) {
-      const {
-        id,
-        created_at: createdAt,
-        function_signature: functionSignature,
-        hex_signature: functionSelector,
-      } = results.results[0] as {
-        id: string
-        created_at: string
-        function_signature: string
-        hex_signature: string
+  if (selector.length >= 10) {
+    try {
+      const results = await fetchJson(
+        `https://www.4byte.directory/api/v1/signatures/?hex_signature=${selector.slice(
+          0,
+          10
+        )}`
+      )
+      if (
+        "count" in results &&
+        results.count !== 0 &&
+        "results" in results &&
+        results.results.length > 0
+      ) {
+        const {
+          id,
+          created_at: createdAt,
+          function_signature: functionSignature,
+          hex_signature: functionSelector,
+        } = results.results[0] as {
+          id: string
+          created_at: string
+          function_signature: string
+          hex_signature: string
+        }
+        return {
+          id,
+          createdAt,
+          functionSignature,
+          functionSelector,
+        }
       }
-      return {
-        id,
-        createdAt,
-        functionSignature,
-        functionSelector,
-      }
+    } catch (err) {
+      logger.error(
+        "Error looking up function selector from 4byte.directory.",
+        err
+      )
     }
-  } catch (err) {
-    logger.error(
-      "Error looking up function selector from 4byte.directory.",
-      err
-    )
   }
   return null
 }
