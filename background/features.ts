9af9e3b532a770a57359d14a0fa9eb6fde533467
Matt Luongo <mhluongo@gmail.com>
2022-08-30 18:00:38 -0400

    Put tabbed onboarding behind a feature flag
    Refs #2025

--- background/features.ts
+++ background/features.ts
@@ -15,3 +15,5 @@ export const SUPPORT_NFTS = process.env.SUPPORT_NFTS === "true"
 export const SUPPORT_MULTIPLE_LANGUAGES =
   process.env.SUPPORT_MULTIPLE_LANGUAGES === "true"
 export const SUPPORT_ANALYTICS = process.env.SUPPORT_ANALYTICS === "true"
+export const SUPPORT_TABBED_ONBOARDING =
+  process.env.SUPPORT_TABBED_ONBOARDING === "true"
--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,8 @@
-import browser from "webextension-polyfill"
-import { startMain } from "@tallyho/tally-background"
+import { browser, startMain } from "@tallyho/tally-background"
+import { SUPPORT_TABBED_ONBOARDING } from "@tallyho/tally-background/features"
 
 browser.runtime.onInstalled.addListener((obj) => {
-  if (obj.reason === "install") {
+  if (obj.reason === "install" && SUPPORT_TABBED_ONBOARDING) {
     const url = browser.runtime.getURL("onboarding.html")
     browser.tabs.create({ url })
   }
