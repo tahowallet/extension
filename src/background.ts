15d79bdc87cb5211b0bcee2681abd26d23c0cf38
Matt Luongo <mhluongo@gmail.com>
2022-08-30 17:51:49 -0400

    Open an onboarding tab on extension install
    I duplicated the HTML endpoint that we're using for Ledger in the hope
    that we can split those codepaths down the road.

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,11 @@
-import { startApi } from "@tallyho/tally-background"
+import browser from "webextension-polyfill"
+import { startMain } from "@tallyho/tally-background"
 
-startApi()
+browser.runtime.onInstalled.addListener((obj) => {
+  if (obj.reason === "install") {
+    const url = browser.runtime.getURL("onboarding.html")
+    browser.tabs.create({ url })
+  }
+})
+
+startMain()
