786d587067c2d714fda329503eddcd4b0b6112a4
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2022-09-01 17:53:02 -0500

    2011: inline all styles for KeyringSetPassword.tsx and KeyringChangePassword.tsx

--- ui/components/Keyring/KeyringChangePassword.tsx
+++ ui/components/Keyring/KeyringChangePassword.tsx
@@ -12,7 +12,6 @@ import SharedButton from "../Shared/SharedButton"
 import SharedInput from "../Shared/SharedInput"
 import SharedBackButton from "../Shared/SharedBackButton"
 import PasswordStrengthBar from "../Password/PasswordStrengthBar"
-import styles from "./styles"
 
 export default function KeyringChangePassword(): ReactElement {
   const [currentPassword, setCurrentPassword] = useState("")
@@ -151,7 +150,59 @@ export default function KeyringChangePassword(): ReactElement {
       </form>
       <style jsx>{`
         ${titleStyle}
-        ${styles}
+        .top {
+          display: flex;
+          width: 100%;
+        }
+        .wordmark {
+          background: url("./images/wordmark@2x.png");
+          background-size: cover;
+          width: 95px;
+          height: 25px;
+          position: absolute;
+          left: 0;
+          right: 0;
+          margin: 0 auto;
+        }
+        .serif_header {
+          width: 335px;
+          text-align: center;
+          margin-top: 40px;
+          margin-bottom: 25px;
+        }
+        section {
+          padding-top: 25px;
+          background-color: var(--hunter-green);
+        }
+        .input_wrap {
+          width: 211px;
+          padding-top: 10px;
+        }
+        .strength_bar_wrap {
+          width: 211px;
+          height: 26px;
+          box-sizing: border-box;
+          padding-top: 10px;
+        }
+        .input_wrap_padding_bottom {
+          padding-bottom: 30px;
+        }
+        .set_as_default_ask {
+          display: flex;
+          width: 262px;
+          justify-content: space-between;
+          align-items: center;
+          color: var(--green-20);
+          font-weight: 500;
+        }
+        .restore {
+          display: none;
+          position: fixed;
+          bottom: 26px;
+        }
+        .button_wrap {
+          padding-top: 10px;
+        }
       `}</style>
     </section>
   )
