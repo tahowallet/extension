76b658a3be4277b7623e94dd241dd56235cecca7
Daedalus <0xDaedalus@users.noreply.github.com>
2022-09-03 10:35:27 -0600

    Add integration test for chainService.handlePendingTransaction

---
+++ background/services/chain/tests/index.integration.test.ts
@@ -0,0 +1,96 @@
+import sinon from "sinon"
+import ChainService from ".."
+import { OPTIMISM } from "../../../constants"
+import {
+  AnyEVMTransaction,
+  TransactionRequest,
+  TransactionRequestWithNonce,
+} from "../../../networks"
+import {
+  createAnyEVMTransaction,
+  createChainService,
+  createLegacyTransactionRequest,
+} from "../../../tests/factories"
+import SerialFallbackProvider from "../serial-fallback-provider"
+
+type ChainServiceExternalized = Omit<ChainService, ""> & {
+  handlePendingTransaction: (transaction: AnyEVMTransaction) => void
+  populateEVMTransactionNonce: (
+    transactionRequest: TransactionRequest
+  ) => Promise<TransactionRequestWithNonce>
+  evmChainLastSeenNoncesByNormalizedAddress: {
+    [chainID: string]: { [normalizedAddress: string]: number }
+  }
+}
+
+describe("Chain Service", () => {
+  const sandbox = sinon.createSandbox()
+  let chainService: ChainService
+
+  beforeEach(async () => {
+    sandbox.restore()
+    chainService = await createChainService()
+    await chainService.startService()
+  })
+  it("handlePendingTransactions should update nonce tracking, subscribe to transaction confirmations, and persist the transaction to indexedDB", async () => {
+    const chainServiceExternalized =
+      chainService as unknown as ChainServiceExternalized
+    const CHAIN_NONCE = 100
+    // Return a fake provider
+    const onceSpy = sandbox.spy()
+    const providerForNetworkOrThrow = sandbox
+      .stub(chainServiceExternalized, "providerForNetworkOrThrow")
+      .callsFake(
+        () =>
+          ({
+            getTransactionCount: async () => CHAIN_NONCE,
+            once: onceSpy,
+          } as unknown as SerialFallbackProvider)
+      )
+
+    const transactionRequestWithoutNonce = createLegacyTransactionRequest({
+      nonce: undefined,
+    })
+
+    // Populate EVM Transaction Nonce
+    await chainServiceExternalized.populateEVMTransactionNonce(
+      transactionRequestWithoutNonce
+    )
+
+    const { chainID, from, network } = transactionRequestWithoutNonce
+    expect(providerForNetworkOrThrow.called).toBe(true)
+    expect(
+      chainServiceExternalized.evmChainLastSeenNoncesByNormalizedAddress[
+        chainID
+      ][from]
+    ).toBe(CHAIN_NONCE)
+
+    const validOptimismEVMTransaction = createAnyEVMTransaction({
+      nonce: 101,
+      from,
+      network,
+    })
+
+    await chainServiceExternalized.handlePendingTransaction(
+      validOptimismEVMTransaction
+    )
+    // Handling a pending transaction should update the last seem EVM transaction nonce
+    expect(
+      chainServiceExternalized.evmChainLastSeenNoncesByNormalizedAddress[
+        chainID
+      ][validOptimismEVMTransaction.from]
+    ).toBe(validOptimismEVMTransaction.nonce)
+
+    // provider.once should be called inside of subscribeToTransactionConfirmation
+    // with the transaction hash
+    expect(onceSpy.called).toBe(true)
+
+    // Transaction should be persisted to the db
+    expect(
+      await chainServiceExternalized.getTransaction(
+        OPTIMISM,
+        validOptimismEVMTransaction.hash
+      )
+    ).toBeTruthy()
+  })
+})
