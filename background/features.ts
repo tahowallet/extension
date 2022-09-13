fb3f27e4c9077e96b716508f511484659c971555
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2022-09-13 17:25:26 +0200

    Remove CUSTOM_GAS_SELECT flag
    Remove CUSTOM_GAS_SELECT feature flag logic as it is
    `true` for some time now

--- background/features.ts
+++ background/features.ts
@@ -6,7 +6,6 @@ export const RESOLVE_RNS_NAMES = process.env.RESOLVE_RNS_NAMES === "true"
 export const SUPPORT_GOERLI = process.env.SUPPORT_GOERLI === "true"
 export const SUPPORT_ARBITRUM = process.env.SUPPORT_ARBITRUM === "true"
 export const SUPPORT_OPTIMISM = process.env.SUPPORT_OPTIMISM === "true"
-export const CUSTOM_GAS_SELECT = process.env.CUSTOM_GAS_SELECT === "true"
 export const HIDE_TOKEN_FEATURES = process.env.HIDE_TOKEN_FEATURES === "true"
 export const USE_UPDATED_SIGNING_UI =
   process.env.USE_UPDATED_SIGNING_UI === "true"
