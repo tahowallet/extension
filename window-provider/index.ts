e1dabb3a656fe72cfa05398b2690523a9857cfa4
Daedalus <0xDaedalus@users.noreply.github.com>
2022-09-16 07:37:34 -0600

    Add support for kwenta.io

--- window-provider/index.ts
+++ window-provider/index.ts
@@ -27,6 +27,7 @@ const impersonateMetamaskWhitelist = [
   "galxe.com",
   "dydx.exchange",
   "app.euler.finance",
+  "kwenta.io",
 ]
 
 export default class TallyWindowProvider extends EventEmitter {
