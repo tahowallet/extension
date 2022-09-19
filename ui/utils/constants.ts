698c7a27c3a4f5c36c79650f3725b37620f83937
Daedalus <0xDaedalus@users.noreply.github.com>
2022-09-19 15:17:33 -0400

    Move ui constants out of background folder and localize

--- ui/utils/constants.ts
+++ ui/utils/constants.ts
@@ -4,6 +4,7 @@ import {
   OPTIMISM,
   POLYGON,
 } from "@tallyho/tally-background/constants"
+import { i18n } from "../_locales/i18n"
 
 export const doggoTokenDecimalDigits = 18
 
@@ -15,4 +16,13 @@ export const scanWebsite = {
   },
   [POLYGON.chainID]: { title: "Polygonscan", url: "https://polygonscan.com" },
   [GOERLI.chainID]: { title: "Etherscan", url: "https://goerli.etherscan.io/" },
+}
+
+export const ESTIMATED_SPEED_IN_READABLE_FORMAT_RELATIVE_TO_CONFIDENCE_LEVEL: {
+  [confidence: number]: string
+} = {
+  0: i18n.t("networkFees.speeds.0"),
+  70: i18n.t("networkFees.speeds.70"),
+  95: i18n.t("networkFees.speeds.95"),
+  99: i18n.t("networkFees.speeds.99"),
 }
