9d4bb281f7af5d372a0fbe73705170b831de9419
Antonio Salazar Cardozo <antonio@thesis.co>
2022-09-19 22:33:43 -0400

    Use placeholder images for Ledger connection status
    These are used in the new signing flow, and were just empty. We now use
    the connected/disconnected images we use in Ledger onboarding, except
    the disconnected image covers a lot of different states that could use
    clearer images.

--- ui/components/Signing/Signer/SignerLedger/SignerLedgerConnectionStatus.tsx
+++ ui/components/Signing/Signer/SignerLedger/SignerLedgerConnectionStatus.tsx
@@ -14,17 +14,47 @@ export default function SignerLedgerConnectionStatus({
   switch (ledgerState.state) {
     case "available":
       if (mustEnableArbitraryDataSigning) {
-        return <img src="" alt="Ledger is connected but cannot sign" />
+        return (
+          <img
+            src="/images/connect_ledger_indicator_disconnected.svg"
+            alt="Ledger is connected but cannot sign"
+          />
+        )
       }
-      return <img src="" alt="Ledger is ready to sign" />
+      return (
+        <img
+          src="/images/connect_ledger_indicator_connected.svg"
+          alt="Ledger is ready to sign"
+        />
+      )
     case "no-ledger-connected":
-      return <img src="" alt="Ledger is disconnected" />
+      return (
+        <img
+          src="/images/connect_ledger_indicator_disconnected.svg"
+          alt="Ledger is disconnected"
+        />
+      )
     case "wrong-ledger-connected":
-      return <img src="" alt="Wrong Ledger connected" />
+      return (
+        <img
+          src="/images/connect_ledger_indicator_disconnected.svg"
+          alt="Wrong Ledger connected"
+        />
+      )
     case "multiple-ledgers-connected":
-      return <img src="" alt="Multiple ledgers connected" />
+      return (
+        <img
+          src="/images/connect_ledger_indicator_disconnected.svg"
+          alt="Multiple ledgers connected"
+        />
+      )
     case "busy":
-      return <img src="" alt="Ledger is busy" />
+      return (
+        <img
+          src="/images/connect_ledger_indicator_disconnected.svg"
+          alt="Ledger is busy"
+        />
+      )
     default:
       return assertUnreachable(ledgerState)
   }
