28e06846b5f161c1f972759a9cc91bd579fd619a
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2022-09-29 12:13:04 +0200

    Remove hover class for account avatar

--- ui/components/Shared/SharedCurrentAccountInformation.tsx
+++ ui/components/Shared/SharedCurrentAccountInformation.tsx
@@ -21,15 +21,9 @@ export default function SharedCurrentAccountInformation({
   const areKeyringsUnlocked = useAreKeyringsUnlocked(false)
   const icon = areKeyringsUnlocked ? "un-lock" : "lock"
   return (
-    <div className="account_info_wrap">
-      <span
-        className={classNames("account_info_label", {
-          hover_label: showHoverStyle,
-        })}
-      >
-        {name ?? shortenedAddress}
-      </span>
-      <div className={classNames("avatar", { hover_avatar: showHoverStyle })} />
+    <div className={classNames("account_info_wrap", { hover: showHoverStyle })}>
+      <span className="account_info_label">{name ?? shortenedAddress}</span>
+      <div className="avatar" />
       {showKeyring && (
         <div className="keyring_icon_wrap">
           <SharedIcon
@@ -56,13 +50,9 @@ export default function SharedCurrentAccountInformation({
             background: url("${avatarURL ?? "./images/portrait.png"}");
             background-color: var(--green-40);
             background-size: cover;
-            border: 2px solid var(--hunter-green);
           }
-          .hover_label:hover {
+          .hover:hover .account_info_label {
             color: var(--trophy-gold);
-          }
-          .avatar:hover {
-            border: 2px solid var(--trophy-gold);
           }
           .keyring_icon_wrap {
             background-color: var(--hunter-green);
