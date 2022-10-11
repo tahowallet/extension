121d1b6c81e0391d6584827929df6a7e6d158261
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2022-10-11 10:09:32 +0200

    Add a reload counter for rendering global error screen

--- src/popup.ts
+++ src/popup.ts
@@ -4,19 +4,39 @@ import {
 } from "@tallyho/tally-ui"
 import GlobalError from "@tallyho/tally-ui/components/GlobalError/GlobalError"
 
-let counter = 0
+enum NavigationTimingType {
+  "navigate",
+  "reload",
+  "back_forward",
+  "prerender",
+}
 
-const timerId = setInterval(() => {
-  counter += 1
-  if (counter > 2) {
-    clearInterval(timerId)
+setTimeout(() => {
+  const state = window.history?.state || {}
+  let reloadCount = state?.reloadCount || 0
+  const navigationItem = performance.getEntriesByType(
+    "navigation"
+  )[0] as PerformanceEntry & { type: NavigationTimingType }
+  if ((navigationItem?.type).toString() === "reload") {
+    reloadCount += 1
+    state.reloadCount = reloadCount
+    window.history.replaceState(state, "", document.URL)
+  } else if (reloadCount) {
+    delete state.reloadCount
+    reloadCount = 0
+    window.history.replaceState(state, "", document.URL)
+  }
+  if (reloadCount > 1) {
     attachUIToRootElement(GlobalError)
+    delete state.reloadCount
+    reloadCount = 0
+    window.history.replaceState(state, "", document.URL)
   } else {
     const isAppRendered = !!document.getElementsByClassName(
       "top_menu_wrap_decoy"
     ).length
-    if (isAppRendered) {
-      clearInterval(timerId)
+    if (!isAppRendered) {
+      window.location.reload()
     }
   }
 }, 1000)
