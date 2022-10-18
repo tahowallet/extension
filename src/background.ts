bf8d6c6bef85b3b34fab54721a8da50987b613b3
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2022-10-18 13:02:18 +0200

    Clean feature flags from the local storage on extension start

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,9 @@
 import { browser, startMain } from "@tallyho/tally-background"
-import { FeatureFlagTypes, isEnabled } from "@tallyho/tally-background/features"
+import {
+  FeatureFlagTypes,
+  isEnabled,
+  RuntimeFlag,
+} from "@tallyho/tally-background/features"
 
 browser.runtime.onInstalled.addListener((obj) => {
   if (
@@ -8,6 +12,18 @@ browser.runtime.onInstalled.addListener((obj) => {
   ) {
     const url = browser.runtime.getURL("tab.html#onboarding")
     browser.tabs.create({ url })
+  }
+
+  if (
+    obj.reason === "update" &&
+    !isEnabled(FeatureFlagTypes.SWITCH_RUNTIME_FLAGS) &&
+    !!Object.keys(RuntimeFlag).find(
+      (flagName) => localStorage.getItem(flagName) !== null
+    )
+  ) {
+    Object.keys(RuntimeFlag).forEach((flagName) =>
+      localStorage.removeItem(flagName)
+    )
   }
 })
 
