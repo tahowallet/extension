0feba3ce969279382a660137e29d294219ba10a8
Erik Zuuring <erik@circlesphere.co>
2022-10-25 12:45:25 +0300

    Remove steps from Intro.tsx
    - Removed initial steps from `intro.tsx` in favour of design changes.

--- ui/pages/Onboarding/Tabbed/Intro.tsx
+++ ui/pages/Onboarding/Tabbed/Intro.tsx
@@ -1,68 +1,6 @@
 import React, { ReactElement, useState } from "react"
 import { useRouteMatch, Redirect } from "react-router-dom"
-import { FeatureFlags, isEnabled } from "@tallyho/tally-background/features"
 import SharedButton from "../../../components/Shared/SharedButton"
-
-const steps = isEnabled(FeatureFlags.HIDE_TOKEN_FEATURES)
-  ? [
-      {
-        image: {
-          width: 120,
-          height: 120,
-          fileName: "doggo_grey",
-          extraStyles: `margin-top: 25px;`,
-        },
-        title: "Let's get you setup!",
-        body: "The community owned & operated wallet.",
-        buttonCopy: "Continue",
-      },
-      {
-        image: {
-          width: 267,
-          height: 251,
-          fileName: "illustration_onboarding_dao",
-          extraStyles: `margin-top: 21px;`,
-        },
-        title: "Tally Ho! is a DAO",
-        body: `That means Tally Ho is owned by our users. And all profits go straight to the community.`,
-        buttonCopy: "Continue",
-      },
-    ]
-  : [
-      {
-        image: {
-          width: 384,
-          height: 336,
-          fileName: "onboarding/graphic_different",
-          extraStyles: ``,
-        },
-        title: "Tally Ho! is different",
-        body: "Tally Ho! is the first community-owned wallet for Web3 and DeFi. If you own $DOGGO tokens, you are an owner. ",
-        buttonCopy: "Continue",
-      },
-      {
-        image: {
-          width: 384,
-          height: 336,
-          fileName: "onboarding/graphic_token",
-          extraStyles: ``,
-        },
-        title: "The $DOGGO token",
-        body: `You can earn $DOGGO in many ways! Check out the Earn and Swap tabs.`,
-        buttonCopy: "Continue",
-      },
-      {
-        image: {
-          width: 384,
-          height: 336,
-          fileName: "onboarding/graphic_drop",
-          extraStyles: ``,
-        },
-        title: "$DOGGO token drop",
-        body: `If you used Defi in the past, there is a chance you are part of the drop. Check by adding an existing account!`,
-        buttonCopy: "Get started",
-      },
-    ]
 
 export default function Intro(): ReactElement {
   const [redirectToAddWallet] = useState(false)
@@ -165,11 +103,10 @@ export default function Intro(): ReactElement {
             z-index: 1;
           }
           .illustration {
-            background: url("./images/${steps[activeStep - 1].image
-              .fileName}@2x.png");
+            background: url("./images/doggo_grey@2x.png");
             background-size: cover;
-            width: ${steps[activeStep - 1].image.width}px;
-            height: ${steps[activeStep - 1].image.height}px;
+            width: 120px;
+            height: 120px;
             flex-shrink: 0;
             left: 0;
             right: 0;
@@ -177,7 +114,6 @@ export default function Intro(): ReactElement {
             margin-top: 0;
             position: absolute;
             animation: fadeIn ease 0.5s;
-            ${steps[activeStep - 1].image.extraStyles}
           }
           @keyframes fadeIn {
             0% {
