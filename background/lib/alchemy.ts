235d8ec5ada1156bef059ffe3813e61583d91467
Daedalus <0xDaedalus@users.noreply.github.com>
2022-10-25 15:34:46 -0600

    Fix tokenBalance filter to match alchemy API spec

--- background/lib/alchemy.ts
+++ background/lib/alchemy.ts
@@ -191,7 +191,11 @@ export async function getTokenBalances(
       // A hex value of 0x without any subsequent numbers generally means "no
       // value" (as opposed to 0) in Ethereum implementations, so filter it out
       // as effectively undefined.
-      .filter(({ tokenBalance }) => tokenBalance !== "0x")
+      .filter(
+        ({ tokenBalance }) =>
+          tokenBalance !==
+          "0x0000000000000000000000000000000000000000000000000000000000000000"
+      )
       .map((tokenBalance) => {
         let balance = tokenBalance.tokenBalance
         if (balance.length > 66) {
