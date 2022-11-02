be220b5b51fea54cddc0ba4a3696580289a91921
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2022-11-02 09:23:51 +0100

    Improvements in the sample tests
    * remove jest mock for i18n
    * add i18n import to jest file
    * move SharedDropdown tests to the test folder
    * refactor for SharedInput tests

--- ui/components/Shared/__tests__/SharedInput.test.tsx
+++ ui/components/Shared/__tests__/SharedInput.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, waitFor, cleanup } from "@testing-library/react"
+import { render, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import SharedInput from "../SharedInput"
 
@@ -7,23 +7,20 @@ const onChange = jest.fn()
 const text = "Test"
 
 describe("SharedInput", () => {
-  afterEach(() => {
-    cleanup()
-  })
-
   test("should render input element", () => {
     const ui = render(<SharedInput placeholder={text} onChange={onChange} />)
     const inputElement = ui.getByPlaceholderText(text)
+
     expect(inputElement).toBeInTheDocument()
   })
 
   test("should be able to type into input", async () => {
-    const value = "Text text"
+    const value = "Test text"
     const ui = render(<SharedInput placeholder={text} onChange={onChange} />)
     const inputElement = ui.getByPlaceholderText(text)
 
     await userEvent.click(inputElement)
-    userEvent.type(inputElement, value)
+    await userEvent.type(inputElement, value)
     await waitFor(() => expect(inputElement).toHaveValue(value))
   })
 })
--- ui/setupJest.env.ts
+++ ui/setupJest.env.ts
@@ -1,5 +1,2 @@
 import "@testing-library/jest-dom"
-
-jest.mock("react-i18next", () => ({
-  useTranslation: () => ({ t: (key: string) => key }),
-}))
+import "./_locales/i18n"
