dcd77a0b578fd37a82f987a9a730da4cf1e48057
Matt Luongo <mhluongo@gmail.com>
2022-11-05 21:22:42 -0400

    Add a feature flag for the simulations tab

--- background/features.ts
+++ background/features.ts
@@ -25,6 +25,7 @@ export const RuntimeFlag: Record<string, boolean> = {
   SUPPORT_RSK: process.env.SUPPORT_RSK === "true",
   SUPPORT_ACHIEVEMENTS_BANNER:
     process.env.SUPPORT_ACHIEVEMENTS_BANNER === "true",
+  SUPPORT_SIMULATION_TAB: process.env.SUPPORT_SIMULATION_TAB === "true",
 }
 
 /**
