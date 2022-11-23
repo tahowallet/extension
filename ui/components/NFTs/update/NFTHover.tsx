02e2855e640460efa48abadfe3467f356dac234d
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2022-11-23 15:12:15 +0100

    Add NFT items styles

--- ui/components/NFTs/update/NFTHover.tsx
+++ ui/components/NFTs/update/NFTHover.tsx
@@ -2,6 +2,37 @@
 import React, { ReactElement } from "react"
 import SharedIcon from "../../Shared/SharedIcon"
 
+const icons = {
+  close: {
+    icon: "close",
+    label: "close",
+    background: "var(--green-40)",
+    size: 12,
+    style: "",
+  },
+  expand: {
+    icon: "chevron",
+    label: "expand collection",
+    background: "var(--success)",
+    size: 12,
+    style: "margin-bottom: 3px;",
+  },
+  view: {
+    icon: "eye",
+    label: "view details",
+    background: "var(--trophy-gold)",
+    size: 22,
+    style: "",
+  },
+}
+
+const getIcon = (isCollection: boolean, isExpanded: boolean) => {
+  if (isCollection) {
+    return isExpanded ? icons.close : icons.expand
+  }
+  return icons.view
+}
+
 export default function NFTsHover(props: {
   isCollection?: boolean
   isExpanded?: boolean
@@ -9,24 +40,55 @@ export default function NFTsHover(props: {
 }): ReactElement {
   const { isCollection = false, isExpanded = false, onClick } = props
 
-  const label = isCollection
-    ? isExpanded
-      ? "close"
-      : "expand collection"
-    : "view details"
-  const icon = isCollection ? (isExpanded ? "close" : "chevron") : "eye"
+  const { icon, label, background, size, style } = getIcon(
+    isCollection,
+    isExpanded
+  )
+
   return (
     <button type="button" className="nft_hover" onClick={onClick}>
-      <div className="nft_hover_label">{label}</div>
-      <SharedIcon
-        icon={`${icon}@2x.png`}
-        width={24}
-        color="var(--hunter-green)"
-      />
+      <div>{label}</div>
+      <div className="nft_hover_icon">
+        <SharedIcon
+          icon={`${icon}@2x.png`}
+          width={size}
+          color="var(--hunter-green)"
+          customStyles={style}
+        />
+      </div>
       <style jsx>{`
         .nft_hover {
+          opacity: ${isExpanded ? 1 : 0};
+          position: absolute;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          background: rgba(0, 20, 19, 0.75); // --green-120
+          border-radius: 4px;
+          backdrop-filter: blur(2px);
+          font-size: 14px;
+          font-weight: 500;
+          line-height: 16px;
+          letter-spacing: 3%;
+          z-index: 1;
+          transition: opacity 200ms ease-in-out;
         }
-        .nft_hover_label {
+        .nft_hover:hover {
+          opacity: 1;
+        }
+        .nft_hover_icon {
+          margin-top: 6px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: ${background};
+          width: 32px;
+          height: 32px;
+          border-radius: 100%;
         }
       `}</style>
     </button>
--- ui/components/NFTs/update/NFTItem.tsx
+++ ui/components/NFTs/update/NFTItem.tsx
@@ -3,6 +3,7 @@ import { NFT } from "@tallyho/tally-background/nfts"
 import { NFTCollectionCached } from "@tallyho/tally-background/redux-slices/nfts_update"
 import NFTImage from "./NFTImage"
 import NFTHover from "./NFTHover"
+import SharedNetworkIcon from "../../Shared/SharedNetworkIcon"
 
 export default function NFTsItem<T extends NFT | NFTCollectionCached>(props: {
   item: T
@@ -12,15 +13,26 @@ export default function NFTsItem<T extends NFT | NFTCollectionCached>(props: {
 }): ReactElement {
   const { onClick, isCollection = false, isExpanded = false, item } = props
   const { name = "No title", network, thumbnail } = item
-
+  const floorPrice =
+    "floorPrice" in item && item.floorPrice?.value && item.floorPrice
+  const nftsCount = "nfts" in item && item.nfts.length
   return (
     <div className="nft_item">
-      <div>
-        <NFTImage src={thumbnail} alt={name} width={100} />
-        <div className="nft_network" />
-        {"floorPrice" in item && (
-          <div className="nft_item_price">{item.floorPrice}</div>
-        )}
+      <div className="nft_image">
+        <NFTImage src={thumbnail} alt={name} width={168} />
+        <div className="nft_image_details">
+          <SharedNetworkIcon
+            network={network}
+            size={24}
+            hasBackground
+            backgroundOpacity={0.75}
+          />
+          {!!floorPrice && (
+            <div className="nft_item_price">
+              ~{`${floorPrice.value} ${floorPrice.tokenSymbol}`}
+            </div>
+          )}
+        </div>
         <NFTHover
           onClick={() => onClick(item)}
           isCollection={isCollection}
@@ -28,28 +40,45 @@ export default function NFTsItem<T extends NFT | NFTCollectionCached>(props: {
         />
       </div>
       <div className="nft_item_details">
-        <span className="nft_item_name">{name}</span>
-        {"nfts" in item && <span>({item.nfts.length})</span>}
+        <span className="ellipsis">{name}</span>
+        {!!nftsCount && <span className="nft_item_count">({nftsCount})</span>}
       </div>
       <style jsx>{`
         .nft_item {
+          width: 168px;
         }
         .nft_item_price {
+          background: rgba(1, 56, 52, 0.75); // --green-95
+          padding: 4px 5px;
+          font-weight: 500;
+          font-size: 14px;
+          line-height: 16px;
+          letter-spacing: 3%;
+          border-radius: 4px;
+        }
+        .nft_image {
+          position: relative;
+        }
+        .nft_image_details {
+          position: absolute;
+          top: 0;
+          width: 100%;
+          padding: 4px;
+          display: flex;
+          justify-content: space-between;
+          width: calc(100% - 8px);
         }
         .nft_item_details {
+          display: flex;
+          font-size: 14px;
+          font-weight: 500;
+          line-height: 16px;
+          margin-top: 8px;
         }
-        .nft_item_name {
-        }
-
-        // TODO: name network icon component
-        .nft_network {
-          background: url("./images/networks/${network.name
-            ?.replaceAll(" ", "")
-            .toLowerCase()}-square@2x.png");
-          background-size: cover;
-          height: 16px;
-          width: 16px;
-          border-radius: 4px;
+        .nft_item_count {
+          flex-shrink: 0;
+          color: var(--green-40);
+          margin-left: 3px;
         }
       `}</style>
     </div>
