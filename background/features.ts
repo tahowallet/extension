ebacae16af5c827e7b4e60157bba88252501b8b3
Gergo Nagy <gergo.nagy@tally.cash>
2022-11-28 10:46:13 +0100

    add wallet connect feature flag

--- background/features.ts
+++ background/features.ts
@@ -31,6 +31,7 @@ export const RuntimeFlag = {
   SUPPORT_ACHIEVEMENTS_BANNER:
     process.env.SUPPORT_ACHIEVEMENTS_BANNER === "true",
   SUPPORT_NFT_TAB: process.env.SUPPORT_NFT_TAB === "true",
+  SUPPORT_WALLET_CONNECT: process.env.SUPPORT_NFT_TAB === "true",
 } as const
 
 type BuildTimeFlagType = keyof typeof BuildTimeFlag
