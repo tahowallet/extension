9ae9842e4063baac9e96497244886a849616627c
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2022-12-05 11:11:24 +0100

    Add NFTs and Badges tabs headers

--- ui/pages/NFTs.tsx
+++ ui/pages/NFTs.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useState } from "react"
 import classNames from "classnames"
 import { selectNFTsCount } from "@tallyho/tally-background/redux-slices/selectors"
+import { useTranslation } from "react-i18next"
 import SharedPanelSwitcher from "../components/Shared/SharedPanelSwitcher"
 import NFTsExploreBanner from "../components/NFTS_update/NFTsExploreBanner"
 import NFTsHeader from "../components/NFTS_update/NFTsHeader"
@@ -12,6 +13,9 @@ const PANEL_NAMES = ["NFTs", "Badges"]
 
 export default function NFTs(): ReactElement {
   const nftCount = useBackgroundSelector(selectNFTsCount)
+  const { t } = useTranslation("translation", {
+    keyPrefix: "nfts",
+  })
 
   const [panelNumber, setPanelNumber] = useState(0)
 
@@ -32,13 +36,19 @@ export default function NFTs(): ReactElement {
       <div className="standard_width">
         {panelNumber === 0 &&
           (nftCount > 0 ? (
-            <NFTListPortfolio />
+            <>
+              <h2>{t("units.collection_other")}</h2>
+              <NFTListPortfolio />
+            </>
           ) : (
             <NFTsExploreBanner type="nfts" />
           ))}
         {panelNumber === 1 &&
           (nftCount > 0 ? (
-            <NFTListPortfolioBadges />
+            <>
+              <h2>{t("units.badge_other")}</h2>
+              <NFTListPortfolioBadges />
+            </>
           ) : (
             <NFTsExploreBanner type="badge" />
           ))}
@@ -57,6 +67,12 @@ export default function NFTs(): ReactElement {
           }
           .panel_switcher_wrap.margin {
             margin-bottom: 16px;
+          }
+          .page_content h2 {
+            font-weight: 600;
+            font-size: 18px;
+            line-height: 24px;
+            margin: 10px 0 0;
           }
         `}
       </style>
