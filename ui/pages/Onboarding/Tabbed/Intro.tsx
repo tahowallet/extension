2c7de92f6dd9d41cc03053258a52632a6b7deb2b
hyphenized <28708889+hyphenized@users.noreply.github.com>
2022-12-07 23:53:30 -0500

    Fix styles for tabbed onboarding right panel intro

--- ui/pages/Onboarding/Tabbed/Intro.tsx
+++ ui/pages/Onboarding/Tabbed/Intro.tsx
@@ -12,102 +12,83 @@ export default function Intro(): ReactElement {
   }
 
   return (
-    <section>
-      <div className="illustration_section">
+    <section className="fadeIn">
+      <header>
         <div className="illustration" />
-      </div>
-      <div className="bottom_part">
-        <div className="bottom_content">
-          <h1 className="bottom_title">Let&apos;s get you setup!</h1>
-        </div>
-        <div className="button_container">
-          <SharedButton
-            type="primary"
-            size="large"
-            linkTo={`${path}/add-wallet`}
-          >
-            <div className="option standard_width">Use existing wallet</div>
-          </SharedButton>
-          <SharedButton
-            type="secondary"
-            size="large"
-            linkTo={`${path}/new-seed/set-password`}
-          >
-            <div className="option standard_width">Create new wallet</div>
-          </SharedButton>
-        </div>
+        <h1>Let&apos;s get you setup!</h1>
+      </header>
+      <div className="actions">
+        <SharedButton
+          type="primary"
+          size="large"
+          linkTo={`${path}/add-wallet`}
+          center
+          style={{
+            fontSize: "20px",
+            lineHeight: "24px",
+            fontWeight: 500,
+          }}
+        >
+          Use existing wallet
+        </SharedButton>
+        <SharedButton
+          type="secondary"
+          size="large"
+          linkTo={`${path}/new-seed/set-password`}
+          center
+          style={{
+            fontSize: "20px",
+            lineHeight: "24px",
+            fontWeight: 500,
+          }}
+        >
+          Create new wallet
+        </SharedButton>
       </div>
       <style jsx>
         {`
-          .bottom_content {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-          }
-          .button_container {
-            border-radius: 1em;
-            background: var(--green-95);
-            padding: 1em;
-            margin: 3em 0;
-            width: 100%;
-          }
-          .top {
-            display: none;
-          }
           section {
+            max-width: 348px;
+            margin: 0 auto;
             display: flex;
             flex-direction: column;
+            gap: 65px;
             justify-content: center;
             align-items: center;
+            --fade-in-duration: 300ms;
           }
-          h1 {
-            font-family: "Quincy CF";
-            font-weight: 500;
-            font-size: 46px;
-            line-height: 42px;
-            margin: 12px 0px 0px 0px;
-          }
-          p {
-            font-size: 16px;
-            font-weight: 400;
-            line-height: 24px;
-            color: var(--green-40);
-            width: 336px;
-            text-align: center;
-            margin-top: 5px;
-          }
-          .bottom_part {
-            display: flex;
-            margin-top: -38px;
-            flex-direction: column;
-            align-items: center;
-            justify-content: space-between;
-            height: 230px;
-            text-align: center;
-            z-index: 1;
-          }
-          .illustration_section {
-            height: 180px;
-            display: flex;
-            position: relative;
-          }
+
           .illustration {
             background: url("./images/doggo_intro.svg");
             background-size: cover;
-            width: 120px;
-            height: 120px;
-            flex-shrink: 0;
+            width: 80px;
+            height: 80px;
             margin: 0 auto;
-            margin-top: 0;
-            animation: fadeIn ease 0.5s;
           }
-          @keyframes fadeIn {
-            0% {
-              opacity: 0;
-            }
-            100% {
-              opacity: 1;
-            }
+
+          header {
+            display: flex;
+            flex-direction: column;
+            gap: 16px;
+          }
+
+          header h1 {
+            font-family: "Quincy CF";
+            font-weight: 500;
+            font-size: 36px;
+            line-height: 42px;
+            margin: 0;
+          }
+
+          .actions {
+            width: 100%;
+            box-sizing: border-box;
+            border-radius: 16px;
+            background: var(--green-95);
+            padding: 32px;
+            display: flex;
+            flex-direction: column;
+            gap: 28px;
           }
         `}
       </style>
