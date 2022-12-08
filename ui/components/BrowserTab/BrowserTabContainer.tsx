a2a7bb3451217711f0fca843a3ecc6a086a2740c
hyphenized <28708889+hyphenized@users.noreply.github.com>
2022-12-07 23:36:57 -0500

    Add background gradients
    A smoother gradient was generated by applying an easing function over the color steps. Made with post css plugin `postcss-easing-gradients`

--- ui/components/BrowserTab/BrowserTabContainer.tsx
+++ ui/components/BrowserTab/BrowserTabContainer.tsx
@@ -7,18 +7,98 @@ export default function BrowserTabContainer({
 }): ReactElement {
   return (
     <>
-      <div className="container">{children}</div>
+      <div className="container">
+        <div className="cover gradient-1" />
+        <div className="cover gradient-2" />
+        <div className="cover gradient-3" />
+        {children}
+      </div>
+
       <style jsx>{`
         .container {
           height: 100%;
-          background: radial-gradient(
-            ellipse at right top,
-            rgba(36, 107, 103, 0.5) 0%,
-            rgba(0, 20, 19, 1) 100%
-          );
+          background: #041414;
           display: flex;
           flex-direction: column;
           align-items: center;
+        }
+        .cover {
+          position: absolute;
+          top: 0;
+          bottom: 0;
+          left: 0;
+          right: 0;
+        }
+
+        .gradient-3 {
+          --originX: calc(71.25vw / 2);
+          --originY: calc(97.3vh / 2);
+          --left: 39.7vw;
+          --top: 60.2vh;
+          background: radial-gradient(
+            ellipse var(--originX) var(--originY) at
+              calc(var(--left) + var(--originX))
+              calc(var(--top) + var(--originY)),
+            hsl(175.77, 49.65%, 28.04%),
+            hsla(175.77, 49.7%, 27.65%, 0.9657064) 13.11%,
+            hsla(175.75, 49.86%, 26.57%, 0.8737997) 24.72%,
+            hsla(175.71, 50.14%, 24.92%, 0.7407407) 35.26%,
+            hsla(175.66, 50.6%, 22.81%, 0.5829904) 45.16%,
+            hsla(175.58, 51.32%, 20.36%, 0.4170096) 54.84%,
+            hsla(175.46, 52.47%, 17.72%, 0.2592593) 64.74%,
+            hsla(175.29, 54.27%, 15.13%, 0.1262003) 75.28%,
+            hsla(175.1, 56.65%, 13.04%, 0.0342936) 86.89%
+          );
+          opacity: 0.5;
+        }
+
+        /* top left to bottom */
+        .gradient-2 {
+          --originX: calc(148vw / 2);
+          --left: 40vw;
+          --top: -67px;
+          background: radial-gradient(
+            circle var(--originX) at var(--left) var(--top),
+            hsl(175.77, 49.65%, 28.04%),
+            hsla(175.77, 49.68%, 27.81%, 0.9803241) 9.99%,
+            hsla(175.76, 49.77%, 27.18%, 0.9259259) 19.07%,
+            hsla(175.74, 49.92%, 26.2%, 0.84375) 27.44%,
+            hsla(175.71, 50.14%, 24.92%, 0.7407407) 35.26%,
+            hsla(175.68, 50.46%, 23.38%, 0.6238426) 42.72%,
+            hsla(175.62, 50.92%, 21.62%, 0.5) 50%,
+            hsla(175.55, 51.56%, 19.71%, 0.3761574) 57.28%,
+            hsla(175.46, 52.47%, 17.72%, 0.2592593) 64.74%,
+            hsla(175.33, 53.76%, 15.75%, 0.15625) 72.56%,
+            hsla(175.19, 55.43%, 13.98%, 0.0740741) 80.93%,
+            hsla(175.06, 57.2%, 12.67%, 0.0196759) 90.01%,
+            hsla(175, 58.06%, 12.16%, 0)
+          );
+        }
+
+        /* bottom right */
+        .gradient-1 {
+          --originX: calc(143.6vw / 2);
+          --originY: calc(196vh / 2);
+          --left: 37.2vw;
+          --top: -21vh;
+          background: radial-gradient(
+            ellipse var(--originX) var(--originY) at
+              calc(var(--left) + var(--originX))
+              calc(var(--top) + var(--originY)),
+            hsl(175.77, 49.65%, 28.04%),
+            hsla(175.77, 49.68%, 27.81%, 0.9803241) 9.99%,
+            hsla(175.76, 49.77%, 27.18%, 0.9259259) 19.07%,
+            hsla(175.74, 49.92%, 26.2%, 0.84375) 27.44%,
+            hsla(175.71, 50.14%, 24.92%, 0.7407407) 35.26%,
+            hsla(175.68, 50.46%, 23.38%, 0.6238426) 42.72%,
+            hsla(175.62, 50.92%, 21.62%, 0.5) 50%,
+            hsla(175.55, 51.56%, 19.71%, 0.3761574) 57.28%,
+            hsla(175.46, 52.47%, 17.72%, 0.2592593) 64.74%,
+            hsla(175.33, 53.76%, 15.75%, 0.15625) 72.56%,
+            hsla(175.19, 55.43%, 13.98%, 0.0740741) 80.93%,
+            hsla(175, 58.06%, 12.16%, 0) 90.01%
+          );
+          opacity: 0.45;
         }
       `}</style>
     </>
