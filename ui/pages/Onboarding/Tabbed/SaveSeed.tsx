aea2b00b4443d36f717544183cdac8b2205337ac
hyphenized <28708889+hyphenized@users.noreply.github.com>
2022-12-08 03:04:02 -0500

    Refactor SaveSeed
    - Fixed styles
    - Switched to a grid for rendering mnemonic words

--- ui/pages/Onboarding/Tabbed/SaveSeed.tsx
+++ ui/pages/Onboarding/Tabbed/SaveSeed.tsx
@@ -2,17 +2,13 @@ import React, { ReactElement, useState } from "react"
 import SharedButton from "../../../components/Shared/SharedButton"
 import OnboardingStepsIndicator from "../../../components/Onboarding/OnboardingStepsIndicator"
 import { useBackgroundSelector } from "../../../hooks"
-import {
-  OnboardingBox,
-  OnboardingSubheader,
-  OnboardingMessageHeader,
-} from "../styles"
+import OnboardingTip from "./OnboardingTip"
 
 function WarningMessage({ onAccept }: { onAccept: () => void }): ReactElement {
   return (
-    <div className="seed_section">
-      <h1 className="serif_header center_text">Before you start</h1>
-      <div className="onboarding_box">
+    <section className="step_content fadeIn">
+      <h1 className="center_text">Before we get started</h1>
+      <div className="message">
         <div className="message_header">
           <img
             className="message_icon"
@@ -23,42 +19,82 @@ function WarningMessage({ onAccept }: { onAccept: () => void }): ReactElement {
         <p>
           It&apos;s important to write down your secret recovery phrase and
           store it somewhere safe.
+          <br />
+          <br />
+          This is the only way to recover your accounts and funds.
+          <br />
+          <br />
+          <u>You will not be able to export your recovery phrase later.</u>
         </p>
-        <p>This is the only way to recover your accounts and funds.</p>
-        <p>You will not be able to export your recovery phrase later.</p>
       </div>
-      <SharedButton type="primary" size="large" onClick={onAccept}>
-        Create recovery phrase
-      </SharedButton>
+      <div className="cta">
+        <SharedButton type="primary" size="large" onClick={onAccept} center>
+          Create recovery phrase
+        </SharedButton>
+      </div>
+
+      <OnboardingTip>You can upgrade a view-only wallet later</OnboardingTip>
       <style jsx>{`
-        .onboarding_box {
-          ${OnboardingBox}
-          padding-top: 20px;
+        .step_content {
+          display: flex;
+          flex-direction: column;
+          align-items: stretch;
+          max-width: 380px;
+          margin: 0 auto;
         }
-        .onboarding_box p {
-          margin: 8px 0;
+
+        section h1 {
+          font-family: "Quincy CF";
+          font-style: normal;
+          font-weight: 500;
+          font-size: 36px;
+          line-height: 42px;
+          margin: 45px 0 48px;
         }
+
+        .message {
+          background: var(--green-95);
+          border-radius: 8px;
+          font-weight: 600;
+          color: var(--green-20);
+          padding: 24px 29px 32px;
+          margin: 15px 0 24px;
+          display: flex;
+          flex-direction: column;
+        }
+
+        .message p {
+          margin: 0;
+          font-family: "Segment";
+          font-style: normal;
+          font-weight: 600;
+          font-size: 18px;
+          line-height: 24px;
+          color: var(--green-20);
+          text-align: left;
+        }
+
         .message_header {
-          ${OnboardingMessageHeader}
+          display: flex;
+          align-items: center;
+          font-weight: 600;
+          font-size: 18px;
           color: var(--attention);
         }
+
         .message_icon {
           height: 54px;
           margin: 10px auto;
         }
-        .serif_header {
-          font-family: "Quincy CF";
-          font-weight: 500;
-          font-size: 46px;
-          line-height: 42px;
-          margin: 1em auto;
-        }
-        ul {
-          width: 200px;
-          margin: auto;
+
+        .cta {
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          margin-bottom: 40px;
         }
       `}</style>
-    </div>
+    </section>
   )
 }
 
@@ -68,117 +104,107 @@ function SeedContainer(): ReactElement {
   })
 
   return (
-    <>
-      <h1 className="serif_header center_text">
-        Save and store your recovery phrase
-      </h1>
-      <div className="words_group">
-        {freshMnemonic && (
-          <>
-            <div className="column_wrap">
-              <div className="column numbers">1 2 3 4 5 6 7 8 9 10 11 12</div>
-              <div className="column dashes">- - - - - - - - - - - -</div>
-              <div className="column words">
-                {freshMnemonic?.slice(0, 12).map((word) => {
-                  return (
-                    <React.Fragment key={word}>
-                      <input type="password" placeholder={word} disabled />
-                      <br />
-                    </React.Fragment>
-                  )
-                })}
+    <section className="fadeIn">
+      <h1 className="center_text">Save and store your recovery phrase</h1>
+      <div className="step_content">
+        <div className="seed_phrase">
+          {freshMnemonic?.map((word, i) => {
+            // It's safe to use index as key here
+            const key = `${word}-${i}`
+            return (
+              <div className="word" key={key}>
+                <i>-</i>
+                {word}
               </div>
-            </div>
-            <div className="column_wrap">
-              <div className="column numbers">
-                13 14 15 16 17 18 19 20 21 22 23 24
-              </div>
-              <div className="column dashes">- - - - - - - - - - - -</div>
-              <div className="column words">
-                {freshMnemonic?.slice(12, 24).map((word) => {
-                  return (
-                    <React.Fragment key={word}>
-                      <input type="password" placeholder={word} disabled />
-                      <br />
-                    </React.Fragment>
-                  )
-                })}
-              </div>
-            </div>
-          </>
-        )}
-      </div>
-      <div className="button_group">
+            )
+          })}
+        </div>
         <SharedButton
           type="primary"
           size="medium"
-          iconMedium="continue"
           linkTo="/onboarding/new-seed/verify"
+          center
         >
           I wrote it down
         </SharedButton>
-        <SharedButton
-          type="tertiary"
-          size="small"
-          iconMedium="copy"
-          onClick={() =>
-            navigator.clipboard.writeText(freshMnemonic?.join(" ") ?? "")
-          }
-        >
-          Copy phrase to clipboard
-        </SharedButton>
+        <div className="copy_phrase">
+          <SharedButton
+            type="tertiary"
+            size="small"
+            iconMedium="copy"
+            onClick={() =>
+              navigator.clipboard.writeText(freshMnemonic?.join(" ") ?? "")
+            }
+            center
+          >
+            Copy phrase to clipboard
+          </SharedButton>
+        </div>
       </div>
       <style jsx>{`
-        .subtitle {
-          ${OnboardingSubheader}
-        }
-        .words_group {
-          ${OnboardingBox}
-          flex-direction: row;
-          justify-content: space-between;
-        }
-        .numbers {
-          width: 18px;
-          text-align: right;
-        }
-        .column {
-          color: var(--green-20);
-          font-weight: 600;
-          line-height: 32px;
-          text-align: right;
-        }
-        .column_wrap {
-          display: flex;
-          height: 382px;
-        }
-        .dashes {
-          width: 12px;
-          margin-right: 8px;
-          margin-left: 5px;
-        }
-        .words {
-          width: 69px;
-          text-align: left;
-        }
-        .button_group {
-          align-items: center;
-          height: 86px;
+        section {
           display: flex;
           flex-direction: column;
-          justify-content: space-between;
+          align-items: center;
         }
-        .serif_header {
+        .step_content{
+          max-width: 430px;
+          display: flex;
+          flex-direction: column;
+          gap: 24px;
+          justify-content: stretch;
+        }
+        h1 {
           font-family: "Quincy CF";
+          font-style: normal;
           font-weight: 500;
-          font-size: 46px;
+          font-size: 36px;
           line-height: 42px;
-          margin: 1em auto;
+          color: var(--white);
+          text-align: center;
+          margin-bottom: 27px;
+          margin-top 24px;
         }
-        input:disabled {
-          color: white;
+        .seed_phrase {
+          display: grid;
+          grid: repeat(8, 1fr) / auto-flow 1fr;
+          place-content: center;
+          counter-reset: step;
+          gap: 16px 24px;
+          padding: 16px;
+          background: var(--green-95);
+          border-radius: 8px;
+        }
+
+        .word::before {
+          width: 20px;
+          text-align: right;
+          content: counter(step);
+          counter-increment: step;
+        }
+
+        .word {
+          color: var(--green-20);
+          display: flex;
+          gap: 8px;
+          font-family: "Segment";
+          font-style: normal;
+          font-weight: 600;
+          font-size: 18px;
+          line-height: 27px;
+        }
+
+        .word i {
+          user-select: none;
+        }
+
+        .copy_phrase {
+          align-items: center;
+          display: flex;
+          flex-direction: column;
         }
       `}</style>
-    </>
+    </section>
   )
 }
 
@@ -188,7 +214,7 @@ export default function SaveSeed(): ReactElement {
   return (
     <div className="steps_section">
       <div className="steps_indicator">
-        <OnboardingStepsIndicator activeStep={1} />
+        <OnboardingStepsIndicator activeStep={revealSeed ? 1 : 0} />
       </div>
       {revealSeed ? (
         <SeedContainer />
@@ -197,16 +223,7 @@ export default function SaveSeed(): ReactElement {
       )}
       <style jsx>
         {`
-          .serif_header {
-            font-family: "Quincy CF";
-            font-weight: 500;
-            font-size: 46px;
-            line-height: 42px;
-            margin: 1em auto;
-          }
           .steps_section {
-            text-align: center;
-            width: 50%;
             margin: auto;
           }
           .steps_indicator {
