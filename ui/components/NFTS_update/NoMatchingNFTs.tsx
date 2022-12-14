be7630de2fb9bfa20fd32a9e9c4796ec922b64d6
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2022-12-14 09:21:40 +0100

    Create separate component when there is no matching NFTs

---
+++ ui/components/NFTS_update/NoMatchingNFTs.tsx
@@ -0,0 +1,36 @@
+import React, { ReactElement } from "react"
+import { useTranslation } from "react-i18next"
+import NFTsExploreBanner from "./NFTsExploreBanner"
+
+export default function NoMatchingNFTs(props: {
+  type: "badge" | "nfts"
+}): ReactElement {
+  const { type } = props
+  const { t } = useTranslation("translation", {
+    keyPrefix: "nfts",
+  })
+
+  return (
+    <div className="content">
+      <h2>
+        {t("noMatchingNFTs", { type: type === "nfts" ? "NFTs" : "badges" })}
+      </h2>
+      <NFTsExploreBanner type={type} />
+      <style jsx>{`
+        .content {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 32px;
+          margin-top: 38px;
+        }
+        .content h2 {
+          font-weight: 600;
+          font-size: 18px;
+          line-height: 24px;
+          margin: 0;
+        }
+      `}</style>
+    </div>
+  )
+}
