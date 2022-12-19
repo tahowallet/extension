803138d252ff404b65612da5b009b859ec5a4175
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2022-12-19 21:01:18 +0100

    Add custom style for shared radio button

--- ui/components/Shared/SharedRadio.tsx
+++ ui/components/Shared/SharedRadio.tsx
@@ -30,33 +30,46 @@ export default function SharedRadio({
         checked={checked}
         onChange={() => onChange(!checked)}
       />
-      <label htmlFor={id} className="label">
+      <label htmlFor={id} className="radio-label">
         {label}
       </label>
       <style jsx>{`
         .radio {
-          display: flex;
-          align-items: center;
-          position: relative;
-          font-size: 14px;
-          color: var(--green-60);
-          user-select: none;
           padding: 8px 0;
         }
-        input {
-          all: revert;
-          cursor: pointer;
-          margin: 0;
-          margin-right: 10px;
-          accent-color: var(--trophy-gold);
-        }
-        .label {
+        .radio-label {
+          display: flex;
+          flex-direction: row;
+          align-items: center;
           font-weight: 500;
           font-size: 14px;
           line-height: 16px;
           line-height: normal;
           color: var(--white);
           margin-top: 0;
+        }
+        .radio-label:before {
+          content: "";
+          border-radius: 100%;
+          display: inline-block;
+          width: 16px;
+          height: 16px;
+          position: relative;
+          margin-right: 8px;
+          vertical-align: top;
+          cursor: pointer;
+          text-align: center;
+          transition: all 250ms ease;
+          background-color: var(--green-80);
+          box-shadow: inset 0 0 0 2px var(--green-60);
+        }
+        input[type="radio"] {
+          position: absolute;
+          opacity: 0;
+        }
+        input:checked ~ .radio-label:before {
+          background-color: var(--green-95);
+          box-shadow: inset 0 0 0 5px var(--trophy-gold);
         }
       `}</style>
     </div>
