b13f8cba8938cbb56925e339fb2bc7da25f8173b
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2023-01-19 09:30:50 +0100

    Update icons for Abilities page

--- ui/pages/Abilities.tsx
+++ ui/pages/Abilities.tsx
@@ -1,17 +1,21 @@
 import { selectFilteredAbilities } from "@tallyho/tally-background/redux-slices/selectors"
 import React, { ReactElement } from "react"
+import { useTranslation } from "react-i18next"
 import { useSelector } from "react-redux"
 import AbilityCard from "./Abilities/AbilityCard"
 
 export default function Abilities(): ReactElement {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "overview.abilities",
+  })
   const abilities = useSelector(selectFilteredAbilities)
 
   return (
     <>
       <section className="standard_width_padded">
         <div className="title">
-          <div className="icon_daylight" />
-          <h1>Daylight Abilities!</h1>
+          <div className="icon_tail" />
+          <h1> {t("title")}</h1>
         </div>
         {abilities.map((ability) => (
           <AbilityCard key={ability.abilityId} ability={ability} />
@@ -24,18 +28,24 @@ export default function Abilities(): ReactElement {
             flex-direction: row;
             align-items: center;
           }
+          h1 {
+            font-weight: 500;
+            font-size: 22px;
+            line-height: 32px;
+          }
           section {
             display: flex;
             flex-flow: column;
             height: 544px;
             background-color: var(--hunter-green);
           }
-          .icon_daylight {
-            background: url("./images/assets/daylight.png");
-            background-size: 39px 22px;
-            width: 39px;
-            height: 22px;
-            margin-right: 8px;
+          .icon_tail {
+            background: url("./images/tail.svg");
+            background-size: 32px 32px;
+            width: 32px;
+            height: 32px;
+            margin-right: 16px;
+            border-radius: 24px;
           }
         `}
       </style>
