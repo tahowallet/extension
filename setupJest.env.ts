4ff6cc455dd0ee0d535b44e78fc28af0038d73ff
hyphenized <28708889+hyphenized@users.noreply.github.com>
2023-02-11 01:56:02 -0500

    Fix flaky tests and add utility to debug flaky tests

--- setupJest.env.ts
+++ setupJest.env.ts
@@ -7,3 +7,24 @@ beforeEach(() => {
 afterEach(() => {
   global.indexedDB = new IDBFactory()
 })
+
+it.flaky = function checkFlaky(label: string, testCase: () => unknown): void {
+  // eslint-disable-next-line no-only-tests/no-only-tests
+  it.only(label, () => {
+    const results = []
+    for (let i = 0; i < 2000; i += 1) {
+      results.push(testCase())
+    }
+    return Promise.all(results)
+  })
+}
+
+// eslint-disable-next-line @typescript-eslint/no-namespace, @typescript-eslint/no-unused-vars
+declare namespace jest {
+  interface It {
+    /**
+     * Used for debugging flaky tests
+     */
+    flaky: (label: string, testCase: () => unknown) => void
+  }
+}
