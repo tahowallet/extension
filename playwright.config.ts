0e5ed58c4f1da30a1ebe1220afcc033d37d57e84
Jorge Luis <28708889+hyphenized@users.noreply.github.com>
2023-02-22 14:56:09 -0500

    Fix env vars loading in e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,10 +2,11 @@ import type { PlaywrightTestConfig } from "@playwright/test"
 import { devices } from "@playwright/test"
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
+ * Read environment variables
  */
-// require('dotenv').config();
+import "dotenv-defaults/config"
+
+const CI_ENV = typeof process.env.CI === "string"
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -24,11 +25,11 @@ const config: PlaywrightTestConfig = {
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: CI_ENV,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: CI_ENV ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: CI_ENV ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
