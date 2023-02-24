be123899f292c37f8188d02f92dd66a62ac6d97d
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2023-02-24 12:49:50 +0100

    Block buttons when the window's focus changes
    This solution is moved from the old signing flow.

--- ui/components/Signing/Signer/SignerBaseFrame.tsx
+++ ui/components/Signing/Signer/SignerBaseFrame.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect, useState } from "react"
+import React, { ReactElement, useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
 import {
   selectIsTransactionLoaded,
@@ -21,24 +21,66 @@ export default function SignerBaseFrame({
   onReject,
 }: SignerBaseFrameProps): ReactElement {
   const { t } = useTranslation("translation", { keyPrefix: "signTransaction" })
-
   const transactionDetails = useBackgroundSelector(selectTransactionData)
+
   const isTransactionDataReady = useBackgroundSelector(
     selectIsTransactionLoaded
   )
   const hasInsufficientFunds =
     transactionDetails?.annotation?.warnings?.includes("insufficient-funds")
 
-  const [isOnDelayToSign /* , setIsOnDelayToSign */] = useState(false)
+  /*
+    Prevent shenanigans by disabling the sign button for a bit
+    when rendering new sign content or when changing window focus.
+  */
+  const delaySignButtonTimeout = useRef<number | undefined>()
+
+  const [isOnDelayToSign, setIsOnDelayToSign] = useState(false)
   // Debounced unlock buttons because dispatching transaction events is async and can happen in batches
   const [unlockButtons, setUnlockButtons] = useDebounce(
     isTransactionDataReady,
     300
   )
+  const [focusChangeNonce, setFocusChangeNonce] = useState(0)
+
+  function clearDelaySignButtonTimeout() {
+    if (typeof delaySignButtonTimeout.current !== "undefined") {
+      clearTimeout(delaySignButtonTimeout.current)
+      delaySignButtonTimeout.current = undefined
+    }
+  }
+
   useEffect(
     () => setUnlockButtons(isTransactionDataReady),
     [isTransactionDataReady, setUnlockButtons]
   )
+
+  useEffect(() => {
+    const increaseFocusChangeNonce = () => {
+      setFocusChangeNonce((x) => x + 1)
+    }
+    window.addEventListener("focus", increaseFocusChangeNonce)
+    window.addEventListener("blur", increaseFocusChangeNonce)
+
+    return () => {
+      window.removeEventListener("focus", increaseFocusChangeNonce)
+      window.removeEventListener("blur", increaseFocusChangeNonce)
+    }
+  }, [])
+
+  // Runs on updates
+  useEffect(() => {
+    clearDelaySignButtonTimeout()
+
+    if (document.hasFocus()) {
+      delaySignButtonTimeout.current = window.setTimeout(() => {
+        setIsOnDelayToSign(false)
+        // Random delay between 0.5 and 2 seconds
+      }, Math.floor(Math.random() * (5 - 1) + 1) * 500)
+    } else {
+      setIsOnDelayToSign(true)
+    }
+  }, [focusChangeNonce])
 
   return (
     <>
