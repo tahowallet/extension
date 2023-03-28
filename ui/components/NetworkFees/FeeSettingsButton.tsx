a0630ad9ae0657842179fb6119f38f656a026f31
Daedalus <0xDaedalus@users.noreply.github.com>
2023-03-28 11:08:41 -0600

    Disable network fees for custom networks.
    For networks that are not built in - disable adjusting network fees and add a tooltip explaining that we do not yet support changing gas on these networks.

--- ui/components/NetworkFees/FeeSettingsButton.tsx
+++ ui/components/NetworkFees/FeeSettingsButton.tsx
@@ -1,4 +1,10 @@
 import React, { ReactElement } from "react"
+import { isBuiltInNetwork } from "@tallyho/tally-background/constants"
+import { selectCurrentNetwork } from "@tallyho/tally-background/redux-slices/selectors"
+import { selectTransactionData } from "@tallyho/tally-background/redux-slices/selectors/transactionConstructionSelectors"
+import { useTranslation } from "react-i18next"
+import { useBackgroundSelector } from "../../hooks"
+import SharedTooltip from "../Shared/SharedTooltip"
 import FeeSettingsText from "./FeeSettingsText"
 
 interface FeeSettingsButtonProps {
@@ -8,6 +14,39 @@ interface FeeSettingsButtonProps {
 export default function FeeSettingsButton({
   onClick,
 }: FeeSettingsButtonProps): ReactElement {
+  const transactionData = useBackgroundSelector(selectTransactionData)
+  const selectedNetwork = useBackgroundSelector(selectCurrentNetwork)
+  const currentNetwork = transactionData?.network || selectedNetwork
+
+  const { t } = useTranslation()
+
+  if (!isBuiltInNetwork(currentNetwork)) {
+    return (
+      <div>
+        <SharedTooltip
+          width={175}
+          height={30}
+          type="dark"
+          horizontalShift={90}
+          horizontalPosition="center"
+          verticalPosition="bottom"
+          IconComponent={() => <FeeSettingsText />}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <div>{t("networkFees.settingsDisabledOne")}</div>
+            <div>{t("networkFees.settingsDisabledTwo")}</div>
+          </div>
+        </SharedTooltip>
+      </div>
+    )
+  }
+
   return (
     <button className="settings" type="button" onClick={onClick}>
       <FeeSettingsText />
