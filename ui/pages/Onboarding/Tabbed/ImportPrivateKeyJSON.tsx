f8a93a8b9218eef81ef37bbc1b96a6cda61fb97c
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-03-31 15:32:27 +0200

    Change animation of JSON file decryption

--- ui/pages/Onboarding/Tabbed/ImportPrivateKeyJSON.tsx
+++ ui/pages/Onboarding/Tabbed/ImportPrivateKeyJSON.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement, useCallback, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { importSigner } from "@tallyho/tally-background/redux-slices/keyrings"
-import { useDispatch } from "react-redux"
 import { SignerTypes } from "@tallyho/tally-background/services/keyring"
 import classnames from "classnames"
 import { selectCurrentAccount } from "@tallyho/tally-background/redux-slices/selectors"
@@ -9,7 +8,7 @@ import SharedButton from "../../../components/Shared/SharedButton"
 import PasswordInput from "../../../components/Shared/PasswordInput"
 import SharedFileInput from "../../../components/Shared/SharedFileInput"
 import SharedLoadingDoggo from "../../../components/Shared/SharedLoadingDoggo"
-import { useBackgroundSelector } from "../../../hooks"
+import { useBackgroundDispatch, useBackgroundSelector } from "../../../hooks"
 
 type Props = {
   isImporting: boolean
@@ -20,7 +19,7 @@ type Props = {
 export default function ImportPrivateKeyJSON(props: Props): ReactElement {
   const { setIsImporting, isImporting, finalize } = props
 
-  const dispatch = useDispatch()
+  const dispatch = useBackgroundDispatch()
   const selectedAccountAddress =
     useBackgroundSelector(selectCurrentAccount).address
   const [file, setFile] = useState("")
@@ -54,35 +53,44 @@ export default function ImportPrivateKeyJSON(props: Props): ReactElement {
     setIsImported(true)
   }, [dispatch, file, password, setIsImporting])
 
+  const showJSONForm = !isImporting && !isImported
+
   return (
     <>
       <div
-        className={classnames("loader_wrapper hidden_animation", {
-          hidden: !isImporting || isImported,
+        className={classnames("wrapper hidden_animation", {
+          "hidden zero_height": showJSONForm,
         })}
       >
-        <SharedLoadingDoggo size={70} />
-        <div className="simple_text bold">{t("decrypting")}...</div>
-        <div className="simple_text">{t("decryptingTime")}</div>
+        <div
+          className={classnames("wrapper_content hidden_animation", {
+            hidden: !isImporting,
+          })}
+        >
+          <SharedLoadingDoggo size={70} />
+          <div className="simple_text bold">{t("decrypting")}...</div>
+          <div className="simple_text">{t("decryptingTime")}</div>
+        </div>
+
+        <div
+          className={classnames("wrapper_content hidden_animation", {
+            hidden: isImporting,
+          })}
+        >
+          <div className="confetti">🎉</div>
+          <div className="simple_text bold">{t("completed")}</div>
+          <div className="simple_text">{t("address")}</div>
+          <div className="simple_text address">{selectedAccountAddress}</div>
+        </div>
       </div>
-      <div
-        className={classnames("completed_wrapper hidden_animation", {
-          hidden: !isImported,
-        })}
-      >
-        <div className="confetti">🎉</div>
-        <div className="simple_text bold">{t("completed")}</div>
-        <div className="simple_text">{t("address")}</div>
-        <div className="simple_text address">{selectedAccountAddress}</div>
-      </div>
+
       <div
         className={classnames("form_wrapper hidden_animation", {
-          hidden: isImporting || isImported,
+          "hidden zero_height": !showJSONForm,
         })}
       >
         <SharedFileInput
           onFileLoad={onFileLoad}
-          disabled={isImporting}
           fileTypeLabel={t("json")}
           style={{ marginBottom: "24px", width: "356px" }}
         />
@@ -100,27 +108,26 @@ export default function ImportPrivateKeyJSON(props: Props): ReactElement {
         onClick={isImported ? finalize : importWallet}
         center
       >
-        {isImporting || isImported ? t("finalize") : t("decrypt")}
+        {showJSONForm ? t("decrypt") : t("finalize")}
       </SharedButton>
       <style jsx>{`
-        .loader_wrapper {
+        .wrapper {
           background: var(--green-95);
-          padding: 30px 0;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
           width: 100%;
-          max-height: 185px;
           border-radius: 4px;
-        }
-        .completed_wrapper {
-          width: 100%;
-          background: var(--green-95);
+          height: 185px;
           padding: 15px 0;
+          position: relative;
+        }
+        .wrapper_content {
+          position: absolute;
+          top: 0;
           display: flex;
+          height: 100%;
+          width: 100%;
           flex-direction: column;
           align-items: center;
-          border-radius: 4px;
+          justify-content: center;
         }
         .simple_text.bold {
           font-weight: 600;
@@ -147,11 +154,13 @@ export default function ImportPrivateKeyJSON(props: Props): ReactElement {
           margin-bottom: 4px;
         }
         .hidden {
+          opacity: 0;
+          pointer-events: none;
+        }
+        .zero_height {
+          max-height: 0;
           margin: 0;
           padding: 0;
-          opacity: 0;
-          max-height: 0;
-          pointer-events: none;
         }
       `}</style>
     </>
