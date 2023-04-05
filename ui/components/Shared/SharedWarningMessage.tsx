4f7c291497efb75d7964ae3443028d6a93ba8a71
Karolina Kosiorowska <karolina.kosiorowska@tally.cash>
2023-04-05 14:13:21 +0200

    Update styles for `SharedWarningMessage`

--- ui/components/Shared/SharedWarningMessage.tsx
+++ ui/components/Shared/SharedWarningMessage.tsx
@@ -14,13 +14,19 @@ export default function SharedWarningMessage({
       <style jsx>{`
         .warning_wrap {
           display: flex;
+          align-items: center;
           padding: 8px 40px 8px 12px;
           border: 1px solid var(--attention);
           border-radius: 4px;
+          background: linear-gradient(
+            156deg,
+            rgba(242, 184, 36, 0.4) 0%,
+            rgba(242, 184, 36, 0.1) 45%
+          );
         }
         .icon {
-          margin-top: 4px;
           margin-right: 12px;
+          margin-top: 4px;
           height: 28px;
         }
         .text {
