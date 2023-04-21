17b16da631fc6b6b08e44004282acb14b58011fe
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-04-21 15:46:28 +0200

    Rename feature flag to `SUPPORT_PRIVATE_KEYS`

--- ui/pages/Onboarding/Tabbed/AddWalletOptions.tsx
+++ ui/pages/Onboarding/Tabbed/AddWalletOptions.tsx
@@ -76,7 +76,7 @@ export default function AddWalletOptions(): JSX.Element {
         label: t("options.importPrivateKey"),
         icon: "add_wallet/import_priv_key.svg",
         url: OnboardingRoutes.IMPORT_PRIVATE_KEY,
-        isAvailable: isEnabled(FeatureFlags.SUPPORT_PRIV_KEYS),
+        isAvailable: isEnabled(FeatureFlags.SUPPORT_PRIVATE_KEYS),
       },
       {
         label: t("options.ledger"),
