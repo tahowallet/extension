867f83357db8010752f45b04b7b515adc261ad6d
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-05-17 12:43:07 +0200

    Add integration test for EIP712 `signTypedData` method
    Test filtering additional fields in EIP712 messages - make sure
    all `message` fields that are not defined in `types` are removed
    from the `message`.

--- background/services/internal-ethereum-provider/tests/index.integration.test.ts
+++ background/services/internal-ethereum-provider/tests/index.integration.test.ts
@@ -2,12 +2,16 @@ import sinon from "sinon"
 import InternalEthereumProviderService from ".."
 import * as featureFlags from "../../../features"
 import { EVMNetwork } from "../../../networks"
-
 import {
   createChainService,
   createInternalEthereumProviderService,
 } from "../../../tests/factories"
 import { validateAddEthereumChainParameter } from "../../provider-bridge/utils"
+import { ETHEREUM } from "../../../constants"
+import { SignTypedDataRequest } from "../../../utils/signing"
+import { EIP712TypedData } from "../../../types"
+
+const TEST_ADDRESS = "0x208e94d5661a73360d9387d3ca169e5c130090cd"
 
 describe("Internal Ethereum Provider Service", () => {
   const sandbox = sinon.createSandbox()
@@ -58,5 +62,71 @@ describe("Internal Ethereum Provider Service", () => {
         (network: EVMNetwork) => network.name === "Fantom Opera"
       )
     ).toBeTruthy()
+  })
+
+  it("should filter out fields not specified in 'types' of eth_signTypedData_v4", async () => {
+    const METHOD = "eth_signTypedData_v4"
+    const ORIGIN = ""
+
+    const types = {
+      EIP712Domain: [
+        { name: "name", type: "string" },
+        { name: "version", type: "string" },
+        { name: "chainId", type: "uint256" },
+        { name: "verifyingContract", type: "address" },
+      ],
+      Request: [{ name: "message", type: "string" }],
+    }
+
+    const primaryType = "Request"
+
+    const domain = {
+      name: "EIP-712 Test",
+      version: "1",
+      chainId: "1",
+      verifyingContract: "0xdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",
+    }
+
+    const EIP712_OBJECT: EIP712TypedData = {
+      types,
+      domain,
+      primaryType,
+      message: {
+        "": "This should be removed",
+        "message ": "This should be removed",
+        " message": "This should be removed",
+        message: "This is correct",
+      },
+    }
+
+    const EIP712_OBJECT_FILTERED: SignTypedDataRequest = {
+      account: {
+        address: TEST_ADDRESS,
+        network: ETHEREUM,
+      },
+      typedData: {
+        types,
+        domain,
+        primaryType,
+        message: { message: "This is correct" },
+      },
+    }
+
+    const EIP712_PARAMS = [TEST_ADDRESS, JSON.stringify(EIP712_OBJECT)]
+
+    jest.spyOn(IEPService.emitter, "emit")
+
+    IEPService.emitter.on("signTypedDataRequest", ({ resolver }) => {
+      resolver("") // We have to manually resolve promise here
+    })
+
+    await IEPService.routeSafeRPCRequest(METHOD, EIP712_PARAMS, ORIGIN)
+
+    expect(IEPService.emitter.emit).toHaveBeenCalledWith(
+      "signTypedDataRequest",
+      expect.objectContaining({
+        payload: EIP712_OBJECT_FILTERED,
+      })
+    )
   })
 })
