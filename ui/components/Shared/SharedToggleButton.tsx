b2803d9a150709d2d12467756dfbf87cb357a191
Antonio Salazar Cardozo <antonio@thesis.co>
2023-06-06 00:59:25 -0400

    Allow SharedToggleButton to have a different direction and colors
    `onColor` and `offColor` parameters allow modifying the colors that the
    toggle's bulb takes on when it's turned on as well as when it's turned
    off. `leftToRight`, defaulting to `true`, indicates that the toggle is
    off on the left and on when on the right. Passing it as `false` flips
    the two horizontally.

--- ui/components/Shared/SharedToggleButton.tsx
+++ ui/components/Shared/SharedToggleButton.tsx
@@ -3,12 +3,31 @@ import classNames from "classnames"
 
 interface SharedToggleButtonProps {
   onChange: (toggleValue: boolean) => void
+  /**
+   * The color of the toggle bulb when the toggle is on. Any valid CSS color
+   * (including a `var` expression) can be used.
+   */
+  onColor?: string
+  /**
+   * The color of the toggle bulb when the toggle is off. Any valid CSS color
+   * (including a `var` expression) can be used.
+   */
+  offColor?: string
   value?: boolean | undefined
+  /**
+   * True if the toggle is off when on the left and on when on the right, false
+   * if the direction is opposite (on when on the left and off when on the
+   * right). True by default.
+   */
+  leftToRight: boolean
 }
 
 export default function SharedToggleButton({
   onChange,
+  onColor,
+  offColor,
   value,
+  leftToRight,
 }: SharedToggleButtonProps): ReactElement {
   const [isActive, setIsActive] = useState(value || false)
 
@@ -40,20 +59,28 @@ export default function SharedToggleButton({
             padding: 4px;
             cursor: pointer;
             display: flex;
+
+            ${leftToRight ? "" : "transform: rotate(180deg);"}
           }
           .bulb {
             width: 16px;
             height: 16px;
             border-radius: 20px;
-            background-color: var(--green-40);
+            background-color: ${offColor ?? "var(--green-40)"};
             transition: 0.2s ease-in-out;
+
+            box-shadow: 0px 1px 1px rgba(0, 20, 19, 0.3);
           }
           .is_active .bulb {
             transform: translateX(16px);
-            background-color: var(--trophy-gold);
+            background-color: ${onColor ?? "var(--trophy-gold)"};
           }
         `}
       </style>
     </button>
   )
+}
+
+SharedToggleButton.defaultProps = {
+  leftToRight: true,
 }
