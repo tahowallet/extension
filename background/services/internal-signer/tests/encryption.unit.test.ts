1acca3b8daf38ec6393e431204baee6f31d16647
Jagoda Berry Rybacka <jagoda.rybacka@tally.cash>
2023-06-28 16:38:51 +0200

    Add new unit tests to test encryption with Argon2

--- background/services/internal-signer/tests/encryption.unit.test.ts
+++ background/services/internal-signer/tests/encryption.unit.test.ts
@@ -5,126 +5,133 @@ import {
   VaultVersion,
 } from "../encryption"
 
-describe("Encryption utils", () => {
-  it("derives symmetric keys", async () => {
-    /* eslint-disable no-await-in-loop */
-    for (let i = 0; i < 5; i += 1) {
+describe.each([VaultVersion.PBKDF2, VaultVersion.Argon2])(
+  "Encryption utils",
+  (vaultVersion) => {
+    it("derives symmetric keys", async () => {
+      /* eslint-disable no-await-in-loop */
+      for (let i = 0; i < 5; i += 1) {
+        const password = Buffer.from(
+          global.crypto.getRandomValues(new Uint8Array(16))
+        ).toString("base64")
+        const newSalt = Buffer.from(
+          global.crypto.getRandomValues(new Uint8Array(16))
+        ).toString("base64")
+
+        const { key, salt } = await deriveSymmetricKeyFromPassword(
+          vaultVersion,
+          password,
+          newSalt
+        )
+        expect(salt).toEqual(newSalt)
+
+        expect(new Set(key.usages)).toEqual(new Set(["encrypt", "decrypt"]))
+      }
+      /* eslint-enable no-await-in-loop */
+    })
+
+    it("doesn't throw when encrypting a vault with a password", async () => {
+      const vault = { a: 1 }
+      const password = "this-is-a-poor-password"
+      await encryptVault({
+        version: vaultVersion,
+        vault,
+        passwordOrSaltedKey: password,
+      })
+    })
+
+    it("avoids couple common footguns when encrypting a vault with a password", async () => {
+      const vault = { thisIsAnInterestingKey: "sentinel" }
+      const password = "this-is-a-poor-password"
+      const newVault = await encryptVault({
+        version: vaultVersion,
+        vault,
+        passwordOrSaltedKey: password,
+      })
+      // ensure sensitive plaintext isn't in the output, with a couple simple
+      // transformations. Note this *doesn't* show correctness of encryption — a
+      // simple substitution cipher would still pass this — it's just a smoke test.
+      const importantPlaintext = [
+        "thisIsAnInterestingKey",
+        "sentinel",
+        password,
+      ]
+      const serializedVault = JSON.stringify(newVault)
+      importantPlaintext.forEach((t) => {
+        expect(serializedVault).not.toContain(t)
+        expect(serializedVault).not.toContain(t.toLowerCase())
+        expect(serializedVault).not.toContain(Buffer.from(t).toString("base64"))
+      })
+    })
+
+    it("can decrypt a vault encrypted with a password", async () => {
+      const vault = { a: 1 }
+      const password = "this-is-a-poor-password"
+      const encryptedVault = await encryptVault({
+        version: vaultVersion,
+        vault,
+        passwordOrSaltedKey: password,
+      })
+
+      const newVault = await decryptVault({
+        version: vaultVersion,
+        vault: encryptedVault,
+        passwordOrSaltedKey: password,
+      })
+
+      expect(newVault).toEqual(vault)
+    })
+
+    it("can decrypt a complex vault encrypted with a password", async () => {
+      const vault = {
+        a: { b: [1, 2, 3] },
+        c: null,
+        d: 123,
+      }
+      const password = "this-is-a-poor-password"
+      const encryptedVault = await encryptVault({
+        version: vaultVersion,
+        vault,
+        passwordOrSaltedKey: password,
+      })
+
+      const newVault = await decryptVault({
+        version: vaultVersion,
+        vault: encryptedVault,
+        passwordOrSaltedKey: password,
+      })
+
+      expect(newVault).toEqual(vault)
+    })
+
+    it("can decrypt a complex vault encrypted with a password", async () => {
+      const vault = {
+        a: { b: [1, 2, 3] },
+        c: null,
+        d: 123,
+      }
       const password = Buffer.from(
         global.crypto.getRandomValues(new Uint8Array(16))
       ).toString("base64")
-      const newSalt = Buffer.from(
-        global.crypto.getRandomValues(new Uint8Array(16))
-      ).toString("base64")
 
-      const { key, salt } = await deriveSymmetricKeyFromPassword(
-        VaultVersion.PBKDF2,
-        password,
-        newSalt
+      const saltedKey = await deriveSymmetricKeyFromPassword(
+        vaultVersion,
+        password
       )
-      expect(salt).toEqual(newSalt)
 
-      expect(new Set(key.usages)).toEqual(new Set(["encrypt", "decrypt"]))
-    }
-    /* eslint-enable no-await-in-loop */
-  })
+      const encryptedVault = await encryptVault({
+        version: vaultVersion,
+        vault,
+        passwordOrSaltedKey: saltedKey,
+      })
 
-  it("doesn't throw when encrypting a vault with a password", async () => {
-    const vault = { a: 1 }
-    const password = "this-is-a-poor-password"
-    await encryptVault({
-      version: VaultVersion.PBKDF2,
-      vault,
-      passwordOrSaltedKey: password,
+      const newVault = await decryptVault({
+        version: vaultVersion,
+        vault: encryptedVault,
+        passwordOrSaltedKey: saltedKey,
+      })
+
+      expect(newVault).toEqual(vault)
     })
-  })
-
-  it("avoids couple common footguns when encrypting a vault with a password", async () => {
-    const vault = { thisIsAnInterestingKey: "sentinel" }
-    const password = "this-is-a-poor-password"
-    const newVault = await encryptVault({
-      version: VaultVersion.PBKDF2,
-      vault,
-      passwordOrSaltedKey: password,
-    })
-    // ensure sensitive plaintext isn't in the output, with a couple simple
-    // transformations. Note this *doesn't* show correctness of encryption — a
-    // simple substitution cipher would still pass this — it's just a smoke test.
-    const importantPlaintext = ["thisIsAnInterestingKey", "sentinel", password]
-    const serializedVault = JSON.stringify(newVault)
-    importantPlaintext.forEach((t) => {
-      expect(serializedVault).not.toContain(t)
-      expect(serializedVault).not.toContain(t.toLowerCase())
-      expect(serializedVault).not.toContain(Buffer.from(t).toString("base64"))
-    })
-  })
-
-  it("can decrypt a vault encrypted with a password", async () => {
-    const vault = { a: 1 }
-    const password = "this-is-a-poor-password"
-    const encryptedVault = await encryptVault({
-      version: VaultVersion.PBKDF2,
-      vault,
-      passwordOrSaltedKey: password,
-    })
-
-    const newVault = await decryptVault({
-      version: VaultVersion.PBKDF2,
-      vault: encryptedVault,
-      passwordOrSaltedKey: password,
-    })
-
-    expect(newVault).toEqual(vault)
-  })
-
-  it("can decrypt a complex vault encrypted with a password", async () => {
-    const vault = {
-      a: { b: [1, 2, 3] },
-      c: null,
-      d: 123,
-    }
-    const password = "this-is-a-poor-password"
-    const encryptedVault = await encryptVault({
-      version: VaultVersion.PBKDF2,
-      vault,
-      passwordOrSaltedKey: password,
-    })
-
-    const newVault = await decryptVault({
-      version: VaultVersion.PBKDF2,
-      vault: encryptedVault,
-      passwordOrSaltedKey: password,
-    })
-
-    expect(newVault).toEqual(vault)
-  })
-
-  it("can decrypt a complex vault encrypted with a password", async () => {
-    const vault = {
-      a: { b: [1, 2, 3] },
-      c: null,
-      d: 123,
-    }
-    const password = Buffer.from(
-      global.crypto.getRandomValues(new Uint8Array(16))
-    ).toString("base64")
-
-    const saltedKey = await deriveSymmetricKeyFromPassword(
-      VaultVersion.PBKDF2,
-      password
-    )
-
-    const encryptedVault = await encryptVault({
-      version: VaultVersion.PBKDF2,
-      vault,
-      passwordOrSaltedKey: saltedKey,
-    })
-
-    const newVault = await decryptVault({
-      version: VaultVersion.PBKDF2,
-      vault: encryptedVault,
-      passwordOrSaltedKey: saltedKey,
-    })
-
-    expect(newVault).toEqual(vault)
-  })
-})
+  }
+)
---
+++ background/services/internal-signer/tests/storage.unit.test.ts
@@ -0,0 +1,101 @@
+import {
+  EncryptedVault,
+  VaultVersion,
+  decryptVault,
+  encryptVault,
+} from "../encryption"
+import {
+  getEncryptedVaults,
+  migrateVaultsToArgon,
+  writeLatestEncryptedVault,
+} from "../storage"
+
+const mockedPassword = "password"
+const mockedVault = { text: "secret" }
+
+describe("Storage utils", () => {
+  let vaultEncryptedWithPBKDF2: EncryptedVault
+  let vaultEncryptedWithArgon2: EncryptedVault
+
+  beforeAll(async () => {
+    vaultEncryptedWithPBKDF2 = await encryptVault({
+      vault: mockedVault,
+      passwordOrSaltedKey: mockedPassword,
+      version: VaultVersion.PBKDF2,
+    })
+
+    vaultEncryptedWithArgon2 = await encryptVault({
+      vault: mockedVault,
+      passwordOrSaltedKey: mockedPassword,
+      version: VaultVersion.Argon2,
+    })
+  })
+
+  it("should be able to store and retrieve data encrypted with PBKDF2", async () => {
+    await browser.storage.local.set({
+      tallyVaults: {
+        version: VaultVersion.PBKDF2,
+        vaults: [],
+      },
+    })
+    await writeLatestEncryptedVault(vaultEncryptedWithPBKDF2)
+
+    const { vaults, version } = await getEncryptedVaults()
+
+    expect(version).toEqual(VaultVersion.PBKDF2)
+    expect(vaults.length).toEqual(1)
+    expect(vaults[0].vault).toEqual(vaultEncryptedWithPBKDF2)
+  })
+
+  it("should be able to store and retrieve data encrypted with Argon2", async () => {
+    await browser.storage.local.set({
+      tallyVaults: {
+        version: VaultVersion.Argon2,
+        vaults: [],
+      },
+    })
+    await writeLatestEncryptedVault(vaultEncryptedWithArgon2)
+
+    const { vaults, version } = await getEncryptedVaults()
+
+    expect(version).toEqual(VaultVersion.Argon2)
+    expect(vaults.length).toEqual(1)
+    expect(vaults[0].vault).toEqual(vaultEncryptedWithArgon2)
+  })
+
+  it("shoould migrate existing vaults to Argon2", async () => {
+    await browser.storage.local.set({
+      tallyVaults: {
+        version: VaultVersion.PBKDF2,
+        vaults: [],
+      },
+    })
+    await writeLatestEncryptedVault(vaultEncryptedWithPBKDF2)
+
+    const { vaults, version } = await migrateVaultsToArgon(mockedPassword)
+
+    expect(version).toEqual(VaultVersion.Argon2)
+    expect(vaults.length).toEqual(1)
+
+    const decryptedVault = await decryptVault({
+      version: VaultVersion.Argon2,
+      vault: vaults[0].vault,
+      passwordOrSaltedKey: mockedPassword,
+    })
+    expect(decryptedVault).toEqual(mockedVault)
+  })
+
+  it("should not migrate vaults if they are already encrypted with Argon2", async () => {
+    await browser.storage.local.set({
+      tallyVaults: {
+        version: VaultVersion.Argon2,
+        vaults: [],
+      },
+    })
+    await writeLatestEncryptedVault(vaultEncryptedWithArgon2)
+
+    const { vaults } = await migrateVaultsToArgon(mockedPassword)
+
+    expect(vaults[0].vault).toEqual(vaultEncryptedWithArgon2)
+  })
+})
