5aadbd441c718cd49906342cfe1e168a3e3aee39
Antonio Salazar Cardozo <antonio@thesis.co>
2023-10-03 22:50:15 -0400

    Add a skeleton for the portal Banner
    We use a dismissable item for the banner itself, which will be dismissed
    on clicking the Explore The Island link.
    
    No styling is included at the moment, and there is a stub for not
    displaying the banner unless the token has been detected as
    deployed---this part is not yet implemented.

---
+++ ui/components/Wallet/Banner/PortalBanner.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement, useCallback } from "react"
+import {
+  dismissableItemMarkedAsShown,
+  selectShouldShowDismissableItem,
+} from "@tallyho/tally-background/redux-slices/ui"
+import { useBackgroundDispatch, useBackgroundSelector } from "../../../hooks"
+
+export default function PortalBanner(): ReactElement | null {
+  const isTokenDeployed = true // useBackgroundSelector(isTestTahoDeployed)
+  const isBannerVisible = useBackgroundSelector(
+    selectShouldShowDismissableItem("testnet-portal-is-open-banner"),
+  )
+  const dispatch = useBackgroundDispatch()
+
+  const dismissBanner = () =>
+    dispatch(dismissableItemMarkedAsShown("testnet-portal-is-open-banner"))
+
+  if (!isTokenDeployed || !isBannerVisible) {
+    return null
+  }
+
+  return (
+    <div>
+      The portal is open!{" "}
+      <a
+        href="https://island.taho.xyz/"
+        target="_blank"
+        rel="noreferrer"
+        onClick={dismissBanner}
+      >
+        Explore The Island
+      </a>
+    </div>
+  )
+}
