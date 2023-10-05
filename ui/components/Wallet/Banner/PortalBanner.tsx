12a702505593ed3ea350966682ff5f35669d5c7b
Antonio Salazar Cardozo <antonio@thesis.co>
2023-10-04 23:04:32 -0400

    Implement portal banner aligned to styles
    Notably, rather than using a link, we use a `SharedButton`. This is
    because the styling is basically the same as `SharedButton`, and
    `SharedLink` can't currently reproduce that (bummer). It also means link
    opening has to be implemented via browser.tabs.create :/

--- ui/components/Wallet/Banner/PortalBanner.tsx
+++ ui/components/Wallet/Banner/PortalBanner.tsx
@@ -1,10 +1,14 @@
-import React, { ReactElement, useCallback } from "react"
+import browser from "webextension-polyfill"
+import React, { ReactElement } from "react"
 import {
   dismissableItemMarkedAsShown,
   selectShouldShowDismissableItem,
 } from "@tallyho/tally-background/redux-slices/ui"
 import { selectIsTestTahoDeployed } from "@tallyho/tally-background/redux-slices/claim"
 import { useBackgroundDispatch, useBackgroundSelector } from "../../../hooks"
+import SharedButton from "../../Shared/SharedButton"
+import SharedIcon from "../../Shared/SharedIcon"
+import SharedBanner from "../../Shared/SharedBanner"
 
 export default function PortalBanner(): ReactElement | null {
   const isTokenDeployed = useBackgroundSelector(selectIsTestTahoDeployed)
@@ -13,24 +17,68 @@ export default function PortalBanner(): ReactElement | null {
   )
   const dispatch = useBackgroundDispatch()
 
-  const dismissBanner = () =>
+  const showIslandAndDismissBanner = () => {
+    // FIXME Shortcut. We should use a real link here.
+    browser.tabs.create({ url: "https://island.taho.xyz" })
     dispatch(dismissableItemMarkedAsShown("testnet-portal-is-open-banner"))
+  }
 
   if (!isTokenDeployed || !isBannerVisible) {
     return null
   }
 
   return (
-    <div>
-      The portal is open!{" "}
-      <a
-        href="https://island.taho.xyz/"
-        target="_blank"
-        rel="noreferrer"
-        onClick={dismissBanner}
+    <SharedBanner
+      customStyles={`
+        padding: 8px 11px 0 0;
+        margin-bottom: 18px;
+        background:
+          var(--gradients-bg-cards,
+              radial-gradient(
+                57.41% 54.95% at 64.58% 47.64%,
+                rgba(27, 97, 94, 0.00) 0%,
+                rgba(27, 97, 94, 0.20) 100%
+              ),
+              linear-gradient(
+                156deg,
+                rgba(26, 94, 91, 0.90) 5.26%,
+                rgba(26, 106, 103, 0.00) 71.95%),
+                rgba(6, 48, 46, 0.50)
+             )
+      `}
+    >
+      <i className="portal_open_image" />
+      <h2 className="serif_header">The portal is open!</h2>{" "}
+      <SharedButton
+        type="primary"
+        size="medium"
+        onClick={showIslandAndDismissBanner}
       >
-        Explore The Island
-      </a>
-    </div>
+        Explore The Island{" "}
+        <SharedIcon
+          icon="new_tab@2x.png"
+          width={16}
+          color="var(--castle-black)"
+          customStyles="margin-left: 5px;"
+        />
+      </SharedButton>
+      <style jsx>
+        {`
+          .portal_open_image {
+            float: left;
+            width: 132px;
+            height: 92px;
+            background: url("./images/island/portal-image@2x.png");
+            background-size: cover;
+          }
+
+          h2 {
+            font-size: 22px;
+            line-height: 32px;
+            margin-bottom: 5px;
+          }
+        `}
+      </style>
+    </SharedBanner>
   )
 }
