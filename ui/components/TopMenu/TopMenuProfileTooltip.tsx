85636419d5fc77c6059345ad51d20918dda5bd6f
Antonio Salazar Cardozo <antonio@thesis.co>
2024-10-06 12:59:31 -0400

    Switch TopMenuProfileTooltip to use SharedAddress for copying

--- ui/components/TopMenu/TopMenuProfileTooltip.tsx
+++ ui/components/TopMenu/TopMenuProfileTooltip.tsx
@@ -1,16 +1,20 @@
 import React, { ReactElement } from "react"
 import { useTranslation } from "react-i18next"
+import SharedAddress from "../Shared/SharedAddress"
 
 export default function TopMenuProfileTooltip(props: {
-  copyAddress: () => void
+  address: string
 }): ReactElement {
   const { t } = useTranslation()
-  const { copyAddress } = props
+  const { address } = props
 
   return (
-    <button type="button" className="tooltip" onClick={copyAddress}>
-      <span className="tooltip_label">{t("topMenu.copyAddressTooltip")}</span>
-      <span className="tooltip_icon" />
+    <div className="tooltip">
+      <SharedAddress
+        address={address}
+        name={t("topMenu.copyAddressTooltip")}
+        showCopyIcon
+      />
       <style jsx>{`
         .tooltip {
           display: flex;
@@ -25,27 +29,7 @@ export default function TopMenuProfileTooltip(props: {
           border-radius: 8px;
           color: var(--green-5);
         }
-        .tooltip:hover {
-          color: var(--trophy-gold);
-        }
-        .tooltip_label {
-          white-space: nowrap;
-          font-size: 16px;
-          line-height: 24px;
-        }
-        .tooltip_icon {
-          mask-image: url("./images/copy@2x.png");
-          mask-size: cover;
-          width: 24px;
-          height: 24px;
-          margin-left: 10px;
-          display: inline-block;
-          background-color: var(--green-5);
-        }
-        .tooltip:hover .tooltip_icon {
-          background-color: var(--trophy-gold);
-        }
       `}</style>
-    </button>
+    </div>
   )
 }
