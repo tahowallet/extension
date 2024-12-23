e6d2b3e35fe11e7592b9737020310dfe77143de8
Antonio Salazar Cardozo <antonio@thesis.co>
2024-12-23 15:39:42 -0500

    Move inpage provider injection to content script
    Manifest v3 CSP changes prevent injecting the in-page provider source
    through a script element. Injection has been moved to a content script
    so that execution time and context remain consistent.
    
    Co-authored-by: hyphenized <28708889+hyphenized@users.noreply.github.com>

--- provider-bridge/index.ts
+++ provider-bridge/index.ts
@@ -3,13 +3,12 @@ import {
   EXTERNAL_PORT_NAME,
   PROVIDER_BRIDGE_TARGET,
   WINDOW_PROVIDER_TARGET,
+  isObject,
 } from "@tallyho/provider-bridge-shared"
 
 const windowOriginAtLoadTime = window.location.origin
 
-const INJECTED_WINDOW_PROVIDER_SOURCE = "@@@WINDOW_PROVIDER@@@"
-
-export function connectProviderBridge(): void {
+export default function connectProviderBridge(): void {
   const port = browser.runtime.connect({ name: EXTERNAL_PORT_NAME })
   window.addEventListener("message", (event) => {
     if (
@@ -54,6 +53,10 @@ export function connectProviderBridge(): void {
   })
 
   port.onMessage.addListener((data) => {
+    if (!isObject(data)) {
+      return
+    }
+
     // TODO: replace with better logging before v1. Now it's invaluable in debugging.
     // eslint-disable-next-line no-console
     console.log(
@@ -80,25 +83,4 @@ export function connectProviderBridge(): void {
   port.postMessage({
     request: { method: "tally_getConfig", origin: windowOriginAtLoadTime },
   })
-}
-
-export function injectTahoWindowProvider(): void {
-  if (document.contentType !== "text/html") return
-
-  try {
-    const container = document.head || document.documentElement
-    const scriptTag = document.createElement("script")
-    // this makes the script loading blocking which is good for us
-    // bc we want to load before anybody has a chance to temper w/ the window obj
-    scriptTag.setAttribute("async", "false")
-    scriptTag.textContent = INJECTED_WINDOW_PROVIDER_SOURCE
-
-    container.insertBefore(scriptTag, container.children[0])
-  } catch (e) {
-    throw new Error(
-      `Taho: oh nos the content-script failed to initilaize the Taho window provider.
-        ${e}
-        It's time for a seppuku...🗡`,
-    )
-  }
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -13,7 +13,6 @@ import LiveReloadPlugin from "webpack-livereload-plugin"
 import CopyPlugin, { ObjectPattern } from "copy-webpack-plugin"
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin"
 import WebExtensionArchivePlugin from "./build-utils/web-extension-archive-webpack-plugin"
-import InjectWindowProvider from "./build-utils/inject-window-provider"
 import "dotenv-defaults/config"
 
 const supportedBrowsers = ["chrome"]
@@ -71,7 +70,6 @@ const baseConfig: Configuration = {
     },
   },
   plugins: [
-    new InjectWindowProvider(),
     new Dotenv({
       defaults: true,
       systemvars: true,
